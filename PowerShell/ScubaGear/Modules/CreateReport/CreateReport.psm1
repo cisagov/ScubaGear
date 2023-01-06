e4fd60508aaab99334c88454cb16ff2de767538b
Alden Hilton <adhilto@sandia.gov>
2023-01-06 12:34:38 -0800

    Moved CAP table to end, combined users included and excluded columns, impletemented Apps/Actions column

--- PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
+++ PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
@@ -1,69 +1,230 @@
-function Get-CapTable {
-    $FileName = Join-Path -Path $OutPath -ChildPath "$($OutProviderFileName).json"
-    $SettingsExport =  Get-Content $FileName | ConvertFrom-Json
-    $Caps = $SettingsExport.conditional_access_policies
-
-    $ExternalUserStrings = @{"b2bCollaborationGuest" = "B2B collaboration guest users";
+# Dictionaries used to map the codes used in the provider output to
+# human-friendly strings
+$ExternalUserStrings = @{"b2bCollaborationGuest" = "B2B collaboration guest users";
         "b2bCollaborationMember" = "B2B collaboration member users";
         "b2bDirectConnectUser" = "B2B direct connect users";
         "internalGuest" = "Local guest users";
         "serviceProvider" = "Service provider users";
         "otherExternalUser" = "Other external users"}
 
+$StateStrings = @{"enabled" = "On";
+    "enabledForReportingButNotEnforced" = "Report-only";
+    "disabled" = "Off"}
+
+$ActionStrings = @{"urn:user:registersecurityinfo" = "Register security info";
+    "urn:user:registerdevice" = "Register or join devices"}
+function Get-IncludedUsers {
+    <#
+    .Description
+    Parses the provider output to generate a list of included users for a given conditional access policy (Cap).
+    .Functionality
+    Internal
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [System.Object]
+        $Cap
+    )
+    process {
+
+        $IncludedUsers = @()
+            if ($Cap.Conditions.Users.IncludeUsers -Contains "All") {
+                $IncludedUsers += "All"
+            }
+            elseif ($Cap.Conditions.Users.IncludeUsers -Contains "None") {
+                $IncludedUsers += "None"
+            }
+            else {
+                # Users
+                # TODO add JS button that enumerates the users
+                if ($Cap.Conditions.Users.IncludeUsers.Length -eq 1) {
+                    $IncludedUsers += "1 specific user"
+                }
+                elseif ($Cap.Conditions.Users.IncludeUsers.Length -gt 1) {
+                    $IncludedUsers += "$($Cap.Conditions.Users.IncludeUsers.Length) specific users"
+                }
+
+                # Roles
+                # TODO add JS button that enumerates the roles
+                if ($Cap.Conditions.Users.IncludeRoles.Length -eq 1) {
+                    $IncludedUsers += "1 specific role"
+                }
+                elseif ($Cap.Conditions.Users.IncludeRoles.Length -gt 1) {
+                    $IncludedUsers += "$($Cap.Conditions.Users.IncludeRoles.Length) specific roles"
+                }
+
+                # Groups
+                # TODO add JS button that enumerates the groups
+                if ($Cap.Conditions.Users.IncludeGroups.Length -eq 1) {
+                    $IncludedUsers += "1 specific group"
+                }
+                elseif ($Cap.Conditions.Users.IncludeGroups.Length -gt 1) {
+                    $IncludedUsers += "$($Cap.Conditions.Users.IncludeGroups.Length) specific groups"
+                }
+
+                # External/guests
+                if ($null -ne $Cap.Conditions.Users.IncludeGuestsOrExternalUsers.ExternalTenants.MembershipKind) {
+                    $GuestOrExternalUserTypes = $Cap.Conditions.Users.IncludeGuestsOrExternalUsers.GuestOrExternalUserTypes -Split ","
+                    $IncludedUsers += @($GuestOrExternalUserTypes | ForEach-Object {$ExternalUserStrings[$_]})
+                }
+            }
+        $IncludedUsers
+    }
+}
+
+function Get-ExcludedUsers {
+    <#
+    .Description
+    Parses the provider output to generate a list of excluded users for a given conditional access policy (Cap).
+    .Functionality
+    Internal
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [System.Object]
+        $Cap
+    )
+    process {
+        $ExcludedUsers = @()
+        # Users
+        # TODO add JS button that enumerates the users
+        if ($Cap.Conditions.Users.ExcludeUsers.Length -eq 1) {
+            $ExcludedUsers += "1 specific user"
+        }
+        elseif ($Cap.Conditions.Users.ExcludeUsers.Length -gt 1) {
+            $ExcludedUsers += "$($Cap.Conditions.Users.ExcludeUsers.Length) specific users"
+        }
+
+        # Roles
+        # TODO add JS button that enumerates the roles
+        if ($Cap.Conditions.Users.ExcludeRoles.Length -eq 1) {
+            $ExcludedUsers += "1 specific role"
+        }
+        elseif ($Cap.Conditions.Users.ExcludeRoles.Length -gt 1) {
+            $ExcludedUsers += "$($Cap.Conditions.Users.ExcludeRoles.Length) specific roles"
+        }
+
+        # Groups
+        # TODO add JS button that enumerates the groups
+        if ($Cap.Conditions.Users.ExcludeGroups.Length -eq 1) {
+            $ExcludedUsers += "1 specific group"
+        }
+        elseif ($Cap.Conditions.Users.ExcludeGroups.Length -gt 1) {
+            $ExcludedUsers += "$($Cap.Conditions.Users.ExcludeGroups.Length) specific groups"
+        }
+
+        # External/guests
+        if ($null -ne $Cap.Conditions.Users.ExcludeGuestsOrExternalUsers.ExternalTenants.MembershipKind) {
+            $GuestOrExternalUserTypes = $Cap.Conditions.Users.ExcludeGuestsOrExternalUsers.GuestOrExternalUserTypes -Split ","
+            $ExcludedUsers += @($GuestOrExternalUserTypes | ForEach-Object {$ExternalUserStrings[$_]})
+        }
+
+        # If no users are excluded, rather than display an empty cell, display "None"
+        if ($ExcludedUsers.Length -eq 0) {
+            $ExcludedUsers += "None"
+        }
+        $ExcludedUsers
+    }
+}
+
+function Get-Applications {
+    <#
+    .Description
+    Parses the provider output to generate a list of included/excluded applications/actions for a given conditional access policy (Cap).
+    .Functionality
+    Internal
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [System.Object]
+        $Cap
+    )
+    process {
+        $Actions = ""
+        if ($Cap.Conditions.Applications.IncludeApplications.Length -gt 0) {
+            # For "Select what this policy applies to", "Cloud Apps" was  selected
+            $Actions += "Policy applies to: apps.<br>"
+            # Included apps: 
+            if ($Cap.Conditions.Applications.IncludeApplications -Contains "All") {
+                $Actions += "Apps included: All"
+            }
+            elseif ($Cap.Conditions.Applications.IncludeApplications -Contains "None") {
+                $Actions += "Apps included: None"
+            }
+            elseif ($Cap.Conditions.Applications.IncludeApplications.Length -eq 1) {
+                $Actions += "Apps included: 1 specific app"
+            }
+            else {
+                $Actions += "Apps included: $($Cap.Conditions.Applications.IncludeApplications.Length) specific apps"
+            }
+
+            # Excluded apps: # TODO FILTERs
+            if ($Cap.Conditions.Applications.ExcludeApplications.Length -eq 0) {
+                $Actions += "<br>Apps excluded: None"
+            }
+            elseif ($Cap.Conditions.Applications.ExcludeApplications.Length -eq 1) {
+                $Actions += "<br>Apps excluded: 1 specific app"
+            }
+            else {
+                $Actions += "<br>Apps excluded: $($Cap.Conditions.Applications.ExcludeApplications.Length) specific apps"
+            }
+        }
+        elseif ($Cap.Conditions.Applications.IncludeUserActions.Length -gt 0) {
+            # For "Select what this policy applies to", "User actions" was selected
+            $Actions += "Policy applies to: actions.<br>"
+            $Actions += "User action: $($ActionStrings[$Cap.Conditions.Applications.IncludeUserActions[0]])"
+            # While "IncludeUserActions" is a list, the GUI doesn't actually let you select more than one
+            # item at a time, hence "IncludeUserActions[0]" above
+        }
+        else {
+            # For "Select what this policy applies to", "Authentication context" was selected
+            # TODO Add JS button that enumerates the contexts
+            $AuthContexts = $Cap.Conditions.Applications.IncludeAuthenticationContextClassReferences
+            if ($AuthContexts.Length -eq 1) {
+                $Actions += "Policy applies to: 1 authentication context"
+            }
+            else {
+                $Actions += "Policy applies to: $($AuthContexts.Length) authentication contexts"
+            }
+        }
+
+        $Actions
+    }
+}
+
+function Get-CapTable {
+    $FileName = Join-Path -Path $OutPath -ChildPath "$($OutProviderFileName).json"
+    $SettingsExport =  Get-Content $FileName | ConvertFrom-Json
+    $Caps = $SettingsExport.conditional_access_policies
+
     $Table = @()
 
     foreach ($Cap in $Caps) {
-
-        $IncludedUsers = @()
-        if ($Cap.Conditions.Users.IncludeUsers -Contains "All") {
-            $IncludedUsers += "All"
-        }
-        elseif ($Cap.Conditions.Users.IncludeUsers -Contains "None") {
-            $IncludedUsers += "None"
-        }
-        else {
-            $SpecificUsers = @($Cap.Conditions.Users.IncludeUsers | Where-Object {$_ -ne "All"})
-            if ($SpecificUsers.Length -eq 1) {
-                $IncludedUsers += "$($SpecificUsers.Length) specific user"
-            }
-            elseif ($SpecificUsers.Length -gt 1) {
-                $IncludedUsers += "$($SpecificUsers.Length) specific users"
-            }
-
-            $SpecificRoles = @($Cap.Conditions.Users.IncludeRoles | Where-Object {$_ -ne "All"})
-            if ($SpecificRoles.Length -eq 1) {
-                $IncludedUsers += "$($SpecificRoles.Length) specific role"
-            }
-            elseif ($SpecificRoles.Length -gt 1) {
-                $IncludedUsers += "$($SpecificRoles.Length) specific roles"
-            }
-
-            $SpecificGroups = @($Cap.Conditions.Users.IncludeGroups | Where-Object {$_ -ne "All"})
-            if ($SpecificGroups.Length -eq 1) {
-                $IncludedUsers += "$($SpecificGroups.Length) specific group"
-            }
-            elseif ($SpecificGroups.Length -gt 1) {
-                $IncludedUsers += "$($SpecificGroups.Length) specific groups"
-            }
-
-            if ($null -ne $Cap.Conditions.Users.IncludeGuestsOrExternalUsers.ExternalTenants.MembershipKind) {
-                $GuestOrExternalUserTypes = $Cap.Conditions.Users.IncludeGuestsOrExternalUsers.GuestOrExternalUserTypes -Split ","
-                $IncludedUsers += @($GuestOrExternalUserTypes | ForEach-Object {$ExternalUserStrings[$_]})
-            }
-        }
-        $IncludedUsers = $IncludedUsers -Join ", "
-
+        $UsersIncluded = $(Get-IncludedUsers -Cap $Cap) -Join ", "
+        $UsersExcluded = $(Get-ExcludedUsers -Cap $Cap) -Join ", "
+        $Apps = Get-Applications -Cap $Cap
         $CapDetails = [pscustomobject]@{
             "Name" = $Cap.DisplayName;
-            "Users included" = $IncludedUsers -Join ", ";
-            "Users excluded" = ""
+            "Users" = "Users included: $($UsersIncluded)<br>Users excluded: $($UsersExcluded)";
+            "Apps/Actions" = $Apps;
+            "State" = $StateStrings[$Cap.State];
+            "Conditions" = "TODO";
+            "Access Controls" = "TODO";
         }
 
         $Table += $CapDetails
     }
 
     $TableHtml = $Table | ConvertTo-Html -Fragment
-    $TableHtml = "<div class=caps>$($TableHtml)</div>"
+    $TableHtml = "
+    <div id=caps>
+        <hr>
+        <h2>Conditional Access Policies</h2>
+        $($TableHtml)
+    </div>"
     $TableHtml
 }
 
@@ -184,9 +345,6 @@ function New-Report {
         $Name = $Title.Title
         $Fragments += $Fragment | ConvertTo-Html -PreContent "<h2>$Number $Name</h2>" -Fragment
 
-        if ($Number -eq "AAD 2.1") {
-            $Fragments += Get-CapTable
-        }
     }
 
     $Title = "$($FullName) Baseline Report"
@@ -201,9 +359,11 @@ function New-Report {
     $ReportHTML = $ReportHTML.Replace("{TITLE}", $Title)
     if ($BaselineName -eq "aad") {
         $ReportHTML = $ReportHTML.Replace("{AADWARNING}", $AADWarning)
+        $ReportHTML = $ReportHTML.Replace("{CAPTABLES}", $(Get-CapTable))
     }
     else {
         $ReportHTML = $ReportHTML.Replace("{AADWARNING}", $NoWarning)
+        $ReportHTML = $ReportHTML.Replace("{CAPTABLES}", "")
     }
 
     $MainCSS = Get-Content $(Join-Path -Path $ReporterPath -ChildPath "main.css")
