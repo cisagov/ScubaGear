c2d35dd677c30bd1d7f13be230eadf8bf5f471d5
Dylan Gao <107067015+Dylan-MITRE@users.noreply.github.com>
2023-01-26 14:25:12 -0500

    remove SPOSite

--- PowerShell/ScubaGear/Modules/Providers/ExportOneDriveProvider.psm1
+++ PowerShell/ScubaGear/Modules/Providers/ExportOneDriveProvider.psm1
@@ -18,15 +18,7 @@ function Export-OneDriveProvider {
     Import-Module (Join-Path -Path $HelperFolderPath -ChildPath "SPOSiteHelper.psm1")
     $Tracker = Get-CommandTracker
 
-    #Get InitialDomainPrefix
-    $InitialDomain = ($Tracker.TryCommand("Get-MgOrganization")).VerifiedDomains | Where-Object {$_.isInitial}
-    $InitialDomainPrefix = $InitialDomain.Name.split(".")[0]
-
-    #Get SPOSiteIdentity
-    $SPOSiteIdentity = Get-SPOSiteHelper -M365Environment $M365Environment -InitialDomainPrefix $InitialDomainPrefix
-
     $SPOTenantInfo = ConvertTo-Json @($Tracker.TryCommand("Get-SPOTenant"))
-    $ExpectedResults = ConvertTo-Json @($Tracker.TryCommand("Get-SPOSite", @{"Identity"="$($SPOSiteIdentity)"}))
     $TenantSyncInfo = ConvertTo-Json @($Tracker.TryCommand("Get-SPOTenantSyncClientRestriction"))
 
     $SuccessfulCommands = ConvertTo-Json @($Tracker.GetSuccessfulCommands())
@@ -35,7 +27,6 @@ function Export-OneDriveProvider {
     # Note the spacing and the last comma in the json is important
     $json = @"
     "SPO_tenant_info": $SPOTenantInfo,
-    "Expected_results": $ExpectedResults,
     "Tenant_sync_info": $TenantSyncInfo,
     "OneDrive_successful_commands": $SuccessfulCommands,
     "OneDrive_unsuccessful_commands": $UnSuccessfulCommands,
