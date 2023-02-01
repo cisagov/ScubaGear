ac455948581c2ebfa3023b4acd9c0d2ed65afdaf
buidav <dbui>
2023-02-01 14:37:40 -0800

    add PnP.PowerShell dependency

--- PowerShell/ScubaGear/RequiredVersions.ps1
+++ PowerShell/ScubaGear/RequiredVersions.ps1
@@ -16,6 +16,11 @@ $ModuleList = @(
         MaximumVersion = [version] '16.99.99999'
     },
     @{
+        ModuleName = 'PnP.PowerShell' # alternate for SharePoint PowerShell
+        ModuleVersion = [version] '1.12.0'
+        MaximumVersion = [version] '1.99.99999'
+    },
+    @{
         ModuleName = 'Microsoft.PowerApps.Administration.PowerShell'
         ModuleVersion = [version] '2.0.0'
         MaximumVersion = [version] '2.99.99999'
