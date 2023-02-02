ca038db80d491e310777766d4691f57f970813ce
Alden Hilton <adhilto@sandia.gov>
2023-02-01 20:31:08 -0800

    Added basic error handling and documentation to the JavaScript

--- PowerShell/ScubaGear/Modules/CreateReport/scripts/main.js
+++ PowerShell/ScubaGear/Modules/CreateReport/scripts/main.js
@@ -1,38 +1,52 @@
+/**
+ * Adds the red, green, yellow, and gray coloring to the individual report pages.
+ */
 let colorRows = () => {
     let rows = document.querySelectorAll('tr');
     const statusCol = 1;
     const criticalityCol = 2;
     for (let i = 0; i < rows.length; i++) {
-        if (rows[i].children[statusCol].innerHTML === "Fail") {
-            rows[i].style.background = "#deb8b8";
+        try {
+            if (rows[i].children[statusCol].innerHTML === "Fail") {
+                rows[i].style.background = "#deb8b8";
+            }
+            else if (rows[i].children[statusCol].innerHTML === "Warning") {
+                rows[i].style.background = "#fff7d6";
+            }
+            else if (rows[i].children[statusCol].innerHTML === "Pass") {
+                rows[i].style.background = "#d5ebd5";
+            }
+            else if (rows[i].children[criticalityCol].innerHTML.includes("Not-Implemented")) {
+                rows[i].style.background = "#ebebf2";
+            }
+            else if (rows[i].children[criticalityCol].innerHTML.includes("3rd Party")) {
+                rows[i].style.background = "#ebebf2";
+            }
+            else if (rows[i].children[statusCol].innerHTML.includes("Error")) {
+                rows[i].style.background = "#deb8b8";
+                rows[i].querySelectorAll('td')[1].style.borderColor = "black";
+                rows[i].querySelectorAll('td')[1].style.color = "#d10000";
+            }
         }
-        else if (rows[i].children[statusCol].innerHTML === "Warning") {
-            rows[i].style.background = "#fff7d6";
-        }
-        else if (rows[i].children[statusCol].innerHTML === "Pass") {
-            rows[i].style.background = "#d5ebd5";
-        }
-        else if (rows[i].children[criticalityCol].innerHTML.includes("Not-Implemented")) {
-            rows[i].style.background = "#ebebf2";
-        }
-        else if (rows[i].children[criticalityCol].innerHTML.includes("3rd Party")) {
-            rows[i].style.background = "#ebebf2";
-        }
-        else if (rows[i].children[statusCol].innerHTML.includes("Error")) {
-            rows[i].style.background = "#deb8b8";
-            rows[i].querySelectorAll('td')[1].style.borderColor = "black";
-            rows[i].querySelectorAll('td')[1].style.color = "#d10000";
+        catch (error) {
+            console.error(`Error in colorRows, i = ${i}`);
+            console.error(error);
         }
     }
 }
 
-/*  For AAD Conditional Access Policies 
-    The "" column after "Name" is for the nameless column with the Show more/Show less buttons
-*/
+/**
+ * For the conditional access policy table. For AAD only.
+ * The "" column after "Name" is used for the nameless column that holds the
+ * Show more / Show less buttons.
+ */
 const capColNames = ["Name", "", "State", "Users", "Apps/Actions", "Conditions", "Block/Grant Access", "Session Controls"];
 
+/**
+ * Creates the conditional access policy table at the end of the AAD report.
+ * For all other reports (e.g., teams), this function does nothing.
+ */
 let fillCAPTable = () => {
-    /* For AAD Conditional Access Policies */
     if (caps === undefined || caps === null) {
         /*  The CAP table is only displayed for the AAD baseline, but
             this js file applies to all baselines. If caps is null,
@@ -45,167 +59,221 @@ let fillCAPTable = () => {
         */
        return;
     }
-    let capDiv = document.createElement("div");
-    capDiv.setAttribute("id", "caps");
-    document.querySelector("main").appendChild(capDiv);
+    try {
+        let capDiv = document.createElement("div");
+        capDiv.setAttribute("id", "caps");
+        document.querySelector("main").appendChild(capDiv);
 
-    capDiv.appendChild(document.createElement("hr"));
-    h2 = document.createElement("h2");
-    h2.innerHTML = "Conditional Access Policies";
-    capDiv.appendChild(h2);
+        capDiv.appendChild(document.createElement("hr"));
+        h2 = document.createElement("h2");
+        h2.innerHTML = "Conditional Access Policies";
+        capDiv.appendChild(h2);
 
-    let buttons = document.createElement("div");
-    buttons.classList.add("buttons");
-    capDiv.appendChild(buttons);
+        let buttons = document.createElement("div");
+        buttons.classList.add("buttons");
+        capDiv.appendChild(buttons);
 
-    let expandAll = document.createElement("span");
-    expandAll.classList.add("button");
-    expandAll.appendChild(document.createTextNode("&#x2b; Expand all"));
-    expandAll.title = "Expand all";
-    expandAll.addEventListener("click", expandAllCAPs);
-    buttons.appendChild(expandAll);
+        let expandAll = document.createElement("span");
+        expandAll.classList.add("button");
+        expandAll.appendChild(document.createTextNode("&#x2b; Expand all"));
+        expandAll.title = "Expand all";
+        expandAll.addEventListener("click", expandAllCAPs);
+        buttons.appendChild(expandAll);
 
-    let collapseAll = document.createElement("span");
-    collapseAll.classList.add("button");
-    collapseAll.appendChild(document.createTextNode("&minus; Collapse all"));
-    collapseAll.title = "Collapse all";
-    collapseAll.addEventListener("click", collapseAllCAPs);
-    buttons.appendChild(collapseAll);
+        let collapseAll = document.createElement("span");
+        collapseAll.classList.add("button");
+        collapseAll.appendChild(document.createTextNode("&minus; Collapse all"));
+        collapseAll.title = "Collapse all";
+        collapseAll.addEventListener("click", collapseAllCAPs);
+        buttons.appendChild(collapseAll);
 
-    let table = document.createElement("table");
-    capDiv.appendChild(table);
-    let header = document.createElement("tr");
-    for (let i = 0; i < capColNames.length; i++) {
-        let th = document.createElement("th");
-        th.innerHTML = capColNames[i];
-        header.appendChild(th);
+        let table = document.createElement("table");
+        capDiv.appendChild(table);
+
+        let header = document.createElement("tr");
+        for (let i = 0; i < capColNames.length; i++) {
+            let th = document.createElement("th");
+            th.innerHTML = capColNames[i];
+            header.appendChild(th);
+        }
+        table.appendChild(header);
+
+        for (let i = 0; i < caps.length; i++) {
+            let tr = document.createElement("tr");
+            for (let j = 0; j < capColNames.length; j++) {
+                let td = document.createElement("td");
+                fillTruncatedCell(td, i,j);
+                tr.appendChild(td);
+            }
+
+            let img = document.createElement("img");
+            img.setAttribute('src', 'images/angle-right-solid.svg');
+            img.setAttribute('alt', 'Show more');
+            img.setAttribute('title', 'Show more');
+            img.style.width = '10px';
+            img.rowNumber = i;
+            img.addEventListener("click", expandCAPRow);
+            tr.querySelectorAll('td')[1].appendChild(img);
+            table.appendChild(tr);
+        }
     }
-    table.appendChild(header);
+    catch (error) {
+        console.error("Error in fillCAPTable");
+        console.error(error);
+    }
+}
 
-    for (let i = 0; i < caps.length; i++) {
-        let tr = document.createElement("tr");
-        for (let j = 0; j < capColNames.length; j++) {
-            let td = document.createElement("td");
-            fillTruncatedCell(td, i,j);
-            tr.appendChild(td);
+/**
+ * Fills in the truncated version of the given cell of the AAD conditional
+ * access policy table. For AAD only.
+ * @param {HTMLElement} td The specific td that will be populated.
+ * @param {number} i The row number (0-indexed, not counting the header row).
+ * @param {number} j The the column number (0-indexed).
+ */
+let fillTruncatedCell = (td, i, j) => {
+    try {
+        const charLimit = 50;
+        let content = "";
+        let truncated = false;
+        if (capColNames[j] === "") {
+            content = ""
+        }
+        else if (caps[i][capColNames[j]].constructor === Array && caps[i][capColNames[j]].length > 1) {
+            content = caps[i][capColNames[j]][0];
+            truncated = true;
+        }
+        else {
+            content = caps[i][capColNames[j]];
         }
 
+        if (content.length > charLimit) {
+            td.innerHTML = content.substring(0, charLimit);
+            truncated = true;
+        }
+        else {
+            td.innerHTML = content;
+        }
+
+        if (truncated) {
+            let span = document.createElement("span");
+            span.appendChild(document.createTextNode("..."));
+            span.title = "Show more";
+            span.rowNumber = i;
+            span.addEventListener("click", expandCAPRow);
+            td.appendChild(span);
+        }
+    }
+    catch (error) {
+        console.error(`Error in fillTruncatedCell, i = ${i}, j = ${j}`);
+        console.error(error);
+    }
+}
+
+/**
+ * Fills in the row of the conditional access policy table indicated by the
+ * event with the truncated version of the row. For AAD only.
+ * @param {HTMLElement} event The target of the event.
+ */
+let hideCAPRow = (event) => {
+    try {
+        let i = event.currentTarget.rowNumber;
+        let tr = document.querySelector("#caps tr:nth-of-type(" + (i+2).toString() + ")"); /*i+2
+        because nth-of-type is indexed from 1 and to account for the header row */
+        for (let j = 0; j < capColNames.length; j++) {
+            let td = tr.querySelector("td:nth-of-type(" + (j+1).toString() + ")");
+            fillTruncatedCell(td, i, j);
+        }
         let img = document.createElement("img");
         img.setAttribute('src', 'images/angle-right-solid.svg');
+        img.style.width = '10px';
         img.setAttribute('alt', 'Show more');
         img.setAttribute('title', 'Show more');
-        img.style.width = '10px';
         img.rowNumber = i;
         img.addEventListener("click", expandCAPRow);
         tr.querySelectorAll('td')[1].appendChild(img);
-        table.appendChild(tr);
+    }
+    catch (error) {
+        console.error("Error in hideCAPRow");
+        console.error(error);
     }
 }
 
-let fillTruncatedCell = (td, i, j) => {
-    /*  For AAD Conditional Access Policies
-        i is the row number (0-indexed, not counting the header row)
-        j is the column number (0-indexed)
-    */
-    const charLimit = 50;
-    let content = "";
-    let truncated = false;
-    if (capColNames[j] === "") {
-        content = ""
-    }
-    else if (caps[i][capColNames[j]].constructor === Array && caps[i][capColNames[j]].length > 1) {
-        content = caps[i][capColNames[j]][0];
-        truncated = true;
-    }
-    else {
-        content = caps[i][capColNames[j]];
-    }
-
-    if (content.length > charLimit) {
-        td.innerHTML = content.substring(0, charLimit);
-        truncated = true;
-    }
-    else {
-        td.innerHTML = content;
-    }
-
-    if (truncated) {
-        let span = document.createElement("span");
-        span.appendChild(document.createTextNode("..."));
-        span.title = "Show more";
-        span.rowNumber = i;
-        span.addEventListener("click", expandCAPRow);
-        td.appendChild(span);
-    }
-}
-
-let hideCAPRow = (event) => {
-    /* For AAD Conditional Access Policies */
-    let i = event.currentTarget.rowNumber;
-    let tr = document.querySelector("#caps tr:nth-of-type(" + (i+2).toString() + ")"); /*i+2
-    because nth-of-type is indexed from 1 and to account for the header row */
-    for (let j = 0; j < capColNames.length; j++) {
-        let td = tr.querySelector("td:nth-of-type(" + (j+1).toString() + ")");
-        fillTruncatedCell(td, i, j);
-    }
-    let img = document.createElement("img");
-    img.setAttribute('src', 'images/angle-right-solid.svg');
-    img.style.width = '10px';
-    img.setAttribute('alt', 'Show more');
-    img.setAttribute('title', 'Show more');
-    img.rowNumber = i;
-    img.addEventListener("click", expandCAPRow);
-    tr.querySelectorAll('td')[1].appendChild(img);
-}
-
+/**
+ * Expands all rows of the conditional access policy table to the full version.
+ * For AAD only.
+ */
 let expandAllCAPs = () => {
-    /* For AAD Conditional Access Policies */
-    let buttons = document.querySelectorAll("img[src*='angle-right-solid.svg']");
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].click();
+    try {
+        let buttons = document.querySelectorAll("img[src*='angle-right-solid.svg']");
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].click();
+        }
+    }
+    catch (error) {
+        console.error("Error in expandAllCAPs");
+        console.error(error);
     }
 }
 
+/**
+ * Shrinks all rows of the conditional access policy table to the truncated
+ * version. For AAD only.
+ */
 let collapseAllCAPs = () => {
-    /* For AAD Conditional Access Policies */
-    let buttons = document.querySelectorAll("img[src*='angle-down-solid.svg']");
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].click();
+    try {
+        let buttons = document.querySelectorAll("img[src*='angle-down-solid.svg']");
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].click();
+        }
+    }
+    catch (error) {
+        console.error("Error in collapseAllCAPs");
+        console.error(error);
     }
 }
 
+/**
+ * Fills in the row of the conditional access policy table indicated by the
+ * event with the full version of the row. For AAD only.
+ * @param {HTMLElement} event The target of the event.
+ */
 let expandCAPRow = (event) => {
-    /* For AAD Conditional Access Policies */
-    let i = event.currentTarget.rowNumber;
-    let tr = document.querySelector("#caps tr:nth-of-type(" + (i+2).toString() + ")"); /*i+2
-    because nth-of-type is indexed from 1 and to account for the header row */
-    for (let j = 0; j < capColNames.length; j++) {
-        let td = tr.querySelector("td:nth-of-type(" + (j+1).toString() + ")");
-        fillTruncatedCell(td, i, j);
-        td.innerHTML = "";
-        if (capColNames[j] === "") {
+    try {
+        let i = event.currentTarget.rowNumber;
+        let tr = document.querySelector("#caps tr:nth-of-type(" + (i+2).toString() + ")"); /*i+2
+        because nth-of-type is indexed from 1 and to account for the header row */
+        for (let j = 0; j < capColNames.length; j++) {
+            let td = tr.querySelector("td:nth-of-type(" + (j+1).toString() + ")");
+            fillTruncatedCell(td, i, j);
             td.innerHTML = "";
-            let img = document.createElement("img");
-            img.setAttribute('src', 'images/angle-down-solid.svg');
-            img.setAttribute('alt', 'Show less');
-            img.setAttribute('title', 'Show less');
-            img.style.width = '14px';
-            img.rowNumber = i;
-            img.addEventListener("click", hideCAPRow);
-            tr.querySelectorAll('td')[1].appendChild(img);
-        }
-        else if (caps[i][capColNames[j]].constructor === Array && caps[i][capColNames[j]].length > 1) {
-            let ul = document.createElement("ul");
-            for (let k = 0; k < caps[i][capColNames[j]].length; k++) {
-                let li = document.createElement("li");
-                li.innerHTML = caps[i][capColNames[j]][k];
-                ul.appendChild(li);
+            if (capColNames[j] === "") {
+                td.innerHTML = "";
+                let img = document.createElement("img");
+                img.setAttribute('src', 'images/angle-down-solid.svg');
+                img.setAttribute('alt', 'Show less');
+                img.setAttribute('title', 'Show less');
+                img.style.width = '14px';
+                img.rowNumber = i;
+                img.addEventListener("click", hideCAPRow);
+                tr.querySelectorAll('td')[1].appendChild(img);
             }
-            td.appendChild(ul);
+            else if (caps[i][capColNames[j]].constructor === Array && caps[i][capColNames[j]].length > 1) {
+                let ul = document.createElement("ul");
+                for (let k = 0; k < caps[i][capColNames[j]].length; k++) {
+                    let li = document.createElement("li");
+                    li.innerHTML = caps[i][capColNames[j]][k];
+                    ul.appendChild(li);
+                }
+                td.appendChild(ul);
+            }
+            else {
+                td.innerHTML = caps[i][capColNames[j]];
+            }
         }
-        else {
-            td.innerHTML = caps[i][capColNames[j]];
-        }
+    }
+    catch (error) {
+        console.error("Error in expandCAPRow");
+        console.error(error);
     }
 }
 
