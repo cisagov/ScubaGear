fc53f3400a818493ab925eb12d8bf9487f62c574
Addam Schroll <aschroll@mitre.org>
2023-02-08 14:53:53 -0600

    Add Teams 2.2/2.3 tenant configs

---
+++ Testing/Functional/TenantConfigs/Teams_2_2_Correct.ps1
@@ -0,0 +1,37 @@
+# Generated with Microsoft365DSC version 1.23.201.1
+# For additional information on how to use Microsoft365DSC, please visit https://aka.ms/M365DSC
+param (
+)
+
+Configuration Teams_2_2_Correct
+{
+    param (
+    )
+
+    Import-DscResource -ModuleName 'Microsoft365DSC'
+
+    Node localhost
+    {
+        TeamsMeetingPolicy Global
+        {
+            Ensure                                     = "Present";
+            AllowAnonymousUsersToStartMeeting          = $False;
+            Identity                                   = "Global";
+            ApplicationId                              = $ConfigurationData.NonNodeData.ApplicationId;
+            TenantId                                   = $ConfigurationData.NonNodeData.TenantId;
+            CertificateThumbprint                      = $ConfigurationData.NonNodeData.CertificateThumbprint;
+        }
+
+        TeamsMeetingPolicy Custom_Policy_1
+        {
+            Ensure                                     = "Present";
+            AllowAnonymousUsersToStartMeeting          = $False;
+            Identity                                   = "Custom Policy 1";
+            ApplicationId                              = $ConfigurationData.NonNodeData.ApplicationId;
+            TenantId                                   = $ConfigurationData.NonNodeData.TenantId;
+            CertificateThumbprint                      = $ConfigurationData.NonNodeData.CertificateThumbprint;
+        }
+    }
+}
+
+Teams_2_2_Correct -ConfigurationData .\ConfigurationData.psd1
---
+++ Testing/Functional/TenantConfigs/Teams_2_3_Correct.ps1
@@ -0,0 +1,39 @@
+# Generated with Microsoft365DSC version 1.23.201.1
+# For additional information on how to use Microsoft365DSC, please visit https://aka.ms/M365DSC
+param (
+)
+
+Configuration Teams_2_3_Correct
+{
+    param (
+    )
+
+    Import-DscResource -ModuleName 'Microsoft365DSC'
+
+    Node localhost
+    {
+        TeamsMeetingPolicy Global
+        {
+            Ensure                                     = "Present";
+            AutoAdmittedUsers                          = "EveryoneInCompanyExcludingGuests";
+            AllowPSTNUsersToBypassLobby                = $False;
+            Identity                                   = "Global";
+            ApplicationId                              = $ConfigurationData.NonNodeData.ApplicationId;
+            TenantId                                   = $ConfigurationData.NonNodeData.TenantId;
+            CertificateThumbprint                      = $ConfigurationData.NonNodeData.CertificateThumbprint;
+        }
+
+        TeamsMeetingPolicy Custom_Policy_1
+        {
+            Ensure                                     = "Present";
+            AutoAdmittedUsers                          = "EveryoneInCompanyExcludingGuests";
+            AllowPSTNUsersToBypassLobby                = $False;
+            Identity                                   = "Custom Policy 1";
+            ApplicationId                              = $ConfigurationData.NonNodeData.ApplicationId;
+            TenantId                                   = $ConfigurationData.NonNodeData.TenantId;
+            CertificateThumbprint                      = $ConfigurationData.NonNodeData.CertificateThumbprint;
+        }
+    }
+}
+
+Teams_2_3_Correct -ConfigurationData .\ConfigurationData.psd1
