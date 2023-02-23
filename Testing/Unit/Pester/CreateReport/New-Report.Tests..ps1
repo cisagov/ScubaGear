0e720c26e67f5729faaebb34af2c3ac581568f98
buidav <dbui@sandia.gov>
2023-02-23 12:23:50 -0800

    mocked CreateReport

--- Testing/Unit/Pester/CreateReport/New-Report.Tests..ps1
+++ Testing/Unit/Pester/CreateReport/New-Report.Tests..ps1
@@ -1,39 +1,47 @@
 BeforeAll {
     Import-Module ../../../../PowerShell/ScubaGear/Modules/CreateReport
+    New-Item -Path "./CreateReportStubs" -Name "CreateReportUnitFolder" -ErrorAction SilentlyContinue -ItemType Directory | Out-Null
+    New-Item -Path "./CreateReportStubs/CreateReportUnitFolder" -Name "IndividualReports" -ErrorAction SilentlyContinue -ItemType Directory | Out-Null
 }
 
 Describe 'New-Report' {
-    It 'Given 4 parameters, creates an HTML report' {
+    It 'Given all of the ProductNames, creates an HTML report for each of them' {
         $ProductNames = @("teams", "exo", "defender", "aad", "powerplatform", "sharepoint", "onedrive")
         $ProdToFullName = @{
-                Teams = "Microsoft Teams";
-                EXO = "Exchange Online";
-                Defender = "Microsoft 365 Defender";
-                AAD = "Azure Active Directory";
-                PowerPlatform = "Microsoft Power Platform";
-                SharePoint = "SharePoint Online";
-                OneDrive = "OneDrive for Business";
-            }
+            Teams = "Microsoft Teams";
+            EXO = "Exchange Online";
+            Defender = "Microsoft 365 Defender";
+            AAD = "Azure Active Directory";
+            PowerPlatform = "Microsoft Power Platform";
+            SharePoint = "SharePoint Online";
+            OneDrive = "OneDrive for Business";
+        }
         $ArgToProd = @{
-                teams = "Teams";
-                exo = "EXO";
-                defender = "Defender";
-                aad = "AAD";
-                powerplatform = "PowerPlatform";
-                sharepoint = "SharePoint";
-                onedrive = "OneDrive";
-            }
-        foreach ($N in $ProductNames){
+            teams = "Teams";
+            exo = "EXO";
+            defender = "Defender";
+            aad = "AAD";
+            powerplatform = "PowerPlatform";
+            sharepoint = "SharePoint";
+            onedrive = "OneDrive";
+        }
+        $IndividualReportPath = "./CreateReportStubs/CreateReportUnitFolder/IndividualReports"
+        foreach ($N in $ProductNames) {
             $CreateReportParams = @{
-                'BaselineName' = $ArgToProd[$N];
-                'FullName' = $ProdToFullName[$N];
-                'IndividualReportPath' = "./";
-                'OutPath' = $PSScriptRoot;
-                'OutProviderFileName' = "ProviderSettingsExport";
-                'OutRegoFileName' = "TestResults";
+                'BaselineName'         = $ArgToProd[$N];
+                'FullName'             = $ProdToFullName[$N];
+                'IndividualReportPath' = $IndividualReportPath;
+                'OutPath'              = "./CreateReportStubs";
+                'OutProviderFileName'  = "ProviderSettingsExport";
+                'OutRegoFileName'      = "TestResults";
             }
             New-Report @CreateReportParams
-            Test-Path -Path "./$($N)Report.html" -PathType leaf | Should -Be $true
+            Test-Path -Path "$($IndividualReportPath)/$($N)Report.html" -PathType leaf | Should -Be $true
         }
     }
+}
+
+AfterAll {
+    Remove-Module CreateReport -ErrorAction SilentlyContinue
+    Remove-Item -Recurse -Force -Path "./CreateReportStubs/CreateReportUnitFolder" -ErrorAction SilentlyContinue
 }
