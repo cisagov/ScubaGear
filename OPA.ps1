68525c012adc54432c98c6d492a79e86d10af9c4
Dylan Gao <107067015+Dylan-MITRE@users.noreply.github.com>
2023-03-08 15:24:12 -0500

    Change write-host

--- OPA.ps1
+++ OPA.ps1
@@ -8,18 +8,23 @@
     .EXAMPLE
         .\OPA.ps1
 #>
+# Set prefernces for writing messages
+$DebugPreference = "Continue"
+$InformationPreference = "Continue"
 
-$Expected_version = "v0.42.1"
-$Install_url= "https://openpolicyagent.org/downloads/$($Expected_version)/opa_windows_amd64.exe"
+# Set expected version and OutFile path
+$Expected_version = "0.42.1"
+$Install_url= "https://openpolicyagent.org/downloads/v$($Expected_version)/opa_windows_amd64.exe"
 $OutFile=(Join-Path $pwd.Path $Install_url.SubString($Install_url.LastIndexOf('/')))
 
-Write-Host "Downloading $Install_url`n" -ForegroundColor DarkGreen;
+# Download files
+Write-Information "Downloading $Install_url`n"
 $uri=New-Object "System.Uri" "$Install_url"
 $request=[System.Net.HttpWebRequest]::Create($uri)
 $request.set_Timeout(5000)
 $response=$request.GetResponse()
-$totalLength=[System.Math]::Floor($response.get_ContentLength()/1024)
-$length=$response.get_ContentLength()
+#$totalLength=[System.Math]::Floor($response.get_ContentLength()/1024)
+#$length=$response.get_ContentLength()
 $responseStream=$response.GetResponseStream()
 $destStream=New-Object -TypeName System.IO.FileStream -ArgumentList $OutFile, Create
 $buffer=New-Object byte[] 10KB
@@ -28,23 +33,24 @@ $downloadedBytes=$count
 while ($count -gt 0)
     {
     [System.Console]::CursorLeft=0
-    [System.Console]::Write("Downloaded {0}K of {1}K ({2}%)", [System.Math]::Floor($downloadedBytes/1024), $totalLength, [System.Math]::Round(($downloadedBytes / $length) * 100,0))
+#    [System.Console]::Write("Downloaded {0}K of {1}K ({2}%)", [System.Math]::Floor($downloadedBytes/1024), $totalLength, [System.Math]::Round(($downloadedBytes / $length) * 100,0))
     $destStream.Write($buffer, 0, $count)
     $count=$responseStream.Read($buffer,0,$buffer.length)
     $downloadedBytes+=$count
     }
-Write-Host ""
-Write-Host "`nDownload of `"$OutFile`" finished." -ForegroundColor DarkGreen;
+Write-Information ""
+Write-Information "`nDownload of `"$OutFile`" finished."
 $destStream.Flush()
 $destStream.Close()
 $destStream.Dispose()
 $responseStream.Dispose()
 
+# Version checks
 $Installed_Version= .\opa_windows_amd64.exe version | Select-Object -First 1
-if ($Installed_Version = "Version: $($Expected_version)")
+if ($Installed_Version -eq "Version: $($Expected_version)")
     {
-    Write-Host "`nDonloaed OPA version` `"$Installed_Version`" meets the ScubaGear Requirement" -ForegroundColor DarkGreen;
+    Write-Information "`nDownloaed OPA version` `"$Installed_Version`" meets the ScubaGear Requirement"
     }
 else {
-    Write-Host "`nDonloaed OPA version` `"$Installed_Version`" does not meet the ScubaGear Requirement of` `"$Expected_version`"" -ForegroundColor Red;
+    Write-Information "`nDownloaed OPA version` `"$Installed_Version`" does not meet the ScubaGear Requirement of` `"$Expected_version`""
 }
