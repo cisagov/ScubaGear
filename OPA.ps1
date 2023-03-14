2f2d51be8b2ea0951af1d145a9e6a5f8242f8040
Dylan Gao <107067015+Dylan-MITRE@users.noreply.github.com>
2023-03-14 15:57:07 -0400

    change pwd to Get-location
    Co-authored-by: buidav <105074908+buidav@users.noreply.github.com>

--- OPA.ps1
+++ OPA.ps1
@@ -16,7 +16,7 @@ $ErrorActionPreference = "Stop"
 # Set expected version and OutFile path
 $ExpectedVersion = "0.42.1"
 $InstallUrl= "https://openpolicyagent.org/downloads/v$($ExpectedVersion)/opa_windows_amd64.exe"
-$OutFile=(Join-Path $pwd.Path $InstallUrl.SubString($InstallUrl.LastIndexOf('/')))
+$OutFile=(Join-Path (Get-Location).Path $InstallUrl.SubString($InstallUrl.LastIndexOf('/')))
 $ExpectedHash ="5D71028FED935DC98B9D69369D42D2C03CE84A7720D61ED777E10AAE7528F399"
 
 # Download files
