1b7e4132c6b685e7d7e4e38b910f8cded5101b4d
Ted Kolovos <tkolovos@mitre.org>
2023-03-29 14:21:52 -0400

    removed test files

--- temp.ps1
+++
@@ -1,7 +0,0 @@
-
-$ReportFolders = Get-ChildItem . -directory -Filter "M365BaselineConformance*" | Sort-Object -Property LastWriteTime -Descending
-$OutputFolder = $ReportFolders[0]
-$OutputFolder
-
-$Jsonfile = ".\$OutputFolder\TestResults.json"
-$Jsonfile
