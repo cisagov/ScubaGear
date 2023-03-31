a5077e9fafbf4f0e092b2534222f186cdfb9a99d
Richard Crutchfield <crutchfield@mitre.org>
2023-03-31 11:51:42 -0400

    wip

--- Testing/Functional/SmokeTest/SmokeTestInit.ps1
+++ Testing/Functional/SmokeTest/SmokeTestInit.ps1
@@ -11,9 +11,5 @@ function New-ServicePrincipalCertificate{
 
     Set-Content -Path .\ScubaExecutionCert.txt -Value $EncodedCertificate
     certutil -decode .\ScubaExecutionCert.txt .\ScubaExecutionCert.pfx
-    $Certificate = Import-PfxCertificate -FilePath .\ScubaExecutionCert.pfx -CertStoreLocation Cert:\CurrentUser\My -Password $CertificatePassword
-
-    if (-Not $Certificate.Verify()){
-        throw "Import of service principal certificate failed."
-    }
+    Import-PfxCertificate -FilePath .\ScubaExecutionCert.pfx -CertStoreLocation Cert:\CurrentUser\My -Password $CertificatePassword
 }
