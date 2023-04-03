3f207c36b79aecbac1f45655e34bdb41b8f47f67
Richard Crutchfield <crutchfield@mitre.org>
2023-04-03 13:23:37 -0400

    wip

---
+++ Testing/Functional/SmokeTest/SmokeTest002.Tests.ps1
@@ -0,0 +1,91 @@
+BeforeAll {
+    $ScubaModulePath = Join-Path -Path $PSScriptRoot -ChildPath "../../../PowerShell/ScubaGear/ScubaGear.psd1"
+    Import-Module $ScubaModulePath
+    Import-Module Selenium
+   }
+
+Describe "Smoke Test <Config>" -ForEach @(
+    @{ConfigPath = "SmokeTestConfig001.yaml"; ExpectedResults = "SmokeTestExpected001.json"}
+){
+    It "Config: <ConfigPath>; Expected: <ExpectedResults>" {
+        Invoke-SCuBA -CertificateThumbprint $Env:Thumbprint -AppID $Env:AppId -Organization $Env:Organization -ProductNames "onedrive" -M365Environment "gcc"
+        $ReportFolders = Get-ChildItem . -directory -Filter "M365BaselineConformance*" | Sort-Object -Property LastWriteTime -Descending
+        $OutputFolder = $ReportFolders[0]
+        Test-Path -Path ".\$OutputFolder\TestResults.json" -PathType Leaf |
+            Should -Be $true
+	}
+}
+
+Describe -Tag "UI","Chrome" -Name "Test Report with <Browser>" -ForEach @(
+    @{ Browser = "Chrome"; Driver = Start-SeChrome 2>$null }
+){
+	BeforeAll {
+        $ReportFolders = Get-ChildItem . -directory -Filter "M365BaselineConformance*" | Sort-Object -Property LastWriteTime -Descending
+        $OutputFolder = $ReportFolders[0]
+        $script:url = "file:///$OutputFolder/BaselineReports.html"
+        Enter-SeUrl $script:url -Driver $Driver 2>$null
+	}
+
+	It "Toggle Dark Mode" {
+        $ToggleCheckbox = Find-SeElement -Driver $Driver -Wait -By XPath "//input[@id='toggle']"
+        $ToggleText = Find-SeElement -Driver $Driver -Wait -Id "toggle-text"
+
+        $ToggleCheckbox.Selected | Should -Be $false
+        $ToggleText.Text | Should -Be 'Light Mode'
+
+        $ToggleSwitch = Find-SeElement -Driver $Driver -Wait -ClassName "switch"
+        Invoke-SeClick -Element $ToggleSwitch
+
+        $ToggleText.Text | Should -Be 'Dark Mode'
+        $ToggleCheckbox.Selected | Should -Be $true
+	}
+
+    It "Verify Tenant"{
+        $TenantDataElement = Find-SeElement -Driver $Driver -Wait -ClassName "tenantdata"
+        $TenantDataRows = Find-SeElement -Target $TenantDataElement -By TagName "tr"
+        $TenantDataColumns = Find-SeElement -Target $TenantDataRows[1] -By TagName "td"
+        $Tenant = $TenantDataColumns[0].Text
+        $Tenant | Should -Be "Cybersecurity and Infrastructure Security Agency" -Because $Tenant
+    }
+
+    It "Verify  Domain"{
+        $TenantDataElement = Find-SeElement -Driver $Driver -Wait -ClassName "tenantdata"
+        $TenantDataRows = Find-SeElement -Target $TenantDataElement -By TagName "tr"
+        $TenantDataColumns = Find-SeElement -Target $TenantDataRows[1] -By TagName "td"
+        $Domain = $TenantDataColumns[1].Text
+        $Domain | Should -Be "cisaent.onmicrosoft.com" -Because "Domain is $Domain"
+    }
+
+    It "Goto <Product> (<LinkText>) details" -ForEach @(
+        @{Product = "aad"; LinkText = "Azure Active Directory"}
+        @{Product = "onedrive"; LinkText = "OneDrive for Business"}
+    ){
+        $DetailLink = Find-SeElement -Driver $Driver -Wait -By LinkText $LinkText
+        $DetailLink | Should -Not -BeNullOrEmpty
+        Invoke-SeClick -Element $DetailLink
+
+        $ToggleCheckbox = Find-SeElement -Driver $Driver -Wait -By XPath "//input[@id='toggle']"
+        $ToggleText = Find-SeElement -Driver $Driver -Wait -Id "toggle-text"
+
+        $ToggleText.Text | Should -Be 'Dark Mode'
+        $ToggleCheckbox.Selected | Should -Be $true
+
+        $ToggleSwitch = Find-SeElement -Driver $Driver -Wait -ClassName "switch"
+        Invoke-SeClick -Element $ToggleSwitch
+
+        $ToggleText.Text | Should -Be 'Light Mode'
+        $ToggleCheckbox.Selected | Should -Be $false
+    }
+
+    It "Go Back to main page - Is Dark mode in correct state"{
+        Open-SeUrl -Back -Driver $Driver
+        $ToggleCheckbox = Find-SeElement -Driver $Driver -Wait -By XPath "//input[@id='toggle']"
+        $ToggleText = Find-SeElement -Driver $Driver -Wait -Id "toggle-text"
+        $ToggleText.Text | Should -Be 'Light Mode'
+        $ToggleCheckbox.Selected | Should -Be $false
+    }
+
+	AfterAll {
+		Stop-SeDriver -Driver $Driver 2>$null
+	}
+}
