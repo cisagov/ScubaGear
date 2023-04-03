c17afefb594fe6a8f553d234797e621c31fa8596
Richard Crutchfield <crutchfield@mitre.org>
2023-04-03 13:39:42 -0400

    wip

--- Testing/Functional/SmokeTest/UpdateSelenium.ps1
+++ Testing/Functional/SmokeTest/UpdateSelenium.ps1
@@ -1,25 +1,12 @@
-<#
-    Script autmating update of Selenium Web Driver for MS Edge and Chrome browsers.
-    Created by Wiktor Mrowczynski.
-    v.2022.06.22.A
-#>
-#region INITIALIZATION
 param (
     $registryRoot        = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths",             # root location in registry to check version of currently installed apps
-    $edgeRegistryPath    = "$registryRoot\msedge.exe",                                              # direct registry location for MS Edge (to check version)
     $chromeRegistryPath  = "$registryRoot\chrome.exe",                                              # direct registry location for Chrome (to check version)
     $webDriversPath      = "C:\Program Files\WindowsPowerShell\Modules\Selenium\3.0.1\assemblies",  # local path for all web drivers (assuming that both are in the same location)
-    $edgeDriverPath      = "$($webDriversPath)\msedgedriver.exe",                                   # direct MS Edge driver path
     $chromeDriverPath    = "$($webDriversPath)\chromedriver.exe",                                   # direct Chrome driver path
     $chromeDriverWebsite = "https://chromedriver.chromium.org/downloads",                           # Chrome dooesn't allow to query the version from downloads page; instead available pages can be found here
     $chromeDriverUrlBase = "https://chromedriver.storage.googleapis.com",                           # URL base to ubild direct download link for Chrome driver
     $chromeDriverUrlEnd  = "chromedriver_win32.zip",                                                # Chrome driver download ending (to finish building the URL)
-    $edgeDriverWebsite   = "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/"  # URL to find and download relevant MS Edge Driver version
 )
-#endregion INITIALIZATION
-
-#region FUNCTIONS
-# function checking driver version using the -v switch of each driver
 function Get-LocalDriverVersion{
     param(
         $pathToDriver                                               # direct path to the driver
@@ -38,76 +25,28 @@ function Get-LocalDriverVersion{
     $process.WaitForExit()                                          # run synchronously, we need to wait for result
     $processStOutput    = $process.StandardOutput.ReadToEnd()
 
-    if ($pathToDriver.Contains("msedgedriver")){
-        return ($processStOutput -split " ")[1]                     # MS Edge returns version on 4th place in the output (be carefulm in old versions it was on 1st as well)...
-    }
-    else {
-        return ($processStOutput -split " ")[1]                     # ... while Chrome on 2nd place
-    }
+    return ($processStOutput -split " ")[1]                     # ... while Chrome on 2nd place
 }
 
-# function evaluating a need for update
 function Confirm-NeedForUpdate{
     param(
-        $v1,   # version 1 to compare
-        $v2    # version 2 to compare
+        $v1,
+        $v2
     )
     Write-Debug -Message "v1: $v1; v2: $v2"
     return ([System.Version]$v2).Major -lt ([System.Version]$v1).Major
 }
-#endregion FUNCTIONS
 
-#region MAIN SCRIPT
 $DebugPreference = 'Continue'
 #$DebugPreference = 'SilentlyContinue'
 
 # firstly check which browser versions are installed (from registry)
-$edgeVersion   = (Get-Item (Get-ItemProperty $edgeRegistryPath).'(Default)').VersionInfo.ProductVersion
 $chromeVersion = (Get-Item (Get-ItemProperty $chromeRegistryPath).'(Default)').VersionInfo.ProductVersion
-Write-Debug -Message "Edge driver version(registry):  $edgeVersion"
 Write-Debug -Message "Chrome driver version(registery):  $chromeVersion"
 
 # check which driver versions are installed
-Write-Debug -Message "Edge driver path:  $edgeDriverPath"
-Write-Debug -Message "Chrome driver path:  $chromeDriverPath"
-Write-Debug -Message "$(Get-ChildItem $webDriversPath -Recurse)"
-$edgeDriverVersion   = Get-LocalDriverVersion -pathToDriver $edgeDriverPath
 $chromeDriverVersion = Get-LocalDriverVersion -pathToDriver $chromeDriverPath
-Write-Debug -Message "Edge driver version(file system):  $edgeDriverVersion"
-Write-Debug -Message "Chrome driver version(file system):  $chromeDriverVersion"
 
-# download new MS Edge driver if neccessary
-if (Confirm-NeedForUpdate $edgeVersion $edgeDriverVersion){
-    Write-Debug -Message "Need to update edge driver from $edgeDriverVersion to $edgeVersion"
-
-    # find exact matching version
-    $edgeDriverAvailableVersions = (Invoke-RestMethod $edgeDriverWebsite) -split " " | Where-Object {$_ -like "*href=*win64*"} | ForEach-Object {$_.replace("href=","").replace('"','')}
-    $downloadLink                = $edgeDriverAvailableVersions | Where-Object {$_ -like "*/$edgeVersion/*"}
-
-    # if cannot find (e.g. it's too new to have a web driver), look for relevant major version
-    if (!$downloadLink){
-        $browserMajorVersion = $edgeVersion.Substring(0, $edgeVersion.IndexOf("."))
-        $downloadLink        = $edgeDriverAvailableVersions | Where-Object {$_ -like "*/$browserMajorVersion*"}
-    }
-
-    # in case of multiple links, take the first only
-    if ($downloadLink.Count -gt 1) {
-        $downloadLink = $downloadLink[0]
-    }
-
-    # download the file
-    Invoke-WebRequest $downloadLink -OutFile "edgeNewDriver.zip"
-
-    # epand archive and replace the old file
-    Expand-Archive "edgeNewDriver.zip"              -DestinationPath "edgeNewDriver\"                      -Force
-    Move-Item      "edgeNewDriver/msedgedriver.exe" -Destination     "$($webDriversPath)\msedgedriver.exe" -Force
-
-    # clean-up
-    Remove-Item "edgeNewDriver.zip" -Force
-    Remove-Item "edgeNewDriver"     -Recurse -Force
-}
-
-# download new Chrome driver if neccessary
 if (Confirm-NeedForUpdate $chromeVersion $chromeDriverVersion){
     Write-Debug -Message "Need to update chrome driver from $chromeDriverVersion to $chromeVersion"
 
