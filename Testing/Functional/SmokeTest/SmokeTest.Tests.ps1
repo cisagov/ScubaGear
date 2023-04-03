a020bcb1752a0f2b0a9fb738dfd79c4b78ee6b9e
Richard Crutchfield <crutchfield@mitre.org>
2023-04-03 09:48:10 -0400

    wip

--- Testing/Functional/SmokeTest/SmokeTest.Tests.ps1
+++ Testing/Functional/SmokeTest/SmokeTest.Tests.ps1
@@ -1,11 +1,14 @@
 BeforeAll {
     $ScubaModulePath = Join-Path -Path $PSScriptRoot -ChildPath "../../../PowerShell/ScubaGear/ScubaGear.psd1"
     Import-Module $ScubaModulePath
-    Invoke-SCuBA -CertificateThumbprint $Env:Thumbprint -AppID $Env:AppId -Organization $Env:Organization -ProductNames "onedrive" -M365Environment "gcc"
-}
+   }
 
-Describe "Valid Json" {
-    It "returns 'None' when no users are included" {
+Describe "Smoke Test <Config>" -ForEach @(
+    @{ConfigPath = "SmokeTestConfig001.yaml"; ExpectedResults = "SmokeTestExpected001.json"},
+    @{ConfigPath = "SmokeTestConfig002.yaml"; ExpectedResults = "SmokeTestExpected002.json"}
+){
+    It "Config: <ConfigPath>; Expected: <ExpectedResults>" {
+        Invoke-SCuBA -CertificateThumbprint $Env:Thumbprint -AppID $Env:AppId -Organization $Env:Organization -ProductNames "onedrive" -M365Environment "gcc"
         $ReportFolders = Get-ChildItem . -directory -Filter "M365BaselineConformance*" | Sort-Object -Property LastWriteTime -Descending
         $OutputFolder = $ReportFolders[0]
         Test-Path -Path ".\$OutputFolder\TestResults.json" -PathType Leaf |
