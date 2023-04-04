5f09087fc6895ee71b7766bf039c848eeeed6cc2
buidav <105074908+buidav@users.noreply.github.com>
2023-04-04 09:25:11 -0700

    Add data driven approach to exo auth module unit test
    Co-authored-by: Richard Crutchfield <crutchfield@users.noreply.github.com>

--- Testing/Unit/PowerShell/Connection/Connect-ExoHelper.Tests.ps1
+++ Testing/Unit/PowerShell/Connection/Connect-ExoHelper.Tests.ps1
@@ -1,21 +1,17 @@
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "../../../../PowerShell/ScubaGear/Modules/Connection/ConnectHelpers.psm1") -Function 'Connect-EXOHelper' -Force
 
 InModuleScope ConnectHelpers {
-    Describe -Tag 'Connection' -Name 'Connect-EXOHelper' {
+    Describe -Tag 'Connection' -Name 'Connect-EXOHelper' -ForEach @(
+        @{Endpoint = 'commercial'}
+        @{Endpoint = 'gcc'}
+        @{Endpoint = 'gcchigh'}
+        @{Endpoint = 'dod'}
+    ){
         BeforeAll {
             Mock Connect-ExchangeOnline -MockWith {}
         }
-        It 'When connecting interactively to commercial endpoint, connects to Exchange Online' {
-            {Connect-EXOHelper -M365Environment 'commercial'} | Should -Not -Throw
-        }
-        It 'When connecting interactively to GCC endpoint, connects to Exchange Online' {
-            {Connect-EXOHelper -M365Environment 'gcc'} | Should -Not -Throw
-        }
-        It 'When connecting interactively to GCC High endpoint, connects to Exchange Online' {
-            {Connect-EXOHelper -M365Environment 'gcchigh'} | Should -Not -Throw
-        }
-        It 'When connecting interactively to DOD endpoint, connects to Exchange Online' {
-            {Connect-EXOHelper -M365Environment 'dod'} | Should -Not -Throw
+        It 'When connecting interactively to <Endpoint> endpoint, connects to Exchange Online' {
+            {Connect-EXOHelper -M365Environment $Endpoint} | Should -Not -Throw
         }
     }
 }
