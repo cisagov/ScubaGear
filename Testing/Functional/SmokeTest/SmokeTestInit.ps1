0b064e10eb8736a298448907c7616187aa22f7ec
Richard Crutchfield <crutchfield@mitre.org>
2023-04-12 12:37:07 -0400

    wip

--- Testing/Functional/SmokeTest/SmokeTestInit.ps1
+++ Testing/Functional/SmokeTest/SmokeTestInit.ps1
@@ -13,6 +13,8 @@ function New-ServicePrincipalCertificate{
     certutil -decode .\ScubaExecutionCert.txt .\ScubaExecutionCert.pfx
     $Certificate = Import-PfxCertificate -FilePath .\ScubaExecutionCert.pfx -CertStoreLocation Cert:\CurrentUser\My -Password $CertificatePassword
     $Thumbprint = ([System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate).Thumbprint
+    Remove-Item -Path .\ScubaExecutionCert.txt
+    Remove-Item -Path .\ScubaExecutionCert.pfx
     return $Thumbprint
 }
 
