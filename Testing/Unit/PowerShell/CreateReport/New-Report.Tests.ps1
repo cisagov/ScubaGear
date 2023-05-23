41253abf53950092559541b07546459b6350af8b
Richard Crutchfield <crutchfield@mitre.org>
2023-05-23 16:41:00 -0400

    Update UT for NewReport

--- Testing/Unit/PowerShell/CreateReport/New-Report.Tests.ps1
+++ Testing/Unit/PowerShell/CreateReport/New-Report.Tests.ps1
@@ -1,67 +1,67 @@
-BeforeAll {
-    Import-Module (Join-Path -Path $PSScriptRoot -ChildPath '../../../../PowerShell/ScubaGear/Modules/CreateReport')
-    New-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath "./CreateReportStubs") -Name "CreateReportUnitFolder" -ErrorAction SilentlyContinue -ItemType Directory | Out-Null
-    New-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath "./CreateReportStubs/CreateReportUnitFolder") -Name "IndividualReports" -ErrorAction SilentlyContinue -ItemType Directory | Out-Null
-}
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath '../../../../PowerShell/ScubaGear/Modules/CreateReport')
 
-Describe -Tag CreateReport -Name 'New-Report' {
-    BeforeAll {
-        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ProductNames')]
-        $ProductNames = @("teams", "exo", "defender", "aad", "powerplatform", "sharepoint", "onedrive")
-        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ArgToProd')]
-        $ArgToProd = @{
-            teams         = "Teams";
-            exo           = "EXO";
-            defender      = "Defender";
-            aad           = "AAD";
-            powerplatform = "PowerPlatform";
-            sharepoint    = "SharePoint";
-            onedrive      = "OneDrive";
-        }
-        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ProdToFullName')]
-        $ProdToFullName = @{
-            Teams         = "Microsoft Teams";
-            EXO           = "Exchange Online";
-            Defender      = "Microsoft 365 Defender";
-            AAD           = "Azure Active Directory";
-            PowerPlatform = "Microsoft Power Platform";
-            SharePoint    = "SharePoint Online";
-            OneDrive      = "OneDrive for Business";
-        }
-        $IndividualReportPath = (Join-Path -Path $PSScriptRoot -ChildPath "./CreateReportStubs/CreateReportUnitFolder/IndividualReports")
-        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'CreateReportParams')]
-        $CreateReportParams = @{
-            'IndividualReportPath' = $IndividualReportPath;
-            'OutPath'              = (Join-Path -Path $PSScriptRoot -ChildPath "./CreateReportStubs");
-            'OutProviderFileName'  = "ProviderSettingsExport";
-            'OutRegoFileName'      = "TestResults";
-            'DarkMode'             = $false;
-        }
-        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'SecureBaselines')]
-        $SecureBaselines =  Import-SecureBaseline
-    }
-    It 'Creates a report for <Product>' -ForEach @(
-        @{Product = 'aad'; ErrorCount = 33},
-        @{Product = 'defender'; ErrorCount = 46},
-        @{Product = 'exo'; ErrorCount = 39},
-        @{Product = 'onedrive'; ErrorCount = 8},
-        @{Product = 'powerplatform'; ErrorCount = 8},
-        @{Product = 'sharepoint'; ErrorCount = 6},
-        @{Product = 'teams'; ErrorCount = 28}
-    ){
-        $CreateReportParams += @{
-            'BaselineName' = $ArgToProd[$Product];
-            'FullName'     = $ProdToFullName[$Product];
-            'SecureBaselines' = $SecureBaselines
-        }
-        New-Report @CreateReportParams -ErrorVariable Err 2>&1 > $null
-        $error.Clear() # Clearing the Write-Error messages for excepting missing test results; Otherwise Pester registers as failure
-        $Err.Count | Should -BeExactly $ErrorCount
-        Test-Path -Path "$($IndividualReportPath)/$($ArgToProd[$Product])Report.html" -PathType leaf | Should -Be $true
-    }
-}
+InModuleScope CreateReport {
+    Describe -Tag CreateReport -Name 'New-Report' {
+        BeforeAll {
+            Mock -CommandName Write-Error {}
+            New-Item -Path (Join-Path -Path "TestDrive:" -ChildPath "CreateReportStubs") -Name "CreateReportUnitFolder" -ItemType Directory
+            New-Item -Path (Join-Path -Path "TestDrive:" -ChildPath "CreateReportStubs/CreateReportUnitFolder") -Name "IndividualReports" -ItemType Directory
+            $TestOutPath = (Join-Path -Path "TestDrive:" -ChildPath "CreateReportStubs")
+            Copy-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath "CreateReportStubs/*") -Destination $TestOutPath -Recurse
 
-AfterAll {
-    Remove-Module CreateReport -ErrorAction SilentlyContinue
-    Remove-Item -Recurse -Force -Path (Join-Path -Path $PSScriptRoot -ChildPath "./CreateReportStubs/CreateReportUnitFolder") -ErrorAction SilentlyContinue
+            [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ArgToProd')]
+            $ArgToProd = @{
+                teams         = "Teams";
+                exo           = "EXO";
+                defender      = "Defender";
+                aad           = "AAD";
+                powerplatform = "PowerPlatform";
+                sharepoint    = "SharePoint";
+                onedrive      = "OneDrive";
+            }
+            [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ProdToFullName')]
+            $ProdToFullName = @{
+                Teams         = "Microsoft Teams";
+                EXO           = "Exchange Online";
+                Defender      = "Microsoft 365 Defender";
+                AAD           = "Azure Active Directory";
+                PowerPlatform = "Microsoft Power Platform";
+                SharePoint    = "SharePoint Online";
+                OneDrive      = "OneDrive for Business";
+            }
+            $IndividualReportPath = (Join-Path -Path "TestDrive:" -ChildPath "CreateReportStubs/CreateReportUnitFolder/IndividualReports")
+            [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'CreateReportParams')]
+            $CreateReportParams = @{
+                'IndividualReportPath' = $IndividualReportPath
+                'OutPath'              = $TestOutPath
+                'OutProviderFileName'  = "ProviderSettingsExport"
+                'OutRegoFileName'      = "TestResults"
+                'DarkMode'             = $false
+                'SecureBaselines'      = Import-SecureBaseline
+            }
+        }
+        It 'Creates a report for <Product>' -ForEach @(
+            @{Product = 'aad'; ErrorCount = 1},
+            @{Product = 'defender'; ErrorCount = 3},
+            @{Product = 'exo'; ErrorCount = 2},
+            @{Product = 'onedrive'; ErrorCount = 8},
+            @{Product = 'powerplatform'; ErrorCount = 0},
+            @{Product = 'sharepoint'; ErrorCount = 3},
+            @{Product = 'teams'; ErrorCount = 5}
+        ){
+            $CreateReportParams += @{
+                'BaselineName' = $ArgToProd[$Product];
+                'FullName'     = $ProdToFullName[$Product];
+            }
+
+            { New-Report @CreateReportParams } | Should -Not -Throw
+
+            Should -Invoke -CommandName Write-Error -Exactly -Times $ErrorCount
+            Test-Path -Path "$($IndividualReportPath)/$($ArgToProd[$Product])Report.html" -PathType leaf | Should -Be $true
+        }
+    }
+
+    AfterAll {
+        Remove-Module CreateReport -ErrorAction SilentlyContinue
+    }
 }
