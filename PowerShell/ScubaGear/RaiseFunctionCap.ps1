bb7067e72547764fe6594fce6192d21781765cdf
buidav <105074908+buidav@users.noreply.github.com>
2023-08-31 20:07:14 -0700

    suppress at script param level

--- PowerShell/ScubaGear/RaiseFunctionCap.ps1
+++ PowerShell/ScubaGear/RaiseFunctionCap.ps1
@@ -1,8 +1,11 @@
+[CmdletBind()]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars')]
+param()
 # Increase PowerShell Maximum Function Count to support version 5.1 limitation
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'MaximumFunctionCount')]
 $MaximumFunctionCount = 32767
 
 # Fallback in case the first assignment doesn't execute for some latent reasons
 # PSScriptAnalyzer also is not supressing this warning even with this attribute
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', 'Global:MaximumFunctionCount', Scope='Function')]
 $Global:MaximumFunctionCount = 32767
+
