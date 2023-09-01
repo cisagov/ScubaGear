6cfb1a7c4f349d95855964ccd528f352e9ae34e7
buidav <105074908+buidav@users.noreply.github.com>
2023-08-31 19:28:13 -0700

    specify Global:

--- PowerShell/ScubaGear/RaiseFunctionCap.ps1
+++ PowerShell/ScubaGear/RaiseFunctionCap.ps1
@@ -3,5 +3,5 @@
 $MaximumFunctionCount = 32767
 
 # Fallback in case the first assignment doesn't execute for some latent reasons
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', 'MaximumFunctionCount')]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', 'Global:MaximumFunctionCount')]
 $Global:MaximumFunctionCount = 32767
