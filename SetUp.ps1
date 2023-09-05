54f46782ac9f513f2f24a504c05f34f61da70fce
Zafer Balkan <zafer@zaferbalkan.com>
2023-09-05 23:15:44 +0300

    Updated setup to consider only approved packages

--- SetUp.ps1
+++ SetUp.ps1
@@ -6,8 +6,8 @@
     .DESCRIPTION
         Installs the modules required to support SCuBAGear.  If the Force
         switch is set then any existing module will be re-installed even if
-        already at latest version. If the SkipUpdate switch is set then any
-        existing module will not be updated to th latest version.
+        already at the latest version. If the SkipUpdate switch is set then any
+        existing module will not be updated to the latest version.
     .EXAMPLE
         .\Setup.ps1
     .NOTES
@@ -21,10 +21,6 @@ param(
     [switch]
     $Force,
 
-    [Parameter(HelpMessage = 'If specified then modules will not be updated to latest version')]
-    [switch]
-    $SkipUpdate,
-
     [Parameter(HelpMessage = 'Do not automatically trust the PSGallery repository for module installation')]
     [switch]
     $DoNotAutoTrustRepository,
@@ -35,97 +31,91 @@ param(
 )
 
 # Set preferences for writing messages
-$DebugPreference = "Continue"
-$InformationPreference = "Continue"
+$DebugPreference = 'Continue'
+$InformationPreference = 'Continue'
 
-if (-not $DoNotAutoTrustRepository) {
-    $Policy = Get-PSRepository -Name "PSGallery" | Select-Object -Property -InstallationPolicy
+if (-not $DoNotAutoTrustRepository)
+{
+    $Policy = Get-PSRepository -Name 'PSGallery' | Select-Object -Property -InstallationPolicy
 
-    if ($($Policy.InstallationPolicy) -ne "Trusted") {
-        Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-        Write-Information -MessageData "Setting PSGallery repository to trusted."
+    if ($($Policy.InstallationPolicy) -ne 'Trusted')
+    {
+        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+        Write-Information -MessageData 'Setting PSGallery repository to trusted.'
     }
 }
 
 # Start a stopwatch to time module installation elapsed time
 $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
-$RequiredModulesPath = Join-Path -Path $PSScriptRoot -ChildPath "PowerShell\ScubaGear\RequiredVersions.ps1"
-if (Test-Path -Path $RequiredModulesPath) {
-  . $RequiredModulesPath
+$RequiredModulesPath = Join-Path -Path $PSScriptRoot -ChildPath 'PowerShell\ScubaGear\RequiredVersions.ps1'
+if (Test-Path -Path $RequiredModulesPath)
+{
+    . $RequiredModulesPath
 }
 
-if ($ModuleList) {
+if ($ModuleList)
+{
     # Add PowerShellGet to beginning of ModuleList for installing required modules.
-    $ModuleList = ,@{
-        ModuleName = 'PowerShellGet'
-        ModuleVersion = [version] '2.1.0'
+    $ModuleList = , @{
+        ModuleName     = 'PowerShellGet'
+        ModuleVersion  = [version] '2.1.0'
         MaximumVersion = [version] '2.99.99999'
     } + $ModuleList
 }
 else
 {
-   throw "Required modules list is required."
+    throw 'Required modules list is required.'
 }
 
 $AvailableModules = Get-Module -ListAvailable
 
-foreach ($Module in $ModuleList) {
+foreach ($Module in $ModuleList)
+{
+    $InstalledApprovedModuleVersions = @($AvailableModules | Where-Object -Property Name -EQ $Module.ModuleName | Where-Object { [Version]($_.Version) -le $Module.MaximumVersion -and [Version]($_.Version) -ge $Module.ModuleVersion } | Sort-Object -Property Version -Descending)
 
-    $ModuleName = $Module.ModuleName
+    if ($InstalledApprovedModuleVersions.Count -ne 0)
+    {
+        $HighestApprovedVersion = $InstalledApprovedModuleVersions[0]
+        Write-Debug "$($Module.ModuleName):$($HighestApprovedVersion.Version) already has the latest approved module version installed."
 
-    if ($AvailableModules | Where-Object -Property Name -EQ $ModuleName) {
-        $HighestInstalledVersion = ($AvailableModules | Where-Object -Property Name -EQ $ModuleName | Sort-Object Version -Descending | Select-Object Version -First 1).Version
-        $LatestVersion = (Find-Module -Name $ModuleName).Version
+        if ($Force -eq $true)
+        {
+            Write-Debug "Re-installing module $($Module.MaximumVersion) due to '-Force' parameter"
 
-        if ($HighestInstalledVersion -ge $LatestVersion) {
-            Write-Debug "${ModuleName}:${HighestInstalledVersion} already has latest installed."
-
-            if ($Force -eq $true) {
-                Install-Module -Name $ModuleName `
-                    -Force `
-                    -AllowClobber `
-                    -Scope CurrentUser `
-                    -MaximumVersion $Module.MaximumVersion
-                Write-Information -MessageData "Re-installing module to latest acceptable version: ${ModuleName}"
-            }
-        }
-        else {
-            if ($SkipUpdate -eq $true) {
-                Write-Debug "Skipping update for ${ModuleName}:${HighestInstalledVersion} to newer version ${LatestVersion}."
-            }
-            else {
-                Install-Module -Name $ModuleName `
-                    -Force `
-                    -AllowClobber `
-                    -Scope CurrentUser `
-                    -MaximumVersion $Module.MaximumVersion
-                $MaxInstalledVersion = ($AvailableModules | Where-Object -Property Name -EQ $ModuleName | Sort-Object Version -Descending | Select-Object Version -First 1).Version
-                Write-Information -MessageData " ${ModuleName}:${HighestInstalledVersion} updated to version ${MaxInstalledVersion}."
-            }
+            Install-Module -Name $Module.ModuleName `
+                -Force `
+                -AllowClobber `
+                -Scope CurrentUser `
+                -MaximumVersion $Module.MaximumVersion
+            Write-Information -MessageData "Re-installing module to the latest acceptable version: $($Module.ModuleName)"
         }
     }
-    else {
-        Install-Module -Name $ModuleName `
+    else
+    {
+        Install-Module -Name $Module.ModuleName `
             -AllowClobber `
             -Scope CurrentUser `
             -MaximumVersion $Module.MaximumVersion
-            $MaxInstalledVersion = ($AvailableModules | Where-Object -Property Name -EQ $ModuleName | Sort-Object Version -Descending | Select-Object Version -First 1).Version
-        Write-Information -MessageData "Installed the latest acceptable version of ${ModuleName} version ${MaxInstalledVersion}"
+        Write-Information -MessageData "Installed the the latest acceptable version of $($Module.ModuleName) version $($Module.MaximumVersion)"
     }
 }
 
-if ($NoOPA -eq $true) {
-    Write-Debug "Skipping Download for OPA."
+if ($NoOPA -eq $true)
+{
+    Write-Debug 'Skipping Download for OPA.'
 }
-else {
+else
+{
     $DebugPreference = 'Continue'
-    try {
-    $ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-    . $ScriptDir\OPA.ps1
+    try
+    {
+        $ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+        . $ScriptDir\OPA.ps1
     }
-    catch {
-        Write-Error "An error occurred: cannot call OPA download script"
+    catch
+    {
+        Write-Error 'An error occurred: cannot call OPA download script'
     }
 }
 
@@ -134,5 +124,5 @@ $Stopwatch.stop()
 
 Write-Debug "ScubaGear setup time elapsed:  $([math]::Round($stopwatch.Elapsed.TotalSeconds,0)) seconds."
 
-$DebugPreference = "SilentlyContinue"
-$InformationPreference = "SilentlyContinue"
+$DebugPreference = 'SilentlyContinue'
+$InformationPreference = 'SilentlyContinue'
