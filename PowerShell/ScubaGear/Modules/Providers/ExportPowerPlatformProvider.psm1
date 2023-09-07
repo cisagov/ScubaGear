7f2cbb10d9c45208afc42865cc241a04f1a17927
Brown <rgbrow@sandia.gov>
2023-09-06 21:50:59 -0600

    Updated baseline title comments with new numbering scheme

--- PowerShell/ScubaGear/Modules/Providers/ExportPowerPlatformProvider.psm1
+++ PowerShell/ScubaGear/Modules/Providers/ExportPowerPlatformProvider.psm1
@@ -131,8 +131,10 @@ $($_)
         throw $FullEnvErrorMessage
     }
 
+    # MS.POWERPLATFORM.1.1v1, MS.POWERPLATFORM.1.2v1, MS.POWERPLATFORM.5.1v1
     $EnvironmentCreation = ConvertTo-Json @($Tracker.TryCommand("Get-TenantSettings"))
 
+    # MS.POWERPLATFORM.2.1v1, MS.POWERPLATFORM.2.2v1, MS.POWERPLATFORM.2.3v1
     $EnvironmentList = ConvertTo-Json @($Tracker.TryCommand("Get-AdminPowerAppEnvironment"))
 
     # Check for null return
@@ -161,6 +163,7 @@ $($_)
         Write-Warning "Error running Get-DlpPolicy: $($_). <= If a HTTP 403 ERROR is thrown then this is because you do not have the proper permissions. Necessary roles for running ScubaGear with Power Platform: Power Platform Administrator with a Power Apps License or Global Admininstrator"
     }
 
+    # MS.POWERPLATFORM.3.1v1
     # has to be tested manually because of http 403 errors
     $TenantIsolation = ConvertTo-Json @()
     try {
@@ -180,6 +183,10 @@ $($_)
     catch {
         Write-Warning "Error running Get-PowerAppTenantIsolationPolicy: $($_). <= If a HTTP 403 ERROR is thrown then this is because you do not have the proper permissions. Necessary roles for running ScubaGear with Power Platform: Power Platform Administrator with a Power Apps License or Global Admininstrator"
     }
+
+    # MS.POWERPLATFORM.3.2v1 currently has no corresponding PowerShell Cmdlet
+
+    # MS.POWERPLATFORM.4.1v1 currently has no corresponding PowerShell Cmdlet
 
     $PowerPlatformSuccessfulCommands = ConvertTo-Json @($Tracker.GetSuccessfulCommands())
     $PowerPlatformUnSuccessfulCommands = ConvertTo-Json @($Tracker.GetUnSuccessfulCommands())
