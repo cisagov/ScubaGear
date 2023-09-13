8c86423ca3d723f4999acdf45cfc87bcbb103c4f
Richard Crutchfield <crutchfield@mitre.org>
2023-09-13 07:51:52 -0400

    wip

--- Testing/Functional/Products/Products.Tests.ps1
+++ Testing/Functional/Products/Products.Tests.ps1
@@ -316,6 +316,7 @@ Describe "Policy Checks for <ProductName>"{
 
             $ReportFolders = Get-ChildItem . -directory -Filter "M365BaselineConformance*" | Sort-Object -Property LastWriteTime -Descending
             $OutputFolder = $ReportFolders[0]
+            Write-Host "OutputFolder: $OutputFolder"
             $IntermediateTestResults = LoadTestResults($OutputFolder)
             # Search the results object for the specific requirement we are validating and ensure the results are what we expect
             [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'PolicyResultObj', Justification = 'Variable is used in ScriptBlock')]
