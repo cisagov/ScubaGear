e0a16299e8e8528ae507cdb43e59c8a4eaf8e9ac
Dylan Gao <107067015+Dylan-MITRE@users.noreply.github.com>
2023-11-27 14:15:36 -0500

    Update OPA.ps1
    revert to bit transfer and detailed error messages

--- OPA.ps1
+++ OPA.ps1
@@ -18,36 +18,16 @@ $OPAExe = "opa_windows_amd64.exe"
 $InstallUrl = "https://openpolicyagent.org/downloads/v$($ExpectedVersion)/$OPAExe"
 $OutFile=(Join-Path (Get-Location).Path $InstallUrl.SubString($InstallUrl.LastIndexOf('/')))
 $ExpectedHash ="5D71028FED935DC98B9D69369D42D2C03CE84A7720D61ED777E10AAE7528F399"
+$Display = "Downloading OPA executable"
 
 # Download files
 try {
-    $uri = New-Object "System.Uri" "$InstallUrl"
-    $request = [System.Net.HttpWebRequest]::Create($uri)
-    $request.set_Timeout(15000) #15 second timeout
-    $response = $request.GetResponse()
-    $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
-    $responseStream = $response.GetResponseStream()
-    $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $OutFile, Create
-    $buffer = new-object byte[] 15000KB
-    $count = $responseStream.Read($buffer,0,$buffer.length)
-    $downloadedBytes = $count
-    while ($count -gt 0)
-    {
-        $targetStream.Write($buffer, 0, $count)
-        $count = $responseStream.Read($buffer,0,$buffer.length)
-        $downloadedBytes = $downloadedBytes + $count
-        Write-Progress -activity "Downloading file '$($InstallUrl.split('/') | Select-Object -Last 1)'" -status "Downloaded ($([System.Math]::Floor($downloadedBytes/1024))K of $($totalLength)K): " -PercentComplete ((([System.Math]::Floor($downloadedBytes/1024)) / $totalLength)  * 100)
-    }
-    Write-Progress -activity "Finished downloading file '$($InstallUrl.split('/') | Select-Object -Last 1)'"
-}
+    Start-BitsTransfer -Source $InstallUrl -Destination $OutFile -DisplayName $Display
+    Write-Information ""
+    Write-Information "`nDownload of `"$OutFile`" finished."
 catch {
-    Write-Error "An error has occurred: Unable to download OPA executable. To try manually downloading, see details in README under 'Download the required OPA executable'"
-}
-finally {
-    $targetStream.Flush()
-    $targetStream.Close()
-    $targetStream.Dispose()
-    $responseStream.Dispose()
+    $msg = $Error[0].Exception.Message
+    Write-Error "Unable to download OPA executable. To try manually downloading, see details in README under 'Download the required OPA executable'. Error: $msg"
 }
 
 # Hash checks
