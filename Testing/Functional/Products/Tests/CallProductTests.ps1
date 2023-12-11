41bdb3459e9fd7c9583802286b9e723adde86710
James Garriss <jgarriss@mitre.org>
2023-12-11 09:11:10 -0500

    debug

--- Testing/Functional/Products/Tests/CallProductTests.ps1
+++ Testing/Functional/Products/Tests/CallProductTests.ps1
@@ -21,11 +21,11 @@ Write-Debug $testDataDir
 # TODO: Eventually this hardcoded file should be replaced by reading all the .pson
 #       files in the TestData folder.  Instead of just a hashtable of params, it
 #       will be an array of hashtables.
-$params = Import-PowerShellDataFile $testDataFile
+# $params = Import-PowerShellDataFile $testDataFile
 
-# Create an array of test containers
-$testContainers = @()
-$testContainers += New-PesterContainer -Path $testScriptDir -Data $params
+# # Create an array of test containers
+# $testContainers = @()
+# $testContainers += New-PesterContainer -Path $testScriptDir -Data $params
 
-# Invoke Pester for each test container
-Invoke-Pester -Container $testContainers -Output Detailed
+# # Invoke Pester for each test container
+# Invoke-Pester -Container $testContainers -Output Detailed
