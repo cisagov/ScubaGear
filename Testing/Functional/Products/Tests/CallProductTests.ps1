e7ad90ff4e53930305ccc1977fdb9039966059c9
James Garriss <jgarriss@mitre.org>
2023-12-11 09:51:34 -0500

    hard code dir

--- Testing/Functional/Products/Tests/CallProductTests.ps1
+++ Testing/Functional/Products/Tests/CallProductTests.ps1
@@ -5,27 +5,17 @@
 #       will be added.
 
 # Setup directories
-$thisDir = Get-Location
-Write-Output 'thisDir'
-Write-Output $thisDir
-$testScriptDir = Split-Path -Parent $thisDir
-Write-Output 'testScriptDir'
-Write-Output $testScriptDir
-$testDataDir = Join-Path -Path $testScriptDir -ChildPath TestData
-Write-Output 'testDataDir'
-Write-Output $testDataDir
-# $testDataFile = Join-Path -Path $testDataDir -ChildPath sharepoint-commercial-data.pson
-# Write-Debug 'testDataFile'
-# Write-Debug $testDataFile
+$testDataFile = 'Testing/Functional/Products/Tests/TestData/sharepoint-commercial-data.pson'
+$testScriptDir = 'Testing/Functional/Products'
 
 # TODO: Eventually this hardcoded file should be replaced by reading all the .pson
 #       files in the TestData folder.  Instead of just a hashtable of params, it
 #       will be an array of hashtables.
-# $params = Import-PowerShellDataFile $testDataFile
+$params = Import-PowerShellDataFile $testDataFile
 
 # # Create an array of test containers
-# $testContainers = @()
-# $testContainers += New-PesterContainer -Path $testScriptDir -Data $params
+$testContainers = @()
+$testContainers += New-PesterContainer -Path $testScriptDir -Data $params
 
 # # Invoke Pester for each test container
 # Invoke-Pester -Container $testContainers -Output Detailed
