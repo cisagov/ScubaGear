149fbac05d1320d4199a580231457aa2d4cb4062
James Garriss <jgarriss@mitre.org>
2023-12-15 08:12:46 -0500

    pass thumbprint as arg

--- Testing/Functional/Products/Tests/CallProductTests.ps1
+++ Testing/Functional/Products/Tests/CallProductTests.ps1
@@ -5,7 +5,10 @@
 # NOTE: At the moment it only does this for one product, Sharepoint.  Others
 #       will be added.
 # To run this script, call it from the root of the repo, like so:
-# ./Testing/Functional/Products/Tests/CallProductTests.ps1
+# ./Testing/Functional/Products/Tests/CallProductTests.ps1 <value of thumbprint>
+
+# The thumbprint of the cert used to access the product.
+$thumbprint = $args[0]
 
 # Setup directories
 $testDataFile = 'Testing/Functional/Products/Tests/TestData/sharepoint-commercial-data.pson'
@@ -15,6 +18,11 @@ $testScriptDir = 'Testing/Functional/Products'
 #       files in the TestData folder.  Instead of just a hashtable of params, it
 #       will be an array of hashtables of params.
 $params = Import-PowerShellDataFile $testDataFile
+
+# Add thumbprint to hashtable
+# TODO: When the hardcoded file is replaced, this will need to be added to each
+#       hashtable in the array.
+$params["Thumbprint"] = $thumbprint
 
 # Create an array of test containers
 $testContainers = @()
