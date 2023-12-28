e3aca52f27279a56b6b32ed1f6da83b33667b838
James Garriss <jgarriss@mitre.org>
2023-12-28 14:29:35 -0500

    cleanup

--- Testing/Functional/Products/Tests/CallProductTests.ps1
+++ Testing/Functional/Products/Tests/CallProductTests.ps1
@@ -2,20 +2,12 @@
 # run the functional tests for all the products.
 # NOTE: At the moment it only does this for one product, Sharepoint.  Others
 #       will be added.
-# To run this script, call it from the root of the repo, like so:
-# ./Testing/Functional/Products/Tests/CallProductTests.ps1 <params as a hashtable> <value of thumbprint>
 
 # The hashtable with the params.
 # TODO: For now, this is a hashtable.  Eventually it will need to be an array of hashtables,
 #       one for each product/tenant tested.
-# $params = Invoke-Expression $args[0]
-# $params = [hashtable]::Create($args[0])
-
-
+# The replace and convert commands are used to convert the string (from the GitHub secret) into a hashtable.
 $params = $args[0] -replace '@{' -replace '}' -replace ';',"`n" -replace "'" | ConvertFrom-StringData
-Write-Host "Params"
-Write-Host $params
-Write-Host $params.GetType()
 
 # The thumbprint of the cert used to access the product.
 $thumbprint = $args[1]
