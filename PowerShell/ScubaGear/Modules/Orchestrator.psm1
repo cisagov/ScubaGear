0a10a4d20bd48b7d41688215565f084e120c84e4
Thom Neale <tneale@mitre.org>
2024-01-08 11:39:39 -0500

    Also dedupe product names in Invoke-RunCachede

--- PowerShell/ScubaGear/Modules/Orchestrator.psm1
+++ PowerShell/ScubaGear/Modules/Orchestrator.psm1
@@ -1307,7 +1307,7 @@ function Invoke-RunCached {
                 New-Item -ItemType "Directory" -Path $OutPath | Out-Null
             }
             $OutFolderPath = $OutPath
-            $ProductNames = $ProductNames | Sort-Object
+            $ProductNames = $ProductNames | Sort-Object -Unique
 
             Remove-Resources
             Import-Resources # Imports Providers, RunRego, CreateReport, Connection
