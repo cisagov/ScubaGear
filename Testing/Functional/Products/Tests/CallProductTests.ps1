4141f0c27dea4914bac4f7e42cb2fc23fae836b1
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-01-24 12:00:53 -0500

    Improve nightly functional tests (#828)
    * test json
    
    * add push
    
    * fix typo
    
    * convert from json
    
    * add pwsh
    
    * simplify
    
    * update names
    
    * add comment
    
    * add test
    
    * Add more back
    
    * remove some
    
    * add import back
    
    * add get thumbprint back
    
    * add setup back
    
    * add setup back
    
    * add test products
    
    * add echo
    
    * get params
    
    * remove shell
    
    * remove test products
    
    * add params
    
    * add loop
    
    * add shell
    
    * use echo
    
    * split out get params
    
    * inline params
    
    * read new secret
    
    * add rest back
    
    * set as hash
    
    * debug
    
    * fix string
    
    * fix replace
    
    * add trimstart
    
    * cut out params
    
    * do a hash
    
    * fix typo
    
    * add call to test
    
    * just sharepoint
    
    * test just sharepoint
    
    * move to separate job
    
    * show params
    
    * use array
    
    * declare array as hashtable
    
    * declare array
    
    * loop to extract
    
    * call test
    
    * call product test
    
    * test types
    
    * debug
    
    * debug
    
    * compare before and after
    
    * remove quotes
    
    * debug
    
    * debug
    
    * use replace
    
    * try outstring
    
    * debug
    
    * use csv params
    
    * remove get params job
    
    * test products
    
    * cleanup, new secrets
    
    * rename vars & cleanup
    
    * test YAML linter email
    
    * test w 2 plans
    
    * changed minute to 15 to avoid GitHub bug
    
    * fix lint
    
    * add comment, cleanup names
    
    * remove push trigger

--- Testing/Functional/Products/Tests/CallProductTests.ps1
+++ Testing/Functional/Products/Tests/CallProductTests.ps1
@@ -1,16 +1,12 @@
 <#
     .SYNOPSIS
-    The purpose of this script is to enable a GitHub Action workflow to run the functional tests for all the products.
+    The purpose of this script is to enable a GitHub Action workflow to run the functional tests for one product.
     .EXAMPLE
     To run this script, call it from the root of the repo, like so: ./Testing/Functional/Products/Tests/CallProductTests.ps1 <params> <thumbprint>
-    .NOTES
-    At the moment this script is only used for one product, Sharepoint.  Others will be added over time.
 #>
 
 param(
     # The hashtable with the params.
-    # TODO: For now, this is a hashtable.  Eventually it will need to be an array of hashtables,
-    #       one for each product/tenant tested.
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
     [hashtable]$params,
@@ -23,8 +19,6 @@ param(
 $testScriptDir = 'Testing/Functional/Products'
 
 # Add thumbprint to hashtable
-# TODO: When params becomes an array of hashtables, this will need to be added to each
-#       hashtable in the array.
 $params["Thumbprint"] = $thumbprint
 
 # Create an array of test containers
