3621b16139bd1218af04edcbfe3b345b801b9e46
Devesh Agarwal <dagarwal@mitre.org>
2024-01-25 14:22:16 -0500

    added mock method for get-mgbetausercount

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/Export-AADProvider.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/Export-AADProvider.Tests.ps1
@@ -43,6 +43,10 @@ InModuleScope -ModuleName ExportAADProvider {
                                     }
                                 }
                             }
+                            "Get-MgBetaUserCount" {
+                                $this.SuccessfulCommands += $Command
+                                return [pscustomobject]@{}
+                            }
                             "Get-PrivilegedUser" {
                                 $this.SuccessfulCommands += $Command
                                 return [pscustomobject]@{}
