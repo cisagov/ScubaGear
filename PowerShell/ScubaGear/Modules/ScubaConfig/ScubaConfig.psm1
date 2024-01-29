fa96dd47467cb23a2858b7f9621b144833c471a4
Thom Neale <tneale@mitre.org>
2024-01-29 11:34:25 -0500

    Moved CBH string inside class def per style guide

--- PowerShell/ScubaGear/Modules/ScubaConfig/ScubaConfig.psm1
+++ PowerShell/ScubaGear/Modules/ScubaConfig/ScubaConfig.psm1
@@ -1,19 +1,18 @@
-<#
-.SYNOPSIS
-  This class stores Scuba config data loaded from a file.
-.DESCRIPTION
-  This class is designed to function as a singleton. The singleton instance
-  is cached on the ScubaConfig type itself. In the context of tests, it is
-  important to call .ResetInstance before and after tests as needed to
-  ensure any preexisting configs are not inadvertantly used for the test,
-  or left in place after the test is finished.
-.EXAMPLE
-  $Config = [ScubaConfig]::GetInstance()
-  [ScubaConfig]::ResetInstance()
-  [ScubaConfig]::LoadConfig($SomePath)
-#>
 class ScubaConfig {
-
+    <#
+    .SYNOPSIS
+      This class stores Scuba config data loaded from a file.
+    .DESCRIPTION
+      This class is designed to function as a singleton. The singleton instance
+      is cached on the ScubaConfig type itself. In the context of tests, it is
+      important to call .ResetInstance before and after tests as needed to
+      ensure any preexisting configs are not inadvertantly used for the test,
+      or left in place after the test is finished.
+    .EXAMPLE
+      $Config = [ScubaConfig]::GetInstance()
+      [ScubaConfig]::ResetInstance()
+      [ScubaConfig]::LoadConfig($SomePath)
+    #>
     hidden static [ScubaConfig]$_Instance = [ScubaConfig]::new()
     hidden static [Boolean]$_IsLoaded = $false
 
