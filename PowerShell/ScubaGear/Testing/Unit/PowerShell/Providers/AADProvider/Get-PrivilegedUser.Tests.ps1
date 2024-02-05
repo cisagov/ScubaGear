b1f0c1e0a649768f8af306c35002149a9697c38e
Richard Crutchfield <richard.m.crutchfield@gmail.com>
2024-02-05 14:00:00 -0500

    wip

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/Get-PrivilegedUser.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/Get-PrivilegedUser.Tests.ps1
@@ -3,9 +3,13 @@ Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "$($ProviderPath)/Export
 
 InModuleScope ExportAADProvider {
     BeforeAll {
+        function Get-PrivilegedUser {throw 'this will be mocked'}
         Mock -ModuleName ExportAADProvider Get-PrivilegedUser {}
+        function Get-MgBetaDirectoryRoleMember {throw 'this will be mocked'}
         Mock -ModuleName ExportAADProvider Get-MgBetaDirectoryRoleMember {}
+        function Get-MgBetaUser {throw 'this will be mocked'}
         Mock -ModuleName ExportAADProvider Get-MgBetaUser {}
+        function Get-MgBetaGroupMember {throw 'this will be mocked'}
         Mock -ModuleName ExportAADProvider Get-MgBetaGroupMember {}
     }
     Describe -Tag 'AADProvider' -Name "Get-PrivilegedUser" {
