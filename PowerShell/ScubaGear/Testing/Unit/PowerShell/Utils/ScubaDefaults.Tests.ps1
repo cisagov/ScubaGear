8d0029784dd8db2c5c612d2a8a9314397398f7eb
Richard Crutchfield <richard.m.crutchfield@gmail.com>
2024-02-06 14:08:47 -0500

    Update test

---
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Utils/ScubaDefaults.Tests.ps1
@@ -0,0 +1,14 @@
+BeforeDiscovery{
+    $ModuleBasePath = (Join-Path -Path $PSScriptRoot -ChildPath '..\..\..\..' -Resolve)
+    . (Join-Path -Path $ModuleBasePath -ChildPath "ScubaDefaults.ps1")
+}
+
+Describe "Verify Constants <Name>" -ForEach @(
+    @{Name='DefaultOPAPath'; Expected=$true},
+    @{Name='BadName'; Expected=$false}
+){
+    It "Check for <Name>" {
+        $Result = Get-ScubaDefaults -Name $Name
+        $null -ne $Result | Should -Be $Expected -Because "value: $Results"
+    }
+}
