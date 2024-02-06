52c14e1cb01ad38440320dde979b494dfdb7a6c9
Thom Neale <tneale@mitre.org>
2024-02-06 12:58:34 -0500

    Megalinter appeasement

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/ScubaConfig/ScubaConfigLoadConfig.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/ScubaConfig/ScubaConfigLoadConfig.Tests.ps1
@@ -7,11 +7,11 @@ InModuleScope ScubaConfig {
                 $cfg = [ScubaConfig]::GetInstance()
 		# Load the first file and check the ProductNames value.
                 $file1 = Join-Path -Path $PSScriptRoot -ChildPath config_test_load_config1.json
-                $loaded1 = $cfg.LoadConfig($file1)
+                $cfg.LoadConfig($file1)
                 $products1 = $cfg.Configuration.ProductNames | Should -Be 'teams'
 		# Load the second file and verify that ProductNames has changed.
                 $file2 = Join-Path -Path $PSScriptRoot -ChildPath config_test_load_config2.json
-                $loaded2 = $cfg.LoadConfig($file2)
+                $cfg.LoadConfig($file2)
                 $products1 = $cfg.Configuration.ProductNames | Should -Be 'exo'
             }
             AfterAll {
