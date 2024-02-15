18a972a2c2904c8f60d4ddd3f9b84df2193ac298
Richard Crutchfield <richard.m.crutchfield@gmail.com>
2024-02-15 12:27:17 -0500

    Enhanced UT for SP

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Connection/Connect-Tenant.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Connection/Connect-Tenant.Tests.ps1
@@ -1,21 +1,22 @@
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "../../../../Modules/Connection/Connection.psm1") -Function 'Connect-Tenant' -Force
-Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "../../../../Modules/Connection/ConnectHelpers.psm1") -Force
 
 InModuleScope Connection {
     Describe -Tag 'Connection' -Name "Connect-Tenant" -ForEach @(
         @{Endpoint = 'commercial'}
-        @{Endpoint = 'gcc'}
-        @{Endpoint = 'gcchigh'}
-        @{Endpoint = 'dod'}
+        # @{Endpoint = 'gcc'}
+        # @{Endpoint = 'gcchigh'}
+        # @{Endpoint = 'dod'}
     ){
         BeforeAll {
+            function Connect-MgGraph {throw 'this will be mocked'}
             Mock Connect-MgGraph -MockWith {}
             Mock Connect-PnPOnline -MockWith {}
             Mock Connect-SPOService -MockWith {}
+            function Connect-MicrosoftTeams{throw 'this will be mocked'}
             Mock Connect-MicrosoftTeams -MockWith {}
             Mock Add-PowerAppsAccount -MockWith {}
-            function Connect-EXOHelper {}
-            Mock Connect-EXOHelper -MockWith {}
+            function Connect-EXOHelper {throw 'this will be mocked'}
+            Mock -ModuleName Connection Connect-EXOHelper -MockWith {}
             Mock Get-MgBetaOrganization -MockWith {
                 return [pscustomobject]@{
                     DisplayName     = "DisplayName";
@@ -36,18 +37,45 @@ InModuleScope Connection {
             Mock -CommandName Write-Progress {
             }
         }
-        It 'With Endpoint:  <Endpoint>; ProductNames: <ProductNames>' -ForEach @(
-            @{ProductNames = "aad"}
-            @{ProductNames = "defender"}
-            @{ProductNames = "exo"}
-            @{ProductNames = "powerplatform"}
-            @{ProductNames = "sharepoint"}
-            @{ProductNames = "teams"}
-            @{ProductNames = "aad", "defender", "exo", "powerplatform", "sharepoint", "teams"}
+        Context 'With Endpoint:  <Endpoint>; ProductNames: <ProductNames>' -ForEach @(
+            @{ProductNames = "aad"; Services = @('Connect-MgGraph')}
+            @{ProductNames = "defender"; Services = @('Connect-EXOHelper')}
+            @{ProductNames = "exo"; Services = @('Connect-EXOHelper')}
+            @{ProductNames = "powerplatform"; Services = @('Add-PowerAppsAccount')}
+            @{ProductNames = "sharepoint"; Services = @('Connect-MgGraph', 'Connect-PnPOnline')}
+            @{ProductNames = "teams"; Services = @('Connect-MicrosoftTeams')}
+            @{
+                ProductNames = "aad", "defender", "exo", "powerplatform", "sharepoint", "teams"
+                Services = @(
+                    'Connect-MgGraph', 
+                    'Connect-EXOHelper',
+                    'Add-PowerAppsAccount',
+                    'Connect-PnPOnline',
+                    'Connect-MicrosoftTeams'
+                )
+            }
 
         ){
-            $FailedAuthList = Connect-Tenant -ProductNames $ProductNames -M365Environment $Endpoint
-            $FailedAuthList.Length | Should -Be 0
+
+            It "No Service Principal" {
+                $FailedAuthList = Connect-Tenant -ProductNames $ProductNames -M365Environment $Endpoint
+                $FailedAuthList.Length | Should -Be 0
+            }
+            It "With Service Principal" {
+                $ServicePrincipalParams.CertThumbprintParams.CertificateThumbprint
+                $ServicePrincipalParams =@{
+                    CertThumbprintParams = @{
+                        AppID = "a"
+                        CertificateThumbprint = "b"
+                        Organization = "c"
+                    }
+                }
+                Connect-Tenant -ProductNames $ProductNames -M365Environment $Endpoint -ServicePrincipalParams $ServicePrincipalParams
+                foreach ($Service in $Services){
+                    Should -Invoke -CommandName $Service -Exactly -Times 1 -Because "only want to authenticate to needed service once"
+                }
+            }
+
         }
     }
 }
--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Get-ServicePrincipalParams.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Get-ServicePrincipalParams.Tests.ps1
@@ -3,19 +3,54 @@ Import-Module (Join-Path -Path $PSScriptRoot -ChildPath $OrchestratorPath) -Func
 
 Describe -Tag 'Orchestrator' -Name 'Get-ServicePrincipalParams' {
     InModuleScope Orchestrator {
-        It 'Returns a CertficateThumbprint PSObject with no errors' {
-            $BoundParameters = @{
-                CertificateThumbprint = 'WPOEALFN425A';
-                AppID = '34289UFAHWFALL';
-                Organization = 'example.onmicrosoft.com';
+        Context "Service Principal provided"{
+            BeforeAll{
+                [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'BoundParameters')]
+                $BoundParameters = @{
+                    CertificateThumbprint = 'WPOEALFN425A'
+                    AppID = '34289UFAHWFALL'
+                    Organization = 'example.onmicrosoft.com'
+                }
             }
-            {Get-ServicePrincipalParams -BoundParameters $BoundParameters} | Should -Not -Throw
+            It 'Does not throw exception' {
+                {Get-ServicePrincipalParams -BoundParameters $BoundParameters} | Should -Not -Throw
+            }
+            It "All required items are present"{
+                $Results = Get-ServicePrincipalParams -BoundParameters $BoundParameters
+                $Results | Should -BeOfType [hashtable]
+                $Results.Count | Should -BeExactly 1
+                $Results.CertThumbprintParams.Count | Should -BeExactly 3
+            }
         }
-        It 'Throws an error if no correct Service Principal Params are passed in' {
-            $BoundParameters = @{
-                a = 'a';
+        Context "Partial data for Service Principal"{
+            It "Only AppId"{
+                $BoundParameters = @{
+                    AppID = '34289UFAHWFALL'
+                }
+                {Get-ServicePrincipalParams -BoundParameters $BoundParameters} |
+                    Should -Throw  'Missing parameters required for authentication with Service Principal Auth; Run Get-Help Invoke-Scuba for details on correct arguments'
             }
-            {Get-ServicePrincipalParams -BoundParameters $BoundParameters} | Should -Throw
+            It "Only Thumbprint Only"{
+                $BoundParameters = @{
+                    CertificateThumbprint = 'WPOEALFN425A'
+                }
+                {Get-ServicePrincipalParams -BoundParameters $BoundParameters} |
+                    Should -Throw  'Missing parameters required for authentication with Service Principal Auth; Run Get-Help Invoke-Scuba for details on correct arguments'
+            }
+            It "Only Organization Only"{
+                $BoundParameters = @{
+                    Organization = 'example.onmicrosoft.com'
+                }
+                {Get-ServicePrincipalParams -BoundParameters $BoundParameters} |
+                    Should -Throw  'Missing parameters required for authentication with Service Principal Auth; Run Get-Help Invoke-Scuba for details on correct arguments'
+            }
+        }
+        Context "No Service Principal provided"{
+            It 'Throws an error if no correct Service Principal Params are passed in' {
+                $BoundParameters = @{
+                }
+                {Get-ServicePrincipalParams -BoundParameters $BoundParameters} | Should -Throw
+            }
         }
     }
 }
