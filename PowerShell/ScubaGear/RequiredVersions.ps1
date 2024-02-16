4f2d9825cf6caa3eaf3ef3e6b86ee7f9c00e0619
Addam Schroll <108814318+schrolla@users.noreply.github.com>
2024-02-16 09:00:29 -0600

    * Pin Microsoft.Graph.* max version to 2.12.0 (#908)
    * Pin Microsoft.Online.SharePoint.PowerShell to max version 16.0.24322.12000

--- PowerShell/ScubaGear/RequiredVersions.ps1
+++ PowerShell/ScubaGear/RequiredVersions.ps1
@@ -13,7 +13,7 @@ $ModuleList = @(
     @{
         ModuleName = 'Microsoft.Online.SharePoint.PowerShell' # includes OneDrive
         ModuleVersion = [version] '16.0.0'
-        MaximumVersion = [version] '16.99.99999'
+        MaximumVersion = [version] '16.0.24322.12000'
     },
     @{
         ModuleName = 'PnP.PowerShell' # alternate for SharePoint PowerShell
@@ -33,32 +33,32 @@ $ModuleList = @(
     @{
         ModuleName = 'Microsoft.Graph.Authentication'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.99.99999'
+        MaximumVersion = [version] '2.12.0'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Users'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.99.99999'
+        MaximumVersion = [version] '2.12.0'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Groups'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.99.99999'
+        MaximumVersion = [version] '2.12.0'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Identity.DirectoryManagement'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.99.99999'
+        MaximumVersion = [version] '2.12.0'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Identity.Governance'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.99.99999'
+        MaximumVersion = [version] '2.12.0'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Identity.SignIns'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.99.99999'
+        MaximumVersion = [version] '2.12.0'
     },
     @{
         ModuleName = 'powershell-yaml'
