1e699888c6831b3dce95b84466513e522c79c24d
Dylan Gao <107067015+Dylan-MITRE@users.noreply.github.com>
2024-02-19 11:02:04 -0500

    update supported opa versions to 0.61 (#895)
    * update opa versions
    
    * Update opa version
    
    * README.md opa version

--- SetUp.ps1
+++ SetUp.ps1
@@ -36,7 +36,7 @@ param(
     [Parameter(Mandatory = $false, HelpMessage = 'The version of OPA Rego to be downloaded, must be in "x.x.x" format')]
     [Alias('version')]
     [string]
-    $ExpectedVersion = '0.59.0',
+    $ExpectedVersion = '0.61.0',
 
     [Parameter(Mandatory = $false, HelpMessage = 'The operating system the program is running on')]
     [ValidateSet('Windows','MacOS','Linux')]
