6817236f1edaa0c14946b3cca27848817607afc0
Alden Hilton <adhilto@sandia.gov>
2024-03-06 12:02:12 -0800

    Mock Merge-JsonOutput function in the RunCached test

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Invoke-RunCached.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Invoke-RunCached.Tests.ps1
@@ -16,6 +16,8 @@ InModuleScope Orchestrator {
             Mock -ModuleName Orchestrator Invoke-RunRego {}
             function Invoke-ReportCreation {}
             Mock -ModuleName Orchestrator Invoke-ReportCreation {}
+            function Merge-JsonOutput {}
+            Mock -ModuleName Orchestrator Merge-JsonOutput {}
             function Disconnect-SCuBATenant {}
             Mock -ModuleName Orchestrator Disconnect-SCuBATenant
 
