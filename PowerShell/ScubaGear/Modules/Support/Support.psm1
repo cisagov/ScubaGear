dec97dd0e53a5f320b03517c0ce0780658fca094
Richard Crutchfield <richard.m.crutchfield@gmail.com>
2024-03-06 16:15:40 -0500

    throw exception if RequiredVersions not found

--- PowerShell/ScubaGear/Modules/Support/Support.psm1
+++ PowerShell/ScubaGear/Modules/Support/Support.psm1
@@ -114,8 +114,11 @@ function Initialize-SCuBA {
     # Need to determine where module is so we can get required versions info
     $ModuleBaseDir = Split-Path -Path (Get-Module ScubaGear).Path -Parent
     $RequiredModulesPath = Join-Path -Path $ModuleBaseDir -ChildPath "RequiredVersions.ps1"
-    if (Test-Path -Path $RequiredModulesPath) {
+    if (Test-Path -Path $RequiredModulesPath -Leaf) {
         . $RequiredModulesPath
+    }
+    else {
+        throw "Unable to location RequiredVersions.ps1 at $RequiredModulesPath"
     }
 
     if ($ModuleList) {
