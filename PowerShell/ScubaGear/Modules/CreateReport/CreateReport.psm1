bec68c0b363918d4e29f42c9b7678d1ba0632b19
Devesh Agarwal <dagarwal@mitre.org>
2024-03-20 13:42:46 -0400

    Copied Changes form broken branch for new PR

--- PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
+++ PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
@@ -204,13 +204,26 @@ function New-Report {
 
     # Handle AAD-specific reporting
     if ($BaselineName -eq "aad") {
+        $LicenseInfoArray = $SettingsExport.license_information | ForEach-Object { 
+            [pscustomobject]@{ 
+                "License SKU Identifier" = $_.SkuPartNumber
+                "Licenses in Use" = $_.ConsumedUnits
+                "Total Licenses" = $_.PrepaidUnits.Enabled
+            } 
+        } 
+        # Convert the custom objects to an HTML table 
+        $LicenseTable = $LicenseInfoArray | ConvertTo-Html -As Table -Fragment
+        
+        # Create a section header for the licensing information 
+        $LicensingHTML = "<h2>Tenant Licensing Information</h2>" + $LicenseTable 
+
         $ReportHTML = $ReportHTML.Replace("{AADWARNING}", $AADWarning)
-        $ReportHTML = $ReportHTML.Replace("{CAPTABLES}", "")
+        $ReportHTML = $ReportHTML.Replace("{LICENSING_INFO}", $LicensingHTML)
         $CapJson = ConvertTo-Json $SettingsExport.cap_table_data
     }
     else {
         $ReportHTML = $ReportHTML.Replace("{AADWARNING}", $NoWarning)
-        $ReportHTML = $ReportHTML.Replace("{CAPTABLES}", "")
+        $ReportHTML = $ReportHTML.Replace("{LICENSING_INFO}", "")
         $CapJson = "null"
     }
 
