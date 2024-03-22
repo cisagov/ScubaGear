2ee9469290a5ec8413e64f967015f98e14ce8952
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-03-22 08:30:14 -0400

    remove array

--- utils/DeployUtils.ps1
+++ utils/DeployUtils.ps1
@@ -200,7 +200,7 @@ function ConfigureScubaGearModule {
     $ProjectUri = "https://github.com/cisagov/ScubaGear"
     $LicenseUri = "https://github.com/cisagov/ScubaGear/blob/main/LICENSE"
     # Tags cannot contain spaces
-    $Tags = @('CISA', 'O365', 'M365', 'AzureAD', 'Configuration', 'Exchange', 'Report', 'Security', 'SharePoint', 'Defender', 'Teams', 'PowerPlatform', 'OneDrive')
+    $Tags = 'CISA', 'O365', 'M365', 'AzureAD', 'Configuration', 'Exchange', 'Report', 'Security', 'SharePoint', 'Defender', 'Teams', 'PowerPlatform', 'OneDrive'
     
     $ManifestUpdates = @{
         Path          = $ManifestPath
