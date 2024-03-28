ed390d47dc4852054a502873fef4216a9cba3588
amart241 <amart24@sandia.gov>
2024-03-28 07:20:52 -0700

    added default value for unit testing
    various syntax fixes as well

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Support/New-Config.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Support/New-Config.Tests.ps1
@@ -1,34 +1,34 @@
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath '../../../../Modules/Support')
 
-InModuleScope CreateReport {
-    Describe -Tag CreateReport -Name 'New-Config' {
+InModuleScope Support {
+    Describe -Tag Support -Name 'New-Config' {
         BeforeAll {
             Mock -CommandName Write-Warning {}
 
             $TestPath = New-Item -Path (Join-Path -Path "TestDrive:" -ChildPath "SampleConfig") -Name "CreateSampleConfigFolder" -ItemType Directory
 
             [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'CMDArgs')]
-            CMDArgs = @{
-                $Description = "YAML configuration file with default description";
-                $ProductNames = @("aad", "defender", "exo", "sharepoint", "teams");
-                $M365Environment = "commercial";
-                $OPAPath = ".";
-                $LogIn = $true;
-                $DisconnectOnExit = $false;
-                $OutPath = '.';
-                $AppID = '';
-                $CertificateThumbprint = '';
-                $Organization = '';
-                $OutFolderName = "M365BaselineConformance";
-                $OutProviderFileName = "ProviderSettingsExport";
-                $OutRegoFileName = "TestResults";
-                $OutReportName = "BaselineReports";
-                $ConfigLocation = $TestPath;
+            $CMDArgs = @{
+                Description = "YAML configuration file with default description";
+                ProductNames = @("aad", "defender", "exo", "sharepoint", "teams");
+                M365Environment = "commercial";
+                OPAPath = ".";
+                LogIn = $true;
+                DisconnectOnExit = $false;
+                OutPath = '.';
+                AppID = '0';
+                CertificateThumbprint = '0';
+                Organization = '0';
+                OutFolderName = "M365BaselineConformance";
+                OutProviderFileName = "ProviderSettingsExport";
+                OutRegoFileName = "TestResults";
+                OutReportName = "BaselineReports";
+                ConfigLocation = $TestPath;
             }
         }
         It 'Creates a sample configuration' {
 
-            New-Config @CMDArgs | Should -Not -Throw
+            { New-Config @CMDArgs } | Should -Not -Throw
 
             Test-Path -Path "$($TestPath)/SampleConfig.yaml" -PathType leaf | Should -Be $true
         }
