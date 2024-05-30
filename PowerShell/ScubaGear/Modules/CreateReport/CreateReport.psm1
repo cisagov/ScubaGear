2cd6c19b58c05a1b643b06cb8b929a019d983433
Alden Hilton <106177711+adhilto@users.noreply.github.com>
2024-05-30 08:40:43 -0700

    Add reference URL to ScubaResults.json (#1119)
    * Add reference URL to ScubaResults.json
    
    * Rename to GroupReferenceURL
    
    Co-authored-by: David Bui <105074908+buidav@users.noreply.github.com>
    
    * Rename URL var to GroupReferenceURL part 1
    
    Co-authored-by: Addam Schroll <108814318+schrolla@users.noreply.github.com>
    
    * Rename URL var to GroupReferenceURL part 2
    
    Co-authored-by: Addam Schroll <108814318+schrolla@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: David Bui <105074908+buidav@users.noreply.github.com>
    Co-authored-by: Addam Schroll <108814318+schrolla@users.noreply.github.com>

--- PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
+++ PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
@@ -167,13 +167,15 @@ function New-Report {
         $Number = $BaselineName.ToUpper() + '-' + $BaselineGroup.GroupNumber
         $Name = $BaselineGroup.GroupName
         $GroupAnchor = New-MarkdownAnchor -GroupNumber $BaselineGroup.GroupNumber -GroupName $BaselineGroup.GroupName
-        $MarkdownLink = "<a class='control_group' href=`"$($ScubaGitHubUrl)/blob/v$($SettingsExport.module_version)/PowerShell/ScubaGear/baselines/$($BaselineName.ToLower()).md$GroupAnchor`" target=`"_blank`">$Name</a>"
+        $GroupReferenceURL = "$($ScubaGitHubUrl)/blob/v$($SettingsExport.module_version)/PowerShell/ScubaGear/baselines/$($BaselineName.ToLower()).md$GroupAnchor"
+        $MarkdownLink = "<a class='control_group' href=`"$($GroupReferenceURL)`" target=`"_blank`">$Name</a>"
         $Fragments += $Fragment | ConvertTo-Html -PreContent "<h2>$Number $MarkdownLink</h2>" -Fragment
 
         # Package Assessment Report into Report JSON by Policy Group
         $ReportJson.Results += [pscustomobject]@{
             GroupName = $BaselineGroup.GroupName;
             GroupNumber = $BaselineGroup.GroupNumber;
+            GroupReferenceURL = $GroupReferenceURL;
             Controls = $Fragment;
         }
 
