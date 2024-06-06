a75ba6d79c3ff7137fa802c92e569ceb51365b80
Alden Hilton <adhilto@sandia.gov>
2024-06-06 12:54:10 -0700

    Add test cases for Get-ScubaDkimRecord

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/EXOProvider/Get-ScubaDkimRecord.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/EXOProvider/Get-ScubaDkimRecord.Tests.ps1
@@ -2,16 +2,96 @@ $ProviderPath = "../../../../../Modules/Providers"
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "$($ProviderPath)/ExportEXOProvider.psm1") -Function Get-ScubaDkimRecords -Force
 
 InModuleScope 'ExportEXOProvider' {
-    Describe -Tag 'ExportEXOProvider' -Name "Get-ScubaDkimRecords" {
-        It "TODO handles a domain with DKIM" {
-            # Get-ScubaDkimRecords
-            $true | Should -Be $true
+    Describe -Tag 'ExportEXOProvider' -Name "Get-ScubaDkimRecord" {
+        BeforeAll {
+            Mock -CommandName Write-Warning {}
         }
-
-        It "TODO handles a domain without DKIM" {
-            # Get-ScubaDkimRecords
-            $true | Should -Be $true
-
+        Context "When high-confidence answers are available" {
+            BeforeAll {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @("v=DKIM1...");
+                        "HighConfidence" = $true;
+                        "LogEntries" = @("some text")
+                    }
+                }
+            }
+            It "Resolves 1 domain name" {
+                $Response = Get-ScubaDkimRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
+                $Response.rdata -Contains "v=DKIM1..." | Should -Be $true
+            }
+            It "Resolves multiple domain name" {
+                $Response = Get-ScubaDkimRecord -Domains @(@{"DomainName" = "example.com"},
+                @{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 2
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
+                $Response.rdata -Contains "v=DKIM1..." | Should -Be $true
+            }
+        }
+        Context "When high-confidence answers are not available" {
+            BeforeAll {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @("v=DKIM1...");
+                        "HighConfidence" = $false;
+                        "LogEntries" = @("some text")
+                    }
+                }
+            }
+            It "Prints a warning" {
+                $Response = Get-ScubaDkimRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 1
+                $Response.rdata -Contains "v=DKIM1..." | Should -Be $true
+            }
+        }
+        Context "When not all selectors work" {
+            It "Tries multiple selectors" {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    if ($Qname.Contains("selector1")) {
+                        @{
+                            "Answers" = @();
+                            "HighConfidence" = $true;
+                            "LogEntries" = @("Query returned NXDomain")
+                        }
+                    }
+                    else {
+                        @{
+                            "Answers" = @("v=DKIM1...");
+                            "HighConfidence" = $true;
+                            "LogEntries" = @("some text")
+                        }
+                    }
+                }
+                $Response = Get-ScubaDkimRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 2
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
+                $Response.rdata -Contains "v=DKIM1..." | Should -Be $true
+            }
+            It "Embeds the domain name into the selector" {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    if ($Qname -eq "selector1-example-com._domainkey.example.com") {
+                        @{
+                            "Answers" = @("v=DKIM1...");
+                            "HighConfidence" = $true;
+                            "LogEntries" = @("some text")
+                        }
+                    }
+                    else {
+                        @{
+                            "Answers" = @();
+                            "HighConfidence" = $true;
+                            "LogEntries" = @("Query returned NXDomain")
+                        }
+                    }
+                }
+                $Response = Get-ScubaDkimRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 3
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
+                $Response.rdata -Contains "v=DKIM1..." | Should -Be $true
+            }
         }
     }
 }
