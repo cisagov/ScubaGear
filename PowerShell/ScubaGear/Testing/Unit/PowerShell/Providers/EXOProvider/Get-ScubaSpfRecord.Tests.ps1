74453f51e3d7a76f8710275ce32ab4b23135066d
Alden Hilton <adhilto@sandia.gov>
2024-06-05 22:07:10 -0700

    Add test casese for Get-ScubaSpfRecord

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/EXOProvider/Get-ScubaSpfRecord.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/EXOProvider/Get-ScubaSpfRecord.Tests.ps1
@@ -1,11 +1,52 @@
 $ProviderPath = "../../../../../Modules/Providers"
-Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "$($ProviderPath)/ExportEXOProvider.psm1") -Function 'Get-ScubaSpfRecords' -Force
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "$($ProviderPath)/ExportEXOProvider.psm1") `
+    -Function 'Get-ScubaSpfRecord' -Force
 
 InModuleScope 'ExportEXOProvider' {
-    Describe -Tag 'ExportEXOProvider' -Name "Get-ScubaSpfRecords" {
-        It "TODO return SPF records" {
-            # Get-ScubaSpfRecords
-            $true | Should -Be $true
+    Describe -Tag 'ExportEXOProvider' -Name "Get-ScubaSpfRecord" {
+        Context "When high-confidence answers are available" {
+            BeforeAll {
+                Mock -CommandName Write-Warning {}
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @("v=spf1...");
+                        "HighConfidence" = $true;
+                        "LogEntries" = @("some text")
+                    }
+                }
+            }
+            It "Resolves 1 domain name" {
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
+                $Response.rdata -Contains "v=spf1..." | Should -Be $true
+            }
+    
+            It "Resolves multiple domain names" {
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"},
+                    @{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 2
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
+                $Response.rdata -Contains "v=spf1..." | Should -Be $true
+            }
+        }
+        Context "When high-confidence answers are not available" {
+            BeforeAll {
+                Mock -CommandName Write-Warning {}
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @("v=spf1...");
+                        "HighConfidence" = $false;
+                        "LogEntries" = @("some text")
+                    }
+                }
+            }
+            It "Prints a warning" {
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 1
+                $Response.rdata -Contains "v=spf1..." | Should -Be $true
+            }
         }
     }
 }
