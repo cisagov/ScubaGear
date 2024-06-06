b044a3aa1973ac290b64a20e79ba2000e09b53fd
Alden Hilton <adhilto@sandia.gov>
2024-06-05 18:53:42 -0700

    Added test cases for Select-DohServer

---
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/EXOProvider/Select-DohServer.Test.ps1
@@ -0,0 +1,32 @@
+$ProviderPath = "../../../../../Modules/Providers"
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "$($ProviderPath)/ExportEXOProvider.psm1") -Function 'Select-DohServer' -Force
+
+InModuleScope 'ExportEXOProvider' {
+    Describe -Tag 'ExportEXOProvider' -Name "Select-DohServer" {
+        It "Returns the first server that works" {
+            Mock -CommandName Invoke-WebRequest {}
+            $Server = Select-DohServer
+            Should -Invoke -CommandName Invoke-WebRequest -Exactly -Times 1
+            $Server | Should -Be "cloudflare-dns.com"
+        }
+        It "Tries with IP address if domain is blocked" {
+            Mock -CommandName Invoke-WebRequest {
+                if ($Uri.ToString().Contains("cloudflare-dns.com")) {
+                    throw "some error"
+                }
+                else {}
+            }
+            { Select-DohServer } | Should -Not -Throw
+            Should -Invoke -CommandName Invoke-WebRequest -Exactly -Times 2
+        }
+        It "Returns null if no servers work" {
+            Mock -CommandName Invoke-WebRequest { throw "some error" }
+            $Server = Select-DohServer
+            Should -Invoke -CommandName Invoke-WebRequest -Exactly -Times 3
+            $Server | Should -Be $null
+        }
+    }
+}
+AfterAll {
+    Remove-Module ExportEXOProvider -Force -ErrorAction SilentlyContinue
+}
