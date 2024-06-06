1c4f20ee00c809fddb09a493d9b58fa0756cdc9a
Alden Hilton <adhilto@sandia.gov>
2024-06-06 14:29:44 -0700

    Add test cases for Get-ScubaDmarcRecord

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/EXOProvider/Get-ScubaDmarcRecord.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/EXOProvider/Get-ScubaDmarcRecord.Tests.ps1
@@ -1,11 +1,81 @@
 $ProviderPath = "../../../../../Modules/Providers"
-Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "$($ProviderPath)/ExportEXOProvider.psm1") -Function Get-ScubaDmarcRecords -Force
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "$($ProviderPath)/ExportEXOProvider.psm1") `
+    -Function 'Get-ScubaDmarcRecord' -Force
 
 InModuleScope 'ExportEXOProvider' {
-    Describe -Tag 'ExportEXOProvider' -Name "Get-ScubaDmarcRecords" {
-        It "TODO return DMARC records" {
-            # Get-ScubaDmarcRecords
-            $true | Should -Be $true
+    Describe -Tag 'ExportEXOProvider' -Name "Get-ScubaDmarcRecord" {
+        BeforeAll {
+            Mock -CommandName Write-Warning {}
+        }
+        Context "When high-confidence answers are available" {
+            BeforeAll {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @("v=DMARC1...");
+                        "HighConfidence" = $true;
+                        "LogEntries" = @("some text")
+                    }
+                }
+            }
+            It "Resolves 1 domain name" {
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
+                $Response.rdata -Contains "v=DMARC1..." | Should -Be $true
+            }
+
+            It "Resolves multiple domain names" {
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"},
+                    @{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 2
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
+                $Response.rdata -Contains "v=DMARC1..." | Should -Be $true
+            }
+        }
+        Context "When high-confidence answers are not available" {
+            BeforeAll {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @("v=DMARC1...");
+                        "HighConfidence" = $false;
+                        "LogEntries" = @("some text")
+                    }
+                }
+            }
+            It "Prints a warning" {
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 1
+                $Response.rdata -Contains "v=DMARC1..." | Should -Be $true
+            }
+        }
+        Context "When the DMARC record is unavailable the full domain" {
+            BeforeAll {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    Mock -CommandName Invoke-RobustDnsTxt {
+                        if ($Qname -eq "_dmarc.example.com") {
+                            @{
+                                "Answers" = @("v=DMARC1...");
+                                "HighConfidence" = $true;
+                                "LogEntries" = @("some text")
+                            }
+                        }
+                        else {
+                            @{
+                                "Answers" = @();
+                                "HighConfidence" = $true;
+                                "LogEntries" = @("Query returned NXDomain")
+                            }
+                        }
+                    }
+                }
+            }
+            It "Checks at the organization level" {
+                $Response = Get-ScubaDmarcRecord -Domains @(@{"DomainName" = "a.b.example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 2
+                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
+                $Response.rdata -Contains "v=DMARC1..." | Should -Be $true
+            }
         }
     }
 }
