89c0524ef76829dec8d9122069c6d4db62862321
bosesubham2011 <bosesubham2011@gmail.com>
2024-06-13 05:07:20 +0530

    updated code structure for scalibility

--- Testing/AdvancedUnitTests/Connections/Connection.tests.ps1
+++ Testing/AdvancedUnitTests/Connections/Connection.tests.ps1
@@ -5,7 +5,7 @@ Describe "Connection Tests: "{
     $script:test=0
     $script:test++
     It "$("$script:test".padleft(3, '0')): Syntax test"{
-            $script:syntax = Get-Content "$PSScriptRoot\Connect-Tenant-Mock-Data.txt" |Out-String
+            $script:syntax = Get-Content "$PSScriptRoot\MockData\SyntaxMockData\Connect-Tenant-Mock-Data.txt" |Out-String
             (Get-Command -Syntax Connect-Tenant).Trim()| Should -Be $script:syntax #Can be changed if someone gives me the output of the command before the pipeline
         }
     $script:test++
@@ -31,3 +31,34 @@ Describe "Connection Tests: "{
              }
             }
     }
+
+    Describe "Connection Tests: Disconnect-SCuBATenant"{
+        $script:test=0
+        $script:test++
+        It "$("$script:test".padleft(3, '0')): Syntax test"{
+                $script:syntax = Get-Content "$PSScriptRoot\MockData\SyntaxMockData\Disconnect-SCuBATenant-Mock-Data.txt" |Out-String
+                (Get-Command -Syntax Disconnect-SCuBATenant).Trim()| Should -Be $script:syntax #Can be changed if someone gives me the output of the command before the pipeline
+            }
+        $script:test++
+        It "$("$script:test".padleft(3, '0')): Compatibility with test objects - Success"{
+            try{
+                Disconnect-SCuBATenant -ProductNames "teams"
+                $true| Should -BeTrue  #Can be changed if someone gives me the output of Get-DomainComputer in xml format, after export the object via $object|Export-CliXML "<Path>"
+             }
+             catch{
+                Write-Host "$($_.exception.message) $($_.errordetails.message)"
+                $false| Should -BeTrue
+             }
+            }
+        $script:test++
+        It "$("$script:test".padleft(3, '0')): Compatibility with test objects - failure"{
+            try{
+                Disconnect-SCuBATenant -ProductNames "teams-fail" 
+                 $false| Should -BeTrue  #Can be changed if someone gives me the output of Get-DomainComputer in xml format, after export the object via $object|Export-CliXML "<Path>"
+                }
+            catch{
+                  Write-Host "$($_.exception.message) $($_.errordetails.message)"
+                  $true| Should -BeTrue
+                 }
+                }
+        }
--- Testing/AdvancedUnitTests/Connections/ConnectionHelper.tests.ps1
+++ Testing/AdvancedUnitTests/Connections/ConnectionHelper.tests.ps1
@@ -1,11 +1,11 @@
 Import-Module "$PSScriptRoot\..\..\..\PowerShell\ScubaGear\Modules\Connection\ConnectHelpers.psm1"
 Import-Module "$PSScriptRoot\..\pester-installables\Pester\src\Pester.psd1"
 
-Describe "Connection Helper Tests: "{
+Describe "Connection Helper Tests: Connect-EXOHelper "{
     $script:test=0
     $script:test++
     It "$("$script:test".padleft(3, '0')): Syntax test"{
-            $script:syntax = Get-Content "$PSScriptRoot\ConnectionHelper-mock-data.txt" |Out-String
+            $script:syntax = Get-Content "$PSScriptRoot\MockData\SyntaxMockData\ConnectionHelper-mock-data.txt" |Out-String
             (Get-Command -Syntax Connect-EXOHelper).Trim()| Should -Be $script:syntax #Can be changed if someone gives me the output of the command before the pipeline
         }
     $script:test++
@@ -31,3 +31,36 @@ Describe "Connection Helper Tests: "{
              }
             }
     }
+
+    Describe "Connection Helper Tests: Connect-DefenderHelper"{
+        $script:test=0
+        $script:test++
+        It "$("$script:test".padleft(3, '0')): Syntax test"{
+                $script:syntax = Get-Content "$PSScriptRoot\MockData\SyntaxMockData\Connect-DefenderHelper-Mock-Data.txt" |Out-String
+                (Get-Command -Syntax Connect-DefenderHelper).Trim()| Should -Be $script:syntax #Can be changed if someone gives me the output of the command before the pipeline
+            }
+        $script:test++
+        It "$("$script:test".padleft(3, '0')): Compatibility with test objects - Success"{
+            try{
+                Connect-DefenderHelper -M365Environment "commercial" 
+                $true| Should -BeTrue  #Can be changed if someone gives me the output of Get-DomainComputer in xml format, after export the object via $object|Export-CliXML "<Path>"
+             }
+             catch{
+                Write-Host "$($_.exception.message) $($_.errordetails.message)"
+                $false| Should -BeTrue
+             }
+            }
+        $script:test++
+        It "$("$script:test".padleft(3, '0')): Compatibility with test objects - failure"{
+            try{
+                Connect-DefenderHelper -M365Environment "commercial-fail" 
+                 $false| Should -BeTrue  #Can be changed if someone gives me the output of Get-DomainComputer in xml format, after export the object via $object|Export-CliXML "<Path>"
+                }
+            catch{
+                  Write-Host "$($_.exception.message) $($_.errordetails.message)"
+                  $true| Should -BeTrue
+                 }
+                }
+        }
+
+    Connect-DefenderHelper
