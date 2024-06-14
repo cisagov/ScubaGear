c8a2f5dbb51e5cb0d42f26032bd87558cfcce152
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-06-14 13:31:04 -0400

    Create nightly functions

---
+++ utils/workflow/Prepare-ScubaGear.psm1
@@ -0,0 +1,20 @@
+function Install-Selenium 
+{
+  Install-Module -Name Selenium -Scope CurrentUser -Force
+  Import-Module -Name Selenium
+  ./Testing/Functional/SmokeTest/UpdateSelenium.ps1
+  # Workaround for Selenium. Loading psm1 instead of psd1
+  Import-Module -Name (Get-Module -Name Selenium -ListAvailable).Path -Force
+}
+
+function Setup-ScubaGear
+{
+  Import-Module -Name .\PowerShell\ScubaGear
+  Initialize-SCuBA
+}
+
+Export-ModuleMember -Function
+{
+  'Install-Selenium',
+  'Setup-ScubaGear'
+}
