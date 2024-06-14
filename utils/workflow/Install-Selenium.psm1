b394ae80092d23716263dde499c6c2c35a215b8a
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-06-14 15:10:25 -0400

    Solve path

--- utils/workflow/Install-Selenium.psm1
+++ utils/workflow/Install-Selenium.psm1
@@ -2,7 +2,12 @@ function Install-Selenium
 {
   Install-Module -Name Selenium -Scope CurrentUser -Force
   Import-Module -Name Selenium
-  ..\..\Testing\Functional\SmokeTest\UpdateSelenium.ps1
+  Get-Location
+  $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '..\..' -Resolve
+  Write-Host 'Repo Root Path is'
+  Write-Host $RepoRootPath
+  Join-Path -Path $RepoRootPath -ChildPath 'Testing/Functional/SmokeTest/UpdateSelenium.ps1'
+  # ..\..\Testing\Functional\SmokeTest\UpdateSelenium.ps1
   # Workaround for Selenium. Loading psm1 instead of psd1
   Import-Module -Name (Get-Module -Name Selenium -ListAvailable).Path -Force
 }
