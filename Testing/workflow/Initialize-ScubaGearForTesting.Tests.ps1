0c444e456ccd604c1aa38963028061a4ec10ad5f
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-06-18 09:31:19 -0400

    Try loop with global

--- Testing/workflow/Initialize-ScubaGearForTesting.Tests.ps1
+++ Testing/workflow/Initialize-ScubaGearForTesting.Tests.ps1
@@ -3,11 +3,27 @@ BeforeDiscovery {
   Initialize-ScubaGearForTesting
 }
 
-$global:moduleName = 'MicrosoftTeams'
+# Use Write-Warning b/c other writes don't actually write
+Write-Warning 'Getting required modules...'
+try {
+  . PowerShell\ScubaGear\RequiredVersions.ps1
+}
+catch {
+  throw "Unable to find RequiredVersions.ps1"
+}
+if ($ModuleList) {
+  Write-Warning 'Found list of modules'
+}
+else {
+  Write-Warning 'Did NOT find list of modules'
+}
 
 Describe "Check for installed PowerShell modules" {
-  It "Module $global:moduleName should be installed" {
-    $module = Get-Module -ListAvailable -Name $global:moduleName
-    $module | Should -Not -BeNullOrEmpty
+  foreach ($Module in $ModuleList) {
+    $global:ModuleName = $Module.ModuleName
+    It "Module $global:moduleName should be installed" {
+      $module = Get-Module -ListAvailable -Name $global:ModuleName
+      $module | Should -Not -BeNullOrEmpty
+    }
   }
 }
