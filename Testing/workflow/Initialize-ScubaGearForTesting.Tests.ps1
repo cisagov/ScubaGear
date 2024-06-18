62fe70c2956b073e48b964ee08edc8d8925cae4f
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-06-18 08:58:38 -0400

    Simplify

--- Testing/workflow/Initialize-ScubaGearForTesting.Tests.ps1
+++ Testing/workflow/Initialize-ScubaGearForTesting.Tests.ps1
@@ -3,72 +3,11 @@ BeforeDiscovery {
   Initialize-ScubaGearForTesting
 }
 
-Describe 'Check for installed modules' {
-  Write-Warning 'Getting required modules...'
-  try {
-    . PowerShell\ScubaGear\RequiredVersions.ps1
-  }
-  catch {
-    throw "Unable to find RequiredVersions.ps1"
-  }
-  if ($ModuleList) {
-    Write-Warning 'Found list of modules'
-  }
-  else {
-    Write-Warning 'Did NOT find list of modules'
-  }
-  # foreach ($Module in $ModuleList) {
-  #   $ModuleName = $Module.ModuleName
-  #   # It "$ModuleName should be installed." {
-  #   #   Get-Module -ListAvailable -Name "$(ModuleName)" | Should -BeTrue
-  #   # }
-  #   It "Module $ModuleName should be installed" {
-  #       $module = Get-Module -ListAvailable -Name $ModuleName
-  #       $module | Should -Not -BeNullOrEmpty
-  #   }
-  # }
-  $ModuleName = "MicrosoftTeams"
-  It "Module $ModuleName should be installed" {
-      $module = Get-Module -ListAvailable -Name $ModuleName
-      $module | Should -Not -BeNullOrEmpty
-  }
-  # It 'MicrosoftTeams should be installed' {
-  #   Get-Module -ListAvailable -Name 'MicrosoftTeams' | Should -BeTrue
-  # }
-  # It 'ExchangeOnlineManagement should be installed' {
-  #   Get-Module -ListAvailable -Name 'ExchangeOnlineManagement' | Should -BeTrue
-  # }
-  # It 'Microsoft.Online.SharePoint.PowerShell should be installed' {
-  #   Get-Module -ListAvailable -Name 'Microsoft.Online.SharePoint.PowerShell' | Should -BeTrue
-  # }
-  # It 'PnP.PowerShell should be installed' {
-  #   Get-Module -ListAvailable -Name 'PnP.PowerShell' | Should -BeTrue
-  # }
-  # It 'Microsoft.PowerApps.Administration.PowerShell should be installed' {
-  #   Get-Module -ListAvailable -Name 'Microsoft.PowerApps.Administration.PowerShell' | Should -BeTrue
-  # }
-  # It 'Microsoft.PowerApps.PowerShell should be installed' {
-  #   Get-Module -ListAvailable -Name 'Microsoft.PowerApps.PowerShell' | Should -BeTrue
-  # }
-  # It 'Microsoft.Graph.Authentication should be installed' {
-  #   Get-Module -ListAvailable -Name 'Microsoft.Graph.Authentication' | Should -BeTrue
-  # }
-  # It 'Microsoft.Graph.Beta.Users should be installed' {
-  #   Get-Module -ListAvailable -Name 'Microsoft.Graph.Beta.Users' | Should -BeTrue
-  # }
-  # It 'Microsoft.Graph.Beta.Groups should be installed' {
-  #   Get-Module -ListAvailable -Name 'Microsoft.Graph.Beta.Groups' | Should -BeTrue
-  # }
-  # It 'Microsoft.Graph.Beta.Identity.DirectoryManagement should be installed' {
-  #   Get-Module -ListAvailable -Name 'Microsoft.Graph.Beta.Identity.DirectoryManagement' | Should -BeTrue
-  # }
-  # It 'Microsoft.Graph.Beta.Identity.Governance should be installed' {
-  #   Get-Module -ListAvailable -Name 'Microsoft.Graph.Beta.Identity.Governance' | Should -BeTrue
-  # }
-  # It 'Microsoft.Graph.Beta.Identity.SignIns should be installed' {
-  #   Get-Module -ListAvailable -Name 'Microsoft.Graph.Beta.Identity.SignIns' | Should -BeTrue
-  # }
-  # It 'powershell-yaml should be installed' {
-  #   Get-Module -ListAvailable -Name 'powershell-yaml' | Should -BeTrue
-  # }
+$moduleName = 'MicrosoftTeams'
+
+Describe "Check for installed PowerShell module" {
+    It "Module $moduleName should be installed" {
+        $module = Get-Module -ListAvailable -Name $moduleName
+        $module | Should -Not -BeNullOrEmpty
+    }
 }
