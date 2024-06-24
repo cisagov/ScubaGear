82b785a9a13664219d6f4e62ae49b294bfb2086e
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-06-24 12:34:25 -0400

    file list

--- utils/DeployUtils.ps1
+++ utils/DeployUtils.ps1
@@ -379,7 +379,8 @@ function SignScubaGearModule {
     $FileList = CreateFileList `
         -SourcePath $ModulePath `
         -Extensions "*.ps1", "*.psm1", "*.psd1"  # Array of extensions
-    
+    Write-Host "The file list is $FileList"
+
     Write-Host ">> Calling CallAzureSignTool function to sign scripts, manifest, and modules..."
     CallAzureSignTool `
         -AzureKeyVaultUrl $AzureKeyVaultUrl `
@@ -397,7 +398,7 @@ function SignScubaGearModule {
 
     # New-FileCatlog creates a Windows catalog file (.cat) containing cryptographic hashes 
     # for files and folders in the specified paths.
-    $CatalogFilePath = New-FileCatalog -Path $ModulePath -CatalogFilePath $CatalogFilePath -CatalogVersion 2.0
+    $CatalogFilePath = New-FileCatalog -Path $ModulePath -CatalogFilePath $CatalogFilePath -CatalogVersion 2.0 -Verbose
     Write-Host ">> The catalog path is $CatalogFilePath"
     $CatalogList = New-TemporaryFile
     $CatalogFilePath.FullName | Out-File -FilePath $CatalogList -Encoding utf8 -Force
@@ -414,19 +415,21 @@ function SignScubaGearModule {
     # Signing tool says it was successful, but the test says it was not.
     Write-Host ">> Testing the catalog"
     # There's no -Path parameter.
-    $TestResult = Test-FileCatalog -CatalogFilePath $CatalogFilePath -Path $ModulePath -Detailed
+    Test-FileCatalog -CatalogFilePath $CatalogFilePath -Path $ModulePath -Detailed
+    # $TestResult = Test-FileCatalog -CatalogFilePath $CatalogFilePath -Path $ModulePath -Detailed
 
-    ForEach ($File in $FileList) {
-        Write-Host $File
-    }
-    if ($TestResult -eq 'Valid') {
-        Write-Host ">> Signing the module was successful."
-        return True
-    }
-    else {
-        Write-Host ">> Signing the module was NOT successful."
-        return False
-    }
+    # ForEach ($File in $FileList) {
+    #     Write-Host $File
+    # }
+    # if ($TestResult -eq 'Valid') {
+    #     Write-Host ">> Signing the module was successful."
+    #     return True
+    # }
+    # else {
+    #     Write-Host ">> Signing the module was NOT successful."
+    #     return False
+    # }
+    return False
 }
 
 function CallAzureSignTool {
