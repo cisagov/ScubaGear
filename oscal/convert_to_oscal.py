fa52053aa42c5b4bb31adc5eaa81024dc88b2d9b
Alden Hilton <adhilto@sandia.gov>
2024-07-09 17:48:31 -0700

    Added SAP generation

--- oscal/convert_to_oscal.py
+++ oscal/convert_to_oscal.py
@@ -1,21 +1,39 @@
 # Refer to https://pages.nist.gov/OSCAL-Reference/models/v1.1.2/assessment-results/json-reference/
+# and https://pages.nist.gov/OSCAL-Reference/models/v1.1.2/assessment-plan/json-reference/
 #
 # To validate:
-# ajv validate -s .\oscal_assessment-results_schema.json -d .\results_oscal.json -c ajv-formats
+# ajv validate -s .\oscal_assessment-results_schema.json -d .\sar.json -c ajv-formats
+# or
+# ajv validate -s .\oscal_assessment-results_schema.json -d .\sar.json -c ajv-formats
+#
 # See https://github.com/usnistgov/OSCAL/blob/main/README_validations.md
 
 import os
 import json
 import uuid
 import argparse
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input")
-parser.add_argument("output")
 args = parser.parse_args()
 
 with open(args.input, 'r', encoding='utf-8-sig') as f:
-    results = json.load(f)
+    raw_results = json.load(f)
+
+####################
+# Define constants #
+####################
+
+OSCAL_VERSION = "1.1.2"
+
+SSP_UUID = str(uuid.uuid4())
+SSP_TITLE = f"{raw_results['MetaData']['DisplayName']} SCuBA M365 Baseline System Security Plan"
+
+SAP_UUID = str(uuid.uuid4())
+SAP_TITLE = f"{raw_results['MetaData']['DisplayName']} SCuBA M365 Baseline Assesment Plan"
+
+LAST_MODIFIED = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
 # The props sections are where we have freedom to add custom values.
 # If you define your own value, you need to specify a unique namespace, "ns",
@@ -30,15 +48,208 @@ with open(args.input, 'r', encoding='utf-8-sig') as f:
 # publish something at that URI
 NAME_SPACE = "https://github.com/cisagov/ScubaGear/tree/oscal-exploration/oscal/ns/"
 
-#
-# Save the locations
-#
+# Per https://pages.nist.gov/OSCAL-Reference/models/v1.1.2/assessment-plan/json-reference/#/assessment-plan/assessment-assets/assessment-platforms/uuid
+# the assessment assets have per-subject consistency,
+# (see https://pages.nist.gov/OSCAL/resources/concepts/identifier-use/#consistency)
+# meaning it "should only be changed if the underlying identified subject has changed
+# in a significant way that no longer represents the same identified subject."
+POWERSHELL_UUID = '5b6e9b9e-828d-49ab-8767-5b82b63ee505'
+REGO_UUID = '6628dfaf-cacf-48d2-bbe9-b551b31b400d'
+SCUBAGEAR_UUID = '8b420415-a7cb-4cea-84d3-c691a2e19225'
+
+OUTPUT_FOLDER = os.path.dirname(args.input)
+
+##################
+# Create the SAP #
+##################
+
+# Initialize the SAP structure
+sap = {"assessment-plan": {}}
+
+# Save the UUID
+sap['assessment-plan']['uuid'] = SAP_UUID
+
+# Define the roles
+roles = [
+    {
+        "id": "prepared-for",
+        "title": raw_results['MetaData']['DisplayName'],
+        "description": "The display name of the M365 tenant being assessed by ScubaGear."
+    },
+    {
+        "id": "prepared-by",
+        # TODO this should probably be a person/org, not the tool
+        "title": f"{raw_results['MetaData']['Tool']} {raw_results['MetaData']['ToolVersion']}"
+    }
+]
+
+# Save the metadata
+sap['assessment-plan']["metadata"] = {
+    "title": SAP_TITLE,
+    "last-modified": LAST_MODIFIED,
+    "version": f"{raw_results['MetaData']['Tool']} {raw_results['MetaData']['ToolVersion']}",
+    "oscal-version": OSCAL_VERSION,
+    "roles": roles
+}
+
+# Save a reference to the SSP
+sap['assessment-plan']['import-ssp'] = {
+    "href": f"#{SSP_UUID}"
+}
+
+# Save the control selections
+sap['assessment-plan']['reviewed-controls'] = {}
+sap['assessment-plan']['reviewed-controls']['control-selections'] = []
+for product in raw_results['MetaData']['ProductsAssessed']:
+    selection = {
+        "description": f"Include all controls in the {product} baseline, except those that cannot be" \
+            "checked via ScubaGear.", # TODO also exclude those excluded via config file, once that feature is complete
+        "include-all": {}
+    }
+    excluded_controls = []
+    product_abbr = raw_results['MetaData']['ProductAbbreviationMapping'][product]
+    for group in raw_results['Results'][product_abbr]:
+        for control in group['Controls']:
+            if 'Not-Implemented' in control['Criticality']:
+                excluded_controls.append({'control-id': control["Control ID"]})
+    if len(excluded_controls) > 0:
+        selection['exclude-controls'] = excluded_controls
+    sap['assessment-plan']['reviewed-controls']['control-selections'].append(selection)
+
+# Save the control objective selections
+# TODO I'm not completely confident on the difference between control-selections and control-objective-selections
+sap['assessment-plan']['reviewed-controls']['control-objective-selections'] = [ {"include-all": {}} ]
+
+# Save the assessment subjects
+sap['assessment-plan']['assessment-subjects'] = []
+
+# Add the M365 tenant itself as a subject
+sap['assessment-plan']['assessment-subjects'].append({
+    # TODO type component means it's been definded as a component inthe SSP, which hasn't been written yet
+    # Once it has been written many of these details might be moved there
+    "type": "component",
+    # "title": "M365 tenant",
+    "description": "The M365 tenant being assessed.",
+    "props": [
+        {
+            "name": "TenantId",
+            "ns": NAME_SPACE,
+            "value": raw_results['MetaData']['TenantId']
+        },
+        {
+            "name": "DisplayName",
+            "ns": NAME_SPACE,
+            "value": raw_results['MetaData']['DisplayName']
+        },
+        {
+            "name": "DomainName",
+            "ns": NAME_SPACE,
+            "value": raw_results['MetaData']['DomainName']
+        }
+    ]
+})
+
+# Add the individual products as components
+for product in raw_results['MetaData']['ProductsAssessed']:
+    sap['assessment-plan']['assessment-subjects'].append({
+        # TODO type component means this has been defined in the SSP
+        "type": "component",
+        "description": product
+    })
+
+# Assessment assets
+sap['assessment-plan']['assessment-assets'] = {}
+
+sap['assessment-plan']['assessment-assets']['components'] = []
+sap['assessment-plan']['assessment-assets']['components'].append({
+    "uuid": POWERSHELL_UUID,
+    "type": "software",
+    "title": "PowerShell",
+    "description": "The PowerShell scripting language.",
+    "status": {
+        "state": "operational"
+    }
+})
+sap['assessment-plan']['assessment-assets']['components'].append({
+    "uuid": REGO_UUID,
+    "type": "software",
+    "title": "OPA Rego",
+    "description": "Rego is a query language designed for evaluating policy.",
+    "status": {
+        "state": "operational"
+    }
+})
+
+sap['assessment-plan']['assessment-assets']['assessment-platforms'] = []
+sap['assessment-plan']['assessment-assets']['assessment-platforms'].append({
+        "uuid": SCUBAGEAR_UUID,
+        "title": raw_results['MetaData']['Tool'],
+        "props": [
+            {
+                "name": "version",
+                "value": raw_results['MetaData']['ToolVersion']
+            }
+        ],
+        'uses-components': [{'component-uuid': component['uuid']} for component in
+            sap['assessment-plan']['assessment-assets']['components']]
+})
+
+# Add the tasks
+sap['assessment-plan']['tasks'] = [
+    {
+        "uuid": str(uuid.uuid4()),
+        'type': "action",
+        "title": "Run ScubaGear"
+    }
+]
+
+# Backmatter
+sap['assessment-plan']['back-matter'] = { "resources": [] }
+
+sap['assessment-plan']['back-matter']['resources'].append({
+    'uuid': SSP_UUID,
+    'title': SSP_TITLE,
+    'props': [
+        {
+            'name': 'type',
+            'value': 'system-security-plan'
+        }
+    ],
+    'rlinks': [
+        {
+            "href": f"./ssp.json",
+            "media-type": "text/json"
+        }
+    ]
+})
+
+sap['assessment-plan']['back-matter']['resources'].append({
+    "uuid": str(uuid.uuid4()),
+    "title": "ScubaGear Github repository",
+    'rlinks': [
+        {
+            "href": "https://github.com/cisagov/ScubaGear",
+            "media-type": "text/html"
+        }
+    ]
+})
+
+# Save the SAP
+with open(os.path.join(OUTPUT_FOLDER, "sap.json"), 'w', encoding='utf-8') as f:
+    json.dump(sap, f, indent=4)
+
+
+##################
+# Create the SAR #
+##################
+
+# Define the locations
 locations = []
-for tenant in results['Raw']['tenant_details']:
-    if tenant['DisplayName'] == results['MetaData']['DisplayName']:
+for tenant in raw_results['Raw']['tenant_details']:
+    if tenant['DisplayName'] == raw_results['MetaData']['DisplayName']:
         location = {
             "uuid": str(uuid.uuid4()),
-            "title": results['MetaData']['DisplayName'],
+            "title": raw_results['MetaData']['DisplayName'],
             "address": {
                 "type": "work",
                 "addr-lines": [tenant['AADAdditionalData']['Street']],
@@ -51,91 +262,42 @@ for tenant in results['Raw']['tenant_details']:
         }
     locations.append(location)
 
-#
-# Initialize the OSCAL data structure
-#
-oscal = {
-    "assessment-results": {
-        "uuid": str(uuid.uuid4()),
-        "metadata": {
-            "title": f"{results['MetaData']['DisplayName']} SCuBA M365 Baseline Assesment Results",
-            "published": results['MetaData']['TimestampZulu'],
-            "last-modified": results['MetaData']['TimestampZulu'],
-            "version": f"{results['MetaData']['Tool']} {results['MetaData']['ToolVersion']}",
-            "oscal-version": "1.1.2",
-            "props": [
-                {
-                    "name": "marking",
-                    # Real agency data would probably be CUI. Should that be the default marking?
-                    # Or should it be something that ScubaConnect adds?
-                    # Should ScubaGear omit this field by default but allow it to be specified as a parameter?
-                    # TODO open question
-                    "value": "Controlled Unclassified Information"
-                },
-                {
-                    "name": "TenantId",
-                    "ns": NAME_SPACE,
-                    "value": results['MetaData']['TenantId']
-                },
-                {
-                    "name": "DisplayName",
-                    "ns": NAME_SPACE,
-                    "value": results['MetaData']['DisplayName']
-                },
-                {
-                    "name": "DomainName",
-                    "ns": NAME_SPACE,
-                    "value": results['MetaData']['DomainName']
-                }
-            ],
-            "roles": [
-                {
-                    "id": "prepared-for",
-                    "title": results['MetaData']['DisplayName'],
-                    "description": "The display name of the M365 tenant being assessed by ScubaGear."
-                },
-                {
-                    # The value I put here is dependent on data inserted by ScubaConnect. Is that ok?
-                    # TODO open question
-                    # If CISA ran this as part of ScubaConnect, should it say prepared-by CISA here?
-                    # Should it just say "ScubaGear v1.3.0 instead?"
-                    # TODO open question
-                    "id": "prepared-by",
-                    "title": f"{results['MetaData']['AgencyName']}, {results['MetaData']['SubAgencyName']}",
-                    "description": "The entity that ran ScubaGear"
-                }
-            ],
-            "locations": locations
-        }
-    }
+# Initialize the SAR data structure
+sar = { "assessment-results": {} }
+
+# Add the uuid
+sar['assessment-results']['uuid'] = str(uuid.uuid4())
+
+# Add the metadata
+sar["assessment-results"]["metadata"] = {
+    "title": f"{raw_results['MetaData']['DisplayName']} SCuBA M365 Baseline Assesment Results",
+    "published": raw_results['MetaData']['TimestampZulu'],
+    "last-modified": LAST_MODIFIED,
+    "version": f"{raw_results['MetaData']['Tool']} {raw_results['MetaData']['ToolVersion']}",
+    "oscal-version": OSCAL_VERSION,
+    "roles": roles,
+    "locations": locations
 }
 
-# Do we need to include "parties" and "responsible-parties"? TODO open question
-
-# Reference the the SAP, described in the back-matter
-sap_uuid = str(uuid.uuid4())
-oscal['assessment-results']['import-ap'] = {
-    'href': f'#{sap_uuid}'
+# Reference the the SAP
+# The "#" before the uuid tells you to look for that UUID
+# elsewhere in the SAR for details on where to find the SAP
+sar['assessment-results']['import-ap'] = {
+    'href': f'#{SAP_UUID}'
 }
 
-# local-definitions? TODO open question. Looks like they include a lot of SAP stuff in the template. Why?
-# This is the SAR after all.
-
-
-#
 # Add the results section
-#
-oscal['assessment-results']['results'] = []
-for product in results['MetaData']['ProductsAssessed']:
+sar['assessment-results']['results'] = []
+for product in raw_results['MetaData']['ProductsAssessed']:
     product_uuid = str(uuid.uuid4())
-    product_abbr = results['MetaData']['ProductAbbreviationMapping'][product]
+    product_abbr = raw_results['MetaData']['ProductAbbreviationMapping'][product]
 
     observations = []
     findings = []
     excluded_controls = []
 
     # TODO can we save the group info somewhere?
-    for group in results['Results'][product_abbr]:
+    for group in raw_results['Results'][product_abbr]:
         for control in group['Controls']:
             if 'Not-Implemented' not in control['Criticality']:
                 observation_uuid = str(uuid.uuid4())
@@ -147,7 +309,7 @@ for product in results['MetaData']['ProductsAssessed']:
                     'description': control['Details'],
                     'methods': ["Test"],
                     'types': ['statement-objective'],
-                    'collected': results['MetaData']['TimestampZulu']
+                    'collected': raw_results['MetaData']['TimestampZulu']
                     # 'origins' TODO
                     # 'related-tasks' TODO
                     # 'relevant-evidence' TODO In an ideal world, I think we would refer to the appropriate section
@@ -193,44 +355,8 @@ for product in results['MetaData']['ProductsAssessed']:
         "uuid": str(uuid.uuid4()),
         "title": f"SCuBA M365 {product} Baseline Assesment Results",
         "description": f"Results of assessment performed by ScubaGear for the {product} baseline",
-        "start": results['MetaData']['TimestampZulu'],
-        "end": results['MetaData']['TimestampZulu'],
-        "local-definitions": {
-            # A remark in the example SAR says that ideally all assessment platforms and components
-            # would be defined in the SAP, but we can define them here. Would we want to include
-            # them here? TODO open question
-            "assessment-assets": {
-                # "components": [
-                #     {
-                #         "uuid": product_uuid,
-                #         # Not sure what to put here for "type". TODO open question
-                #         "type": "SaaS offering",
-                #         "title": product_abbr,
-                #         "description": product
-                #     }
-                # ],
-                "assessment-platforms": [
-                    {
-                        # Should ScubaGear has the same UUID between runs? Or maybe it changes with each new
-                        # release? Or is a new random value per run ok? TODO open question.
-                        "uuid": str(uuid.uuid4()),
-                        "title": results['MetaData']['Tool'],
-                        "uses-components": [
-                            {
-                                "component-uuid": product_uuid
-                            }
-                        ],
-                        "props": [
-                            {
-                                "name": "version",
-                                "ns": NAME_SPACE,
-                                "value": results['MetaData']['ToolVersion']
-                            }
-                        ]
-                    }
-                ]
-            }
-        },
+        "start": raw_results['MetaData']['TimestampZulu'],
+        "end": raw_results['MetaData']['TimestampZulu'],
         "reviewed-controls": {
             "control-selections": [
                 {
@@ -252,8 +378,8 @@ for product in results['MetaData']['ProductsAssessed']:
                 {
                     "uuid": str(uuid.uuid4()),
                     "title": "Run ScubaGear",
-                    "start": results['MetaData']['TimestampZulu'],
-                    "end": results['MetaData']['TimestampZulu']
+                    "start": raw_results['MetaData']['TimestampZulu'],
+                    "end": raw_results['MetaData']['TimestampZulu']
                 }
             ]
         },
@@ -264,59 +390,54 @@ for product in results['MetaData']['ProductsAssessed']:
     if len(excluded_controls) > 0:
         result['reviewed-controls']['control-selections'][0]['exclude-controls'] = excluded_controls
 
-    oscal['assessment-results']['results'].append(result)
+    sar['assessment-results']['results'].append(result)
 
 
 
-# Generate a relative path from the location of the OSCAL output the json input
-output_folder = os.path.dirname(args.output)
-relative_path = os.path.relpath(output_folder, args.input)
+# Generate a relative path from the location of the SAR file the json file
 input_fname = os.path.basename(args.input)
-path_to_raw = os.path.join(relative_path, input_fname).replace('\\', '/')
+path_to_raw = f"./{input_fname}"
 
 # Add back-matter
-oscal['assessment-results']['back-matter'] = {
-    'resources': [
+sar['assessment-results']['back-matter'] = { "resources": [] }
+sar['assessment-results']['back-matter']['resources'].append({
+    'uuid': SAP_UUID,
+    'title': SAP_TITLE,
+    'props': [
         {
-            'uuid': sap_uuid,
-            'title': "TODO",
-            'props': [
-                {
-                    'name': 'type',
-                    'value': 'security-assessment-plan'
-                },
-                {
-                    'name': 'published',
-                    'value': 'TODO'
-                },
-                {
-                    'name': 'version',
-                    'value': 'TODO'
-                }
-            ]
-        },
+            'name': 'type',
+            'value': 'security-assessment-plan'
+        }
+    ],
+    'rlinks': [
         {
-            "uuid": str(uuid.uuid4()),
-            "title": "ScubaGear Github repository",
-            'rlinks': [
-                {
-                    "href": "https://github.com/cisagov/ScubaGear",
-                    "media-type": "text/html"
-                }
-            ]
-        },
-        {
-            "uuid": str(uuid.uuid4()),
-            "title": "Raw ScubaGear output",
-            'rlinks': [
-                {
-                    "href": path_to_raw,
-                    "media-type": "text/json"
-                }
-            ]
+            "href": f"./sap.json",
+            "media-type": "text/json"
         }
     ]
-}
+})
 
-with open(args.output, 'w', encoding='utf-8') as f:
-    json.dump(oscal, f, indent=4)
+sar['assessment-results']['back-matter']['resources'].append({
+    "uuid": str(uuid.uuid4()),
+    "title": "ScubaGear Github repository",
+    'rlinks': [
+        {
+            "href": "https://github.com/cisagov/ScubaGear",
+            "media-type": "text/html"
+        }
+    ]
+})
+
+sar['assessment-results']['back-matter']['resources'].append({
+    "uuid": str(uuid.uuid4()),
+    "title": "Raw ScubaGear output",
+    'rlinks': [
+        {
+            "href": path_to_raw,
+            "media-type": "text/json"
+        }
+    ]
+})
+
+with open(os.path.join(OUTPUT_FOLDER, "sar.json"), 'w', encoding='utf-8') as f:
+    json.dump(sar, f, indent=4)
