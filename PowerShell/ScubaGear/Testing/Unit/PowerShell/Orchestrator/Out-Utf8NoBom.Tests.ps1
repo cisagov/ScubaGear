850d73d63d0c9a62ffb44f90078c20eab4668cc5
Addam Schroll <108814318+schrolla@users.noreply.github.com>
2024-08-30 12:42:06 -0500

    Added multiple tests for relative path construction

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Out-Utf8NoBom.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Out-Utf8NoBom.Tests.ps1
@@ -16,24 +16,64 @@ InModuleScope Orchestrator {
         }
 
         Context 'local file' {
-            It 'Write to root drive path' {
+            It 'Set to root drive path' {
                 Out-Utf8NoBom -Content "test" -Location "C:\" -FileName "output.json" `
                 | Should -Be "C:\output.json"
             }
 
-            It 'Write to local subdirectory path' {
-                Out-Utf8NoBom -Content "test" -Location "a" -FileName "output.json" `
+            It 'Set to absolute subdirectory path' {
+                Out-Utf8NoBom -Content "test" -Location "$pwd\a" -FileName "output.json" `
                 | Should -Be "$pwd\a\output.json"
             }
         }
 
-        # Requires ability to create SMB shares on your box to run
+        Context 'relative paths' {
+            It 'Set to subdirectory relative path' {
+                Out-Utf8NoBom -Content "test" -Location "a" -FileName "output.json" `
+                | Should -Be "$pwd\a\output.json"
+            }
+
+            It 'Set to subdirectory local relative path' {
+                Out-Utf8NoBom -Content "test" -Location ".\a" -FileName "output.json" `
+                | Should -Be "$pwd\a\output.json"
+            }
+
+            It 'Set to subdirectory processing .. and .' {
+                Out-Utf8NoBom -Content "test" -Location "a\b\..\..\c\." -FileName "output.json" `
+                | Should -Be "$pwd\c\output.json"
+            }
+        }
+
+        Context 'Check for re-root in path' {
+            It 'Does not re-root subdirectory' {
+                Out-Utf8NoBom -Content "test" -Location "a\d\..\b\." -FileName "output.json" `
+                | Should -Be "$pwd\a\b\output.json"
+            }
+
+            It 'Does not re-root subdirectory rooted at local' {
+                Out-Utf8NoBom -Content "test" -Location ".\a\d\..\b\." -FileName "output.json" `
+                | Should -Be "$pwd\a\b\output.json"
+            }
+
+            It 'Does not re-root parent' {
+                Out-Utf8NoBom -Content "test" -Location "..\$((Get-Item .).Name)\a\d\..\b\." -FileName "output.json" `
+                | Should -Be "$pwd\a\b\output.json"
+            }
+        }
+
+        # Uses default C$ share to test building UNC paths that exist
         Context 'UNC path' {
-            It 'Write to shared UNC path' {
+            It 'Set to shared UNC path' {
+                Out-Utf8NoBom -Content "test" -Location "\\$env:COMPUTERNAME\C$" -FileName "output.json" `
+                | Should -Be "\\$env:COMPUTERNAME\C$\output.json"
+            }
+
+            It 'Set to shared UNC path' {
                 Out-Utf8NoBom -Content "test" -Location "\\$env:COMPUTERNAME\C$" -FileName "output.json" `
                 | Should -Be "\\$env:COMPUTERNAME\C$\output.json"
             }
         }
+
         AfterAll {
             Remove-Module Orchestrator -ErrorAction SilentlyContinue
             Remove-Item -Path "./c" -Force -ErrorAction Ignore
