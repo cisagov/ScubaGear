a8c4411c28e953233dac2af6b58dde5bdfd9fbfa
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-10-28 07:34:23 -0400

    tweak code

--- utils/workflow/Run-PSScriptAnalyzer.ps1
+++ utils/workflow/Run-PSScriptAnalyzer.ps1
@@ -5,41 +5,45 @@ Install-Module -Name PSScriptAnalyzer -Force -Scope CurrentUser
 Import-Module PSScriptAnalyzer
 
 # Get all PowerShell script files in the repository
-$psFiles = Get-ChildItem -Path ./*  -Include *.ps1,*ps1xml,*.psc1,*.psd1,*.psm1,*.pssc,*.psrc,*.cdxml -Recurse
+$PsFiles = Get-ChildItem -Path ./*  -Include *.ps1, *ps1xml, *.psc1, *.psd1, *.psm1, *.pssc, *.psrc, *.cdxml -Recurse
 
 # Analyze each file and collect results
-$results = foreach ($file in $psFiles) {
-    Invoke-ScriptAnalyzer -Path $file.FullName -Settings ./Testing/Linting/PSSA/.powershell-psscriptanalyzer.psd1
+$Results = foreach ($PsFile in $PsFiles) {
+	Invoke-ScriptAnalyzer -Path $PsFile.FullName -Settings ./Testing/Linting/PSSA/.powershell-psscriptanalyzer.psd1
 }
 
 # Report results
-if ($results) {
-    $hasErrors = $false
-    $results | ForEach-Object {
-        Write-Host "File: $($_.ScriptPath)"
-        Write-Host "Line: $($_.Line)"
-        Write-Host "Severity: $($_.Severity)"
-        Write-Host "Message: $($_.Message)"
-        Write-Host "RuleName: $($_.RuleName)"
-        Write-Host "--------------------------"
+if ($Results) {
+	$HasWarnings = $false
+	$HasErrors = $false
+	$results | ForEach-Object {
+		Write-Output "File: $($_.ScriptPath)"
+		Write-Output "Line: $($_.Line)"
+		Write-Output "Severity: $($_.Severity)"
+		Write-Output "Message: $($_.Message)"
+		Write-Output "RuleName: $($_.RuleName)"
+		Write-Output "--------------------------"
 
-        if ($_.Severity -eq 'Error') {
-            $hasErrors = $true
-        }
-    }
-
-    if ($hasErrors) {
-        Write-Host "Errors were found in the PowerShell scripts." -ForegroundColor Red
-        exit 1
-    }
-} else {
-    Write-Host "No issues found in the PowerShell scripts."
+		if ($_.Severity -eq 'Warning') {
+			$HasWarnings = $true
+		}
+		elseif ($_.Severity -eq 'Error') {
+			$HasErrors = $true
+		}
+	}
+	if ($HasWarnings == $true or $HasErrors == $true) {
+		Write-Output "Warnings and/or errors were found in the PowerShell scripts." -ForegroundColor Red
+		exit 1
+	}
+}
+else {
+	Write-Output "No warnings or errors were found in the PowerShell scripts."
 }
 
 Write-Output "`n`n"
 
 # List version of PSSA used.
-Get-Module -ListAvailable | Where-Object {$_.Name -eq "PSScriptAnalyzer"} | Select-Object -Property Name, Version
+Get-Module -ListAvailable | Where-Object { $_.Name -eq "PSScriptAnalyzer" } | Select-Object -Property Name, Version
 
 # Exit with error if any PSSA errors
 If ($Errors -gt 0) {
