79b3bd19d1af78c8a1c442147aa8cafb8363b46d
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-11-15 15:13:05 -0500

    remove alt test

--- Testing/workflow/Invoke-PSSA.Tests.ps1
+++ Testing/workflow/Invoke-PSSA.Tests.ps1
@@ -1,15 +1,6 @@
 # The purpose of this tset is to verify that PSSA is working.
 
-# Suppress PSSA warnings here at the root of the test file.
-# [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
-# param()
-
 BeforeDiscovery {
-  # Arrange to capture the output
-  # $Output = @()
-  # $OriginalWriteOutput = $ExecutionContext.SessionState.InvokeCommand.GetCommand('Write-Output', 'All').ScriptBlock
-  # $ExecutionContext.SessionState.InvokeCommand.SetCommand('Write-Output', { param($Message) $Output += $Message }, 'All')
-
   # Source the function
   . $PSScriptRoot/../../utils/workflow/Invoke-PSSA.ps1
   # Invoke PSSA
@@ -22,12 +13,4 @@ Describe "PSSA Check" {
     $module = Get-Module -ListAvailable -Name 'PSScriptAnalyzer'
     $module | Should -Not -BeNullOrEmpty
   }
-  # It "PSSA should write final output" {
-  #   $Output | Should -Contain "Problems were found in the PowerShell scripts." -or $Output | Should -Contain "No problems were found in the PowerShell scripts."
-  # }
 }
-
-# AfterAll {
-#   # Cleanup
-#   $ExecutionContext.SessionState.InvokeCommand.SetCommand('Write-Output', $OriginalWriteOutput, 'All')
-# }
