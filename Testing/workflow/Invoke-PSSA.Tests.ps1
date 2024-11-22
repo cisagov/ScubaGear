554abe4a4857bf9fb1471030d5821808e979c7c4
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-11-22 09:06:17 -0500

    remove publish tests

--- Testing/workflow/Invoke-PSSA.Tests.ps1
+++ Testing/workflow/Invoke-PSSA.Tests.ps1
@@ -12,16 +12,16 @@ Describe "PSSA Install Check" {
   }
 }
 
-# Describe "PSSA Output Check" {
-#   It "PSSA should write output" {
-#     # Source the function
-#     . $PSScriptRoot/../../utils/workflow/Invoke-PSSA.ps1
-#     # Invoke PSSA
-#     $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '..\..' -Resolve
-#     $Output = Invoke-PSSA -DebuggingMode $false -RepoPath $RepoRootPath 6>&1
-#     $Output | Should -Not -BeNullOrEmpty
-#     # Note: This is a little bit fragile.  It only work as long as one of these two
-#     # summary statements is the final output written.
-#     $Output | Select-Object -Last 1 | Should -BeIn @("Problems were found in the PowerShell scripts.", "No problems were found in the PowerShell scripts.")
-#   }
-# }
+Describe "PSSA Output Check" {
+  It "PSSA should write output" {
+    # Source the function
+    . $PSScriptRoot/../../utils/workflow/Invoke-PSSA.ps1
+    # Invoke PSSA
+    $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '..\..' -Resolve
+    $Output = Invoke-PSSA -DebuggingMode $false -RepoPath $RepoRootPath 6>&1
+    $Output | Should -Not -BeNullOrEmpty
+    # Note: This is a little bit fragile.  It only work as long as one of these two
+    # summary statements is the final output written.
+    $Output | Select-Object -Last 1 | Should -BeIn @("Problems were found in the PowerShell scripts.", "No problems were found in the PowerShell scripts.")
+  }
+}
