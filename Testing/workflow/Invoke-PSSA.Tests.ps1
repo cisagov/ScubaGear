15e531e421bcb9eb5763e37175e40d61cc692799
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-11-22 15:02:34 -0500

    suppress PSSA

--- Testing/workflow/Invoke-PSSA.Tests.ps1
+++ Testing/workflow/Invoke-PSSA.Tests.ps1
@@ -1,5 +1,9 @@
 # The purpose of this test is to verify that PSSA is working.
 
+# Suppress PSSA warnings here at the root of the test file.
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '')]
+param()
+
 BeforeDiscovery {
   $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Invoke-PSSA.ps1' -Resolve
   $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '../..' -Resolve
