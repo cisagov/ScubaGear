2b0e4164faaf5b1083c9022bf123b8f895dd2864
mitchelbaker-cisa <mitchel.baker@cisa.dhs.gov>
2024-11-22 21:12:40 +0000

    fix key name for Office 365 SharePoint Online which did not match correctly; added null values to key/password/federated creds if none exist

--- sp-script.ps1
+++ sp-script.ps1
@@ -34,8 +34,7 @@ function Format-RiskyPermissions {
             ResourceDisplayName = $resource
         }
     }
-
-    $map
+    return $map
 }
 
 function Get-ValidCredentials {
@@ -57,10 +56,16 @@ function Get-ValidCredentials {
     $validCredentials = @()
     foreach ($credential in $credentials) {
         if ($credential.EndDateTime -gt (Get-Date)) {
-            # $credential is of type PSCredential which is immutable, so create a copy
-            $credentialCopy = $credential | Select-Object *, @{Name = "IsFromApplication"; Expression = { $IsFromApplication }}
+            # $credential is of type PSCredential which is immutable, create a copy
+            $credentialCopy = $credential | Select-Object *, @{
+                Name = "IsFromApplication"; 
+                Expression = { $IsFromApplication }
+            }
             $validCredentials += $credentialCopy
         }
+    }
+    if ($null -eq $credentials -or $credentials.Count -eq 0 -or $validCredentials.Count -eq 0) {
+        return $null
     }
     return $validCredentials
 }
@@ -75,75 +80,79 @@ function Get-ApplicationsWithRiskyPermissions {
     .Functionality
     #Internal
     ##>
-
-    try {
-        $applications = Get-MgBetaApplication -All
-        $applicationResults = @()
-        foreach ($app in $applications) {
-            # "AzureADMyOrg" = single tenant
-            # "AzureADMultipleOrgs" = multi tenant
-            $IsMultiTenantEnabled = $false
-            if ($app.signInAudience -eq "AzureADMultipleOrgs") { $IsMultiTenantEnabled = $true }
-        
-            # Map permissions assigned to application to risky permissions
-            $mappedPermissions = @()
-            foreach ($resource in $app.RequiredResourceAccess) {
-                # Exclude delegated permissions with property Type="Scope"
-                $roles = $resource.ResourceAccess | Where-Object { $_.Type -eq "Role" }
-                $resourceAppId = $resource.ResourceAppId
-        
-                foreach($role in $roles) {
-                    $resourceDisplayName = $permissionsJson.resources.$resourceAppId
-                    $roleId = $role.Id
-                    $mappedPermissions = Format-RiskyPermissions `
-                        -Json $permissionsJson `
-                        -Map $mappedPermissions `
-                        -Resource $resourceDisplayName `
-                        -Id $roleId
+    process {
+        try {
+            $applications = Get-MgBetaApplication -All
+            $applicationResults = @()
+            foreach ($app in $applications) {
+                # "AzureADMyOrg" = single tenant
+                # "AzureADMultipleOrgs" = multi tenant
+                $IsMultiTenantEnabled = $false
+                if ($app.signInAudience -eq "AzureADMultipleOrgs") { $IsMultiTenantEnabled = $true }
+            
+                # Map permissions assigned to application to risky permissions
+                $mappedPermissions = @()
+                foreach ($resource in $app.RequiredResourceAccess) {
+                    # Exclude delegated permissions with property Type="Scope"
+                    $roles = $resource.ResourceAccess | Where-Object { $_.Type -eq "Role" }
+                    $resourceAppId = $resource.ResourceAppId
+            
+                    foreach($role in $roles) {
+                        $resourceDisplayName = $permissionsJson.resources.$resourceAppId
+                        $roleId = $role.Id
+                        $mappedPermissions = Format-RiskyPermissions `
+                            -Json $permissionsJson `
+                            -Map $mappedPermissions `
+                            -Resource $resourceDisplayName `
+                            -Id $roleId
+                    }
                 }
-            }
-        
-            # Get federated credentials
-            $federatedCredentials = Get-MgBetaApplicationFederatedIdentityCredential -All -ApplicationId $app.Id
-            $federatedCredentialsResults = @()
-        
-            if ($null -ne $federatedCredentials) {
-                foreach ($federatedCredential in $federatedCredentials) {
-                    $federatedCredentialsResults += [PSCustomObject]@{
-                        Id = $federatedCredential.Id
-                        Name = $federatedCredential.Name
-                        Description = $federatedCredential.Description
-                        Issuer = $federatedCredential.Issuer
-                        Subject = $federatedCredential.Subject
-                        Audiences = $federatedCredential.Audiences | Out-String
+            
+                # Get federated credentials
+                $federatedCredentials = Get-MgBetaApplicationFederatedIdentityCredential -All -ApplicationId $app.Id
+                $federatedCredentialsResults = @()
+            
+                if ($null -ne $federatedCredentials) {
+                    foreach ($federatedCredential in $federatedCredentials) {
+                        $federatedCredentialsResults += [PSCustomObject]@{
+                            Id = $federatedCredential.Id
+                            Name = $federatedCredential.Name
+                            Description = $federatedCredential.Description
+                            Issuer = $federatedCredential.Issuer
+                            Subject = $federatedCredential.Subject
+                            Audiences = $federatedCredential.Audiences | Out-String
+                        }
+                    }
+                }
+                else {
+                    $federatedCredentialsResults = $null
+                }
+            
+                # Disregard entries without risky permissions
+                if ($mappedPermissions.Count -gt 0) {
+                    $applicationResults += [PSCustomObject]@{
+                        ObjectId = $app.Id
+                        AppId = $app.AppId
+                        DisplayName = $app.DisplayName
+                        IsMultiTenantEnabled = $IsMultiTenantEnabled
+                        KeyCredentials = Get-ValidCredentials -Credentials $app.KeyCredentials -IsFromApplication $true
+                        PasswordCredentials = Get-ValidCredentials -Credentials $app.PasswordCredentials -IsFromApplication $true
+                        FederatedCredentials = $federatedCredentialsResults
+                        RiskyPermissions = $mappedPermissions
                     }
                 }
             }
-        
-            # Disregard entries without risky permissions
-            if ($mappedPermissions.Count -gt 0) {
-                $applicationResults += [PSCustomObject]@{
-                    ObjectId = $app.Id
-                    AppId = $app.AppId
-                    DisplayName = $app.DisplayName
-                    IsMultiTenantEnabled = $IsMultiTenantEnabled
-                    KeyCredentials = Get-ValidCredentials -Credentials $app.KeyCredentials -IsFromApplication $true
-                    PasswordCredentials = Get-ValidCredentials -Credentials $app.PasswordCredentials -IsFromApplication $true
-                    FederatedCredentials = $federatedCredentials
-                    RiskyPermissions = $mappedPermissions
-                }
-            }
+        } catch {
+            Write-Warning "An error occurred in Get-ApplicationsWithRiskyPermissions: $($_.Exception.Message)"
+            Write-Warning "Stack trace: $($_.ScriptStackTrace)"
+            throw $_
         }
-    } catch {
-        Write-Warning "An error occurred in Get-ApplicationsWithRiskyPermissions: $($_.Exception.Message)"
-        Write-Warning "Stack trace: $($_.ScriptStackTrace)"
-        throw $_
+        $applicationResults | ConvertTo-Json -Depth 3
     }
-    $applicationResults | ConvertTo-Json -Depth 3
 }
 
-#$riskyApps = Get-ApplicationsWithRiskyPermissions
-#$riskyApps > finalAppResults.json
+$riskyApps = Get-ApplicationsWithRiskyPermissions
+$riskyApps > finalAppResults.json
 
 function Get-ServicePrincipalsWithRiskyPermissions {
     <#
@@ -153,49 +162,49 @@ function Get-ServicePrincipalsWithRiskyPermissions {
     .Functionality
     #Internal
     ##>
-    try {
-        $servicePrincipalResults = @()
-        $servicePrincipals = Get-MgBetaServicePrincipal -All
-        foreach ($servicePrincipal in $servicePrincipals) {
-            # Only retrieves permissions an admin has consented to
-            $appRoleAssignments = Get-MgBetaServicePrincipalAppRoleAssignment -All -ServicePrincipalId $servicePrincipal.Id
-            $mappedPermissions = @()
-            if ($appRoleAssignments.Count -gt 0) {
-                foreach ($role in $appRoleAssignments) {
-                    $resourceDisplayName = $role.ResourceDisplayName
-                    $roleId = $role.AppRoleId
-                    $mappedPermissions = Format-RiskyPermissions `
-                        -Json $permissionsJson `
-                        -Map $mappedPermissions `
-                        -Resource $resourceDisplayName `
-                        -Id $roleId
+    process {
+        try {
+            $servicePrincipalResults = @()
+            $servicePrincipals = Get-MgBetaServicePrincipal -All
+            foreach ($servicePrincipal in $servicePrincipals) {
+                # Only retrieves permissions an admin has consented to
+                $appRoleAssignments = Get-MgBetaServicePrincipalAppRoleAssignment -All -ServicePrincipalId $servicePrincipal.Id
+                $mappedPermissions = @()
+                if ($appRoleAssignments.Count -gt 0) {
+                    foreach ($role in $appRoleAssignments) {
+                        $resourceDisplayName = $role.ResourceDisplayName
+                        $roleId = $role.AppRoleId
+                        $mappedPermissions = Format-RiskyPermissions `
+                            -Json $permissionsJson `
+                            -Map $mappedPermissions `
+                            -Resource $resourceDisplayName `
+                            -Id $roleId
+                    }
+                }
+            
+                # Disregard entries without risky permissions
+                if ($mappedPermissions.Count -gt 0) {
+                    $servicePrincipalResults += [PSCustomObject]@{
+                        ObjectId = $servicePrincipal.Id
+                        AppId = $servicePrincipal.AppId
+                        DisplayName = $servicePrincipal.DisplayName
+                        KeyCredentials = Get-ValidCredentials -Credentials $servicePrincipal.KeyCredentials -IsFromApplication $false
+                        PasswordCredentials = Get-ValidCredentials -Credentials $servicePrincipal.PasswordCredentials -IsFromApplication $false
+                        RiskyPermissions = $mappedPermissions
+                    }
                 }
             }
-        
-            # Disregard entries without risky permissions
-            if ($mappedPermissions.Count -gt 0) {
-                $servicePrincipalResults += [PSCustomObject]@{
-                    ObjectId = $servicePrincipal.Id
-                    AppId = $servicePrincipal.AppId
-                    DisplayName = $servicePrincipal.DisplayName
-                    KeyCredentials = Get-ValidCredentials -Credentials $servicePrincipal.KeyCredentials -IsFromApplication $false
-                    PasswordCredentials = Get-ValidCredentials -Credentials $servicePrincipal.PasswordCredentials -IsFromApplication $false
-                    RiskyPermissions = $mappedPermissions
-                }
-            }
+        } catch {
+            Write-Warning "An error occurred in Get-ApplicationsWithRiskyPermissions: $($_.Exception.Message)"
+            Write-Warning "Stack trace: $($_.ScriptStackTrace)"
+            throw $_
         }
-    } catch {
-        Write-Warning "An error occurred in Get-ApplicationsWithRiskyPermissions: $($_.Exception.Message)"
-        Write-Warning "Stack trace: $($_.ScriptStackTrace)"
-        throw $_
+        $servicePrincipalResults | ConvertTo-Json -Depth 3
     }
-    $servicePrincipalResults | ConvertTo-Json -Depth 3
 }
 
-#$servicePrincipalResults = $servicePrincipalResults | Where-Object { $_."Risky Permissions".Count -gt 0 }
-
-#$riskySPs = Get-ServicePrincipalsWithRiskyPermissions
-#$riskySPs > finalSPResults.json
+$riskySPs = Get-ServicePrincipalsWithRiskyPermissions
+$riskySPs > finalSPResults.json
 
 $riskyApps = (Get-Content -Path "./finalAppResults.json" | ConvertFrom-Json)
 $riskySPs = (Get-Content -Path "./finalSPResults.json" | ConvertFrom-Json)
@@ -228,10 +237,8 @@ foreach ($app in $riskyApps) {
         }
 
         $mergedObject = [PSCustomObject]@{
-            #ObjectId = [PSCustomerObject]@{
-            #    ApplicationObjectId = $app.ObjectId
-            #    ServicePrincipalObjectId = $matchedServicePrincipal.ObjectId
-            #}
+            ApplicationObjectId = $app.ObjectId
+            ServicePrincipalObjectId = $matchedServicePrincipal.ObjectId
             AppId = $app.AppId
             DisplayName = $app.DisplayName
             IsMultiTenantEnabled = $app.IsMultiTenantEnabled
