71afd160814ed6b89d020621cf98b6804d66b138
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-11-22 15:55:56 -0500

    just check install

--- Testing/workflow/Invoke-PSSA.Tests.ps1
+++ Testing/workflow/Invoke-PSSA.Tests.ps1
@@ -1,8 +1,6 @@
 # The purpose of this test is to verify that PSSA is working.
 
 # Suppress PSSA warnings here at the root of the test file.
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '')]
-param()
 
 BeforeDiscovery {
   $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Invoke-PSSA.ps1' -Resolve
@@ -10,26 +8,13 @@ BeforeDiscovery {
   # Source the function
   . $ScriptPath
   # Invoke PSSA, redirecting all Write-Outputs to $Output
-  $Writes = Invoke-PSSA -DebuggingMode $false -RepoPath $RepoRootPath 6>&1
-  $global:Output = $Writes
+  Invoke-PSSA -DebuggingMode $false -RepoPath $RepoRootPath
 }
 
 Describe "PSSA Check" {
-  It "PSSA should write output" {
-    Write-Warning "placeholder..."
-    # $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Invoke-PSSA.ps1' -Resolve
-    # $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '../..' -Resolve
-    # # Source the function
-    # . $ScriptPath
-    # # Invoke PSSA, redirecting all Write-Outputs to $Output
-    # Invoke-PSSA -DebuggingMode $false -RepoPath $RepoRootPath
-    # $Output = Invoke-PSSA -DebuggingMode $false -RepoPath $RepoRootPath 6>&1
-    # Write-Warning $Output
-    # $Module = Get-Module -ListAvailable -Name 'PSScriptAnalyzer'
-    # $Module | Should -Not -BeNullOrEmpty
-    $Output | Should -Not -BeNullOrEmpty
-    # # Note: This is a little bit fragile.  It only work as long as one of these two
-    # # summary statements is the final output written.
-    # $Output | Select-Object -Last 1 | Should -BeIn @("Problems were found in the PowerShell scripts.", "No problems were found in the PowerShell scripts.")
+  It "PSSA should be installed" {
+    $Module = Get-Module -ListAvailable -Name 'PSScriptAnalyzer'
+    $Module | Should -Not -BeNullOrEmpty
+    
   }
 }
