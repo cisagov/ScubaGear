f473693015e1c0f2b609d7bf843a61ea09f297e5
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-12-03 07:36:35 -0500

    time serial

--- Testing/workflow/Invoke-PSSA.Tests.ps1
+++ Testing/workflow/Invoke-PSSA.Tests.ps1
@@ -11,7 +11,7 @@ BeforeDiscovery {
   . $ScriptPath
   # Invoke PSSA, redirecting all Write-Warnings to a variable
   $Warnings = @()
-  Invoke-PSSA -DebuggingMode $false -RepoPath $RepoRootPath -WarningVariable Warnings 
+  Invoke-PSSA -DebuggingMode $false -RepoPath $RepoRootPath -WarningVariable Warnings
   $global:Warnings = $Warnings
 }
 
@@ -21,9 +21,6 @@ Describe "PSSA Check" {
     $Module | Should -Not -BeNullOrEmpty
   }
   It "PSSA should write output" {
-    Write-Warning "----"
-    $global:Warnings | ForEach-Object { Write-Warning $_ } 
-    Write-Warning "----"
     # There should be write-warning statements
     $global:Warnings | Should -Not -BeNullOrEmpty
     # Note: This is a little bit fragile.  It only work as long as one of these two
@@ -31,24 +28,3 @@ Describe "PSSA Check" {
     $global:Warnings | Select-Object -Last 1 | Should -BeIn @("Problems were found in the PowerShell scripts.", "No problems were found in the PowerShell scripts.")
   }
 }
-
-
-# Describe "PSSA Check" {
-#   It "PSSA should write output" {
-#     $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '../..' -Resolve
-#     $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Invoke-PSSA.ps1' -Resolve
-#     # Source the function
-#     . $ScriptPath
-#     # Invoke PSSA, redirecting all Write-Warnings to a variable
-#     $Warnings = @()
-#     Invoke-PSSA -DebuggingMode $false -RepoPath $RepoRootPath -WarningVariable Warnings 
-#     $Module = Get-Module -ListAvailable -Name 'PSScriptAnalyzer'
-#     # The module should be installed
-#     $Module | Should -Not -BeNullOrEmpty
-#     # There should be write-warning statements
-#     $Warnings | Should -Not -BeNullOrEmpty
-#     # Note: This is a little bit fragile.  It only work as long as one of these two
-#     # summary statements is the final output written.
-#     $Warnings | Select-Object -Last 1 | Should -BeIn @("Problems were found in the PowerShell scripts.", "No problems were found in the PowerShell scripts.")
-#   }
-# }
