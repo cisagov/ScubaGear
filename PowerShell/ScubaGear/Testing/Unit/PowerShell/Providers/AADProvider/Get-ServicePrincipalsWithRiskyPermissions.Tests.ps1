accf28e1b46b2ad77377c4b5582ba40097fa3906
mitchelbaker-cisa <mitchel.baker@cisa.dhs.gov>
2024-12-04 02:03:49 +0000

    complete unit tests for risky service principals

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/Get-ServicePrincipalsWithRiskyPermissions.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/Get-ServicePrincipalsWithRiskyPermissions.Tests.ps1
@@ -32,14 +32,45 @@ InModuleScope AADRiskyPermissionsHelper {
             $RiskySPs[0].PasswordCredentials | Should -HaveCount 1
             $RiskySPs[0].FederatedCredentials | Should -BeNullOrEmpty
             $RiskySPs[0].RiskyPermissions | Should -HaveCount 5
+            $RiskySPs[1].DisplayName | Should -Match "Test SP 2"
+            $RiskySPs[1].KeyCredentials | Should -HaveCount 1
+            $RiskySPs[1].PasswordCredentials | Should -BeNullOrEmpty
+            $RiskySPs[1].FederatedCredentials | Should -BeNullOrEmpty
+            $RiskySPs[1].RiskyPermissions | Should -HaveCount 5
         }
 
         It "excludes service principals without any risky permissions" {
-
+            Mock Get-MgBetaServicePrincipal { $MockServicePrincipals }
+            # Set to empty list
+            Mock Get-MgBetaServicePrincipalAppRoleAssignment { @() }
+            
+            $RiskySPs = Get-ServicePrincipalsWithRiskyPermissions
+            $RiskySPs | Should -BeNullOrEmpty
         }
 
         It "excludes roles not included in the RiskyPermissions.json mapping" {
+            $SafePermissions = @(
+                [PSCustomObject]@{
+                    AppRoleId = "2f3e6f8c-093b-4c57-a58b-ba5ce494a169" # Agreement.Read.All
+                    ResourceDisplayName = "Microsoft Graph"
+                }
+                [PSCustomObject]@{
+                    AppRoleId = "e12dae10-5a57-4817-b79d-dfbec5348930" # AppCatalog.Read.All
+                    ResourceDisplayName = "Microsoft Graph"
+                }
+                [PSCustomObject]@{
+                    AppRoleId = "be95e614-8ef3-49eb-8464-1c9503433b86" # Bookmark.Read.All
+                    ResourceDisplayName = "Microsoft Graph"
+                }
+            )
+            $MockServicePrincipalAppRoleAssignments += $SafePermissions
+            $MockServicePrincipalAppRoleAssignments | Should -HaveCount 8
 
+            Mock Get-MgBetaServicePrincipal { $MockServicePrincipals }
+            Mock Get-MgBetaServicePrincipalAppRoleAssignment { $MockServicePrincipalAppRoleAssignments }
+            
+            $RiskySPs = Get-ServicePrincipalsWithRiskyPermissions
+            $RiskySPs[0].RiskyPermissions | Should -HaveCount 5
         }
     }
 }
