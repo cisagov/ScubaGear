09f4a39d6cbba954bd6a7497308f8cb5f7a377af
mitchelbaker-cisa <mitchel.baker@cisa.dhs.gov>
2024-12-04 18:44:35 +0000

    complete unit tests for first party risky apps

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/Get-ApplicationsWithRiskyPermissions.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/Get-ApplicationsWithRiskyPermissions.Tests.ps1
@@ -19,20 +19,37 @@ InModuleScope AADRiskyPermissionsHelper {
             # Refer to $MockApplications in ./RiskyPermissionsSnippets,
             # we are comparing data stored there with the function's return value
             $RiskyApps = Get-ApplicationsWithRiskyPermissions
-            $RiskyApps | Should -HaveCount 2
+            $RiskyApps | Should -HaveCount 3
+
+            $RiskyApps[0].DisplayName | Should -Match "Test App 1"
+            $RiskyApps[0].ObjectId | Should -Match "00000000-0000-0000-0000-000000000001"
+            $RiskyApps[0].AppId | Should -Match "10000000-0000-0000-0000-000000000000"
             $RiskyApps[0].IsMultiTenantEnabled | Should -Be $true
             $RiskyApps[0].KeyCredentials | Should -HaveCount 2
             $RiskyApps[0].PasswordCredentials | Should -HaveCount 1
             $RiskyApps[0].FederatedCredentials | Should -HaveCount 2
             $RiskyApps[0].RiskyPermissions | Should -HaveCount 2
+
+            $RiskyApps[1].DisplayName | Should -Match "Test App 2"
+            $RiskyApps[1].ObjectId | Should -Match "00000000-0000-0000-0000-000000000002"
+            $RiskyApps[1].AppId | Should -Match "20000000-0000-0000-0000-000000000000"
             $RiskyApps[1].IsMultiTenantEnabled | Should -Be $false
             $RiskyApps[1].KeyCredentials | Should -HaveCount 1
             $RiskyApps[1].PasswordCredentials | Should -BeNullOrEmpty
             $RiskyApps[1].FederatedCredentials | Should -HaveCount 2
             $RiskyApps[1].RiskyPermissions | Should -HaveCount 3
+
+            $RiskyApps[2].DisplayName | Should -Match "Test App 3"
+            $RiskyApps[2].ObjectId | Should -Match "00000000-0000-0000-0000-000000000003"
+            $RiskyApps[2].AppId | Should -Match "30000000-0000-0000-0000-000000000000"
+            $RiskyApps[2].IsMultiTenantEnabled | Should -Be $false
+            $RiskyApps[2].KeyCredentials | Should -BeNullOrEmpty
+            $RiskyApps[2].PasswordCredentials | Should -HaveCount 1
+            $RiskyApps[2].FederatedCredentials | Should -HaveCount 2
+            $RiskyApps[2].RiskyPermissions | Should -HaveCount 4
         }
     
-        it "excludes ResourceAccess objects with property Type='Scope'" {
+        It "excludes ResourceAccess objects with property Type='Scope'" {
             # We only care about objects with type="role".
             # Adding a couple objects with type="scope" to verify they're excluded
             $ResourceAccess = $MockApplications[0].RequiredResourceAccess[0].ResourceAccess
@@ -56,22 +73,19 @@ InModuleScope AADRiskyPermissionsHelper {
             $RiskyApps[0].RiskyPermissions | Should -HaveCount 2
         }
     
-        it "correctly formats federated credentials if they exist" {
+        It "correctly formats federated credentials if they exist" {
             Mock Get-MgBetaApplication { $MockApplications[0] }
             Mock Get-MgBetaApplicationFederatedIdentityCredential {}
 
             $RiskyApps = Get-ApplicationsWithRiskyPermissions
+            $ExpectedKeys = @("Id", "Name", "Description", "Issuer", "Subject", "Audiences")
             foreach ($Credential in $RiskyApps[0].FederatedCredentials) {
                 # Check for correct properties
-                $ExpectedKeys = @("Id", "Name", "Description", "Issuer", "Subject", "Audiences")
                 $Credential.PSObject.Properties.Name | Should -Be $ExpectedKeys
-
-                # Check against an invalid property
-                $Credential.PSObject.Properties.Name | Should -NotContain "AppId"
             }
         }
     
-        it "sets the list of federated credentials to null if no credentials exist" {
+        It "sets the list of federated credentials to null if no credentials exist" {
             Mock Get-MgBetaApplication { $MockApplications[0] }
             Mock Get-MgBetaApplicationFederatedIdentityCredential {}
 
@@ -79,7 +93,7 @@ InModuleScope AADRiskyPermissionsHelper {
             $RiskyApps[0].FederatedCredentials | Should -BeNullOrEmpty
         }
     
-        it "excludes applications without risky permissions" {
+        It "excludes applications without risky permissions" {
             # Reset to some arbitrary Id not included in RiskyPermissions.json mapping
             $MockApplications[1].RequiredResourceAccess[0].ResourceAccess = @(
                 [PSCustomObject]@{
@@ -94,7 +108,7 @@ InModuleScope AADRiskyPermissionsHelper {
             Mock Get-MgBetaApplicationFederatedIdentityCredential {}
 
             $RiskyApps = Get-ApplicationsWithRiskyPermissions
-            $RiskyApps | Should -HaveCount 1
+            $RiskyApps | Should -HaveCount 2
         }
     }
 }
