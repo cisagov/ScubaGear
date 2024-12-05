d06effec9d75347abb44b86d7ffae5bc115fc409
mitchelbaker-cisa <mitchel.baker@cisa.dhs.gov>
2024-12-05 11:11:29 +0000

    add unit tests for Merge-Credentials

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/AADRiskyPermissionsHelper/Merge-Credentials.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/AADProvider/AADRiskyPermissionsHelper/Merge-Credentials.Tests.ps1
@@ -3,13 +3,33 @@ $AADRiskyPermissionsHelper = "$($ModulesPath)/Providers/ProviderHelpers/AADRisky
 Import-Module $AADRiskyPermissionsHelper
 
 InModuleScope AADRiskyPermissionsHelper {
-    Describe "Format-Credentials" {
+    Describe "Merge-Credentials" {
         BeforeAll {
             # Import mock data
             . ../RiskyPermissionsSnippets/MockData.ps1
 
-            $MockKeyCredentials = $MockApplications[0].KeyCredentials
-            $MockPasswordCredentials = $MockServicePrincipals[3].PasswordCredentials
+            $MockApplicationCredentials = $MockApplications[0].KeyCredentials
+            $MockSPCredentials = $MockServicePrincipals[0].KeyCredentials
+        }
+        
+        It "merges application/service principal credentials together if both are valid" {
+            $Output = Merge-Credentials -ApplicationCredentials $MockApplicationCredentials -ServicePrincipalCredentials $MockSPCredentials
+            $Output | Should -HaveCount 3
+        }
+    
+        It "returns only application credentials if service principal credentials are null" {
+            $Output = Merge-Credentials -ApplicationCredentials $MockApplicationCredentials -ServicePrincipalCredentials $null
+            $Output | Should -HaveCount 2
+        }
+    
+        It "returns only service principal credentials if application credentials are null" {
+            $Output = Merge-Credentials -ApplicationCredentials $null -ServicePrincipalCredentials $MockSPCredentials
+            $Output | Should -HaveCount 1
+        }
+    
+        It "returns null if neither credentials are valid" {
+            $Output = Merge-Credentials -ApplicationCredentials $null -ServicePrincipalCredentials $null
+            $Output | Should -BeNullOrEmpty
         }
     }
 }
