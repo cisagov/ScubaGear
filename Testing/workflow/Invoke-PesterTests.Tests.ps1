295ab973d184c661b58ac8c9a4e51ba27587ec0e
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-12-10 08:35:42 -0500

    fix path.

--- Testing/workflow/Invoke-PesterTests.Tests.ps1
+++ Testing/workflow/Invoke-PesterTests.Tests.ps1
@@ -3,15 +3,17 @@
 
 Describe "Pester Check" {
   It "Pester should fail" {
+    # This file has intentional problems in it.
+    $FailFilePath = Join-Path -Path $PSScriptRoot -ChildPath '../../Testing/PesterTestFiles/DummyFail.ps1' -Resolve
     # Source the function
     . $PSScriptRoot/../../utils/workflow/Invoke-PesterTests.ps1
-    $FailFilePath = '$PSScriptRoot/../../Testing/PesterTestFiles/DummyFail.ps1'
     { Invoke-PesterTests -Path $FailFilePath } | Should -Throw -Because "directory does not exist."
   }
   It "Pester should pass" {
+    # This file has no problems.
+    $PassFilePath = Join-Path -Path $PSScriptRoot -ChildPath '../../Testing/PesterTestFiles/DummyPass.ps1' -Resolve
     # Source the function
     . $PSScriptRoot/../../utils/workflow/Invoke-PesterTests.ps1
-    $PassFilePath = '$PSScriptRoot/../../Testing/PesterTestFiles/DummyPass.ps1'
     { Invoke-PesterTests -Path $PassFilePath } | Should -Not -Throw
   }
 }
