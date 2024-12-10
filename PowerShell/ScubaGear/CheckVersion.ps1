19ae5d2317a18df15918565e14d9ee04a58b0c06
thom neale <tneale@mitre.org>
2024-12-10 19:15:00 +0000

    Ditch git tag query in favor of module manifest version check

--- PowerShell/ScubaGear/CheckVersion.ps1
+++ PowerShell/ScubaGear/CheckVersion.ps1
@@ -36,51 +36,17 @@ function Invoke-CheckScubaGearVersionPSGallery {
 }
 
 
-function Invoke-CheckScubaGearVersionGit {
-    # Check if git is available
-    $gitAvailable = Get-Command git -ErrorAction SilentlyContinue
-
-    if ($gitAvailable) {
-        $scubaGearPath = "."
-
-        if (Test-Path $scubaGearPath) {
-            # Check if it's a git repo
-            Set-Location -Path $scubaGearPath
-            try {
-                $gitTag = git describe --exact-match --tags 2>$null
-            } catch {
-                if ($_.Exception.Message -match "No names found, cannot describe anything") {
-                    # If we are here, this checkout has no tags. May be a shallow clone used for the test runner.
-                    return
-                }
-            }
-            if ($gitTag) {
-                # Remove leading "v" from the tag
-                $currentVersion = $gitTag.TrimStart("v")
-
-                # Fetch the latest release info from GitHub API
-                $latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/cisagov/ScubaGear/releases/latest"
-                $latestVersion = $latestRelease.tag_name.TrimStart("v")
-
-                # Compare the versions
-                if ([System.Version]$currentVersion -lt [System.Version]$latestVersion) {
-                    Write-Warning "A new version of ScubaGear ($latestVersion) is available. Please consider updating at: https://github.com/cisagov/ScubaGear/releases"
-                }
-            }
-            else {
-                # If we are here, the command is not running inside a git repo. Probably not installed from git.
-                Write-Warning "ScubaGear is not currently at a GitHub tagged release. Consider using the latest tagged release. You can disable this warning using the SCUBAGEAR_SKIP_VERSION_CHECK environment variable."
-            }
-        }
-    }
-    else {
-        # No git executable available. If we are here, ScubaGear was probably not installed from git.
-        return
+function Invoke-CheckScubaGearVersionGithub {
+    $ScubaManifest = Import-PowerShellDataFile (Join-Path -Path $PSScriptRoot -ChildPath 'ScubaGear.psd1' -Resolve) -ErrorAction 'Stop'
+    $CurrentVersion = $ScubaManifest.ModuleVersion
+    $LatestVersion = $(Invoke-RestMethod -Uri "https://api.github.com/repos/cisagov/ScubaGear/releases/latest").tag_name.TrimStart("v")
+    if ($CurrentVersion -ne $LatestVersion) {
+            Write-Warning "A new version of ScubaGear ($latestVersion) is available. Please consider updating at: https://github.com/cisagov/ScubaGear/releases"
     }
 }
 
 # Do the version check if the skip envvar is not defined.
 if ([string]::IsNullOrWhiteSpace($env:SCUBAGEAR_SKIP_VERSION_CHECK)) {
     Invoke-CheckScubaGearVersionPSGallery
-    Invoke-CheckScubaGearVersionGit
+    Invoke-CheckScubaGearVersionGithub
 }
