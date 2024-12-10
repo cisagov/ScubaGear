13468721c3180e42de9038d4b7b1a5cbc83ebebf
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-12-10 10:57:28 -0500

    del files

--- Testing/PesterTestFiles/DummayPass.ps1
+++
@@ -1,3 +0,0 @@
-# This trivial PS file is for unit testing.
-# It simply lists the files in the current directory.
-Get-ChildItem
--- Testing/PesterTestFiles/DummyFail.ps1
+++
@@ -1,8 +0,0 @@
-# This trivial PS file is for unit testing.
-# It intentionally violates PSSA rules.
-
-# Unnecessary Use of Backticks
-$myVariable = `
-  "Hello World"
-# Using Aliases Instead of Full Cmdlet Names
-ls | ? {$_.Name -like "*txt"}
