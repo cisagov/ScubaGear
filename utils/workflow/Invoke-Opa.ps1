636605506596b26269fe56efe1695b439f67bf60
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-12-16 08:09:24 -0500

    create function for opa check

---
+++ utils/workflow/Invoke-Opa.ps1
@@ -0,0 +1,21 @@
+function Invoke-OpaCheck {
+  <#
+    .SYNOPSIS
+      Runs an OPA check that to check Rego source files for parse and compilation errors.
+    .PARAMETER $Paths
+      The array of paths for OPA to check.
+  #>
+  [CmdletBinding()]
+	param(
+		[Parameter(Mandatory = $true)]
+		[string[]]
+		$Paths
+	)
+
+  Write-Warning "Checking paths with OPA..."
+  Write-Warning " "
+
+  foreach ($Path in $Paths) {
+    opa test $Path --strict
+  }
+}
