426f4b221fcf8612840c143a5baee97f2b260c02
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-12-18 15:48:04 -0500

    verify commands

--- Testing/workflow/Build-SignRelease.Tests.ps1
+++ Testing/workflow/Build-SignRelease.Tests.ps1
@@ -1,10 +1,19 @@
-# temp placeholder for a real test
-Describe "Install AST Check" {
+# The purpose of this test is to verify that Azure Sign Tool is working.
+
+BeforeDiscovery {
   $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Build-SignRelease.ps1' -Resolve
   # Source the function
   . $ScriptPath
   Install-AzureSigningTool
-  It "Should be installed" {
+}
+
+Describe "AST Check" {
+  It "AST should be installed" {
+    $Commands = Get-Command AzureSignTool
+    Write-Warning "The commands are"
+    Write-Warning $Commands
+    Write-Warning "The type of commands"
+    Write-Warning $Commands.GetType()
     $ToolPath = (Get-Command AzureSignTool).Path
     Write-Warning "The path to AzureSignTool is $ToolPath"
     Test-Path -Path $TooPath | Should -Be -True
