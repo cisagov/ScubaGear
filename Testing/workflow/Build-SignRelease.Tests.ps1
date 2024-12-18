72501335d9d665ea6e4bce973fb3a602500891bc
James Garriss <52328727+james-garriss@users.noreply.github.com>
2024-12-18 15:26:55 -0500

    install before checking for install

--- Testing/workflow/Build-SignRelease.Tests.ps1
+++ Testing/workflow/Build-SignRelease.Tests.ps1
@@ -1,5 +1,9 @@
 # temp placeholder for a real test
 Describe "Install AST Check" {
+  $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Build-SignRelease.ps1' -Resolve
+  # Source the function
+  . $ScriptPath
+  Install-AzureSigningTool
   It "Should be installed" {
     $ToolPath = (Get-Command AzureSignTool).Path
     Write-Warning "The path to AzureSignTool is $ToolPath"
