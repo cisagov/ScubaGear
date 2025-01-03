30ec25d709372afbd57c1e9a75e09968c9883969
Thom Neale <tneale@mitre.org>
2025-01-03 10:08:17 -0500

    Nuke the PSGallery checks

--- PowerShell/ScubaGear/CheckVersion.ps1
+++ PowerShell/ScubaGear/CheckVersion.ps1
@@ -1,42 +1,5 @@
+
 function Invoke-CheckScubaGearVersion {
-    <#
-    .SYNOPSIS
-    Complain if a newer version of ScubaGear is available on PSGallery.
-
-    .DESCRIPTION
-    Checks PSGallery latest version and compares it to the latest version installed
-    from PSGallery.
-    #>
-
-    # Retrieve the installed version of ScubaGear from the system
-    $InstalledModule = Get-Module -Name ScubaGear -ListAvailable -ErrorAction 'Stop'
-
-    # If multiple different versions are installed, get the most recent.
-    if ($InstalledModule -is [array]) {
-        $InstalledModule = $InstalledModule | Sort-Object -Property Version | Select-Object -Last 1
-    }
-
-    # Check if no results found.
-    if (!$InstalledModule) {
-        # If we are here, ScubaGear is not installed from PSGallery.
-        # Or it may have been installed a different way in a nonstandard folder,
-        # or is running in an extracted release folder. Check github instead.
-        return Invoke-CheckScubaGearVersionGithub -ErrorAction 'Stop'
-    }
-
-    $LatestInstalledVersion = [System.Version]$InstalledModule.Version
-
-    # Retrieve the latest version from PowerShell Gallery
-    $ModuleInfo = Find-Module -Name ScubaGear -ErrorAction 'Stop'
-    $LatestPSGalleryVersion = [System.Version]$ModuleInfo.Version
-
-    if ($LatestInstalledVersion -lt $LatestPSGalleryVersion) {
-        Write-Warning "A newer version of ScubaGear ($LatestPSGalleryVersion) is available on PowerShell Gallery. This notification can be disabled by setting `$env:SCUBAGEAR_SKIP_VERSION_CHECK = `$true before running ScubaGear."
-    }
-}
-
-
-function Invoke-CheckScubaGearVersionGithub {
     <#
     .SYNOPSIS
     Complain if a newer version of ScubaGear is available from the Github release page.
