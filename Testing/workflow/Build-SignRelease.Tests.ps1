430a3c132164135e2b75c5e4f56e3c0ae968f160
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-01-17 14:29:40 -0500

    Comments

--- Testing/workflow/Build-SignRelease.Tests.ps1
+++ Testing/workflow/Build-SignRelease.Tests.ps1
@@ -1,18 +1,14 @@
-# Add test with bad key vault URL
-# Add test with bad cert name
-# Add test that checks for zip file after compress
-
-# The purpose of this test to ensure that the function properly signs the module.
+# The purpose of this test to ensure that the function fails
+# gracefully if the root folder name does not exist.
+# Note:  Functional testing (not unit testing) should be used
+#        to verify that AST itself actually works.
 
 Describe "Bad Inputs Check" {
   It "The root folder name should exist" {
     $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Build-SignRelease.ps1' -Resolve
     # Source the function
     . $ScriptPath
-    # $RootFolderPath = Join-Path -Path $PSScriptRoot -Childpath '../..' -Resolve
-    # Write-Warning "Root Folder Path: $RootFolderPath"
-    # Copy to pester $TestDrive and put in repo folder
-    # pass that repo folder to signature below
+    # The function should throw an exception if the root folder name does not exist.
     { New-ModuleSignature `
       -AzureKeyVaultUrl "https://www.cisa.gov" `
       -CertificateName "certificate name" `
