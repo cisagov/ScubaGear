c0ec264480e3ed56c9106842376518a1eeb961f9
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-01-17 13:45:32 -0500

    add exception test

--- Testing/workflow/Build-SignRelease.Tests.ps1
+++ Testing/workflow/Build-SignRelease.Tests.ps1
@@ -5,18 +5,18 @@
 # The purpose of this test to ensure that the function properly signs the module.
 
 Describe "Bad Inputs Check" {
-  It "Bad key vault URL should be handled gracefully" {
+  It "The root folder name should exist" {
     $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Build-SignRelease.ps1' -Resolve
     # Source the function
     . $ScriptPath
-    $RootFolderPath = Join-Path -Path $PSScriptRoot -Childpath '../..' -Resolve
-    Write-Warning "Root Folder Path: $RootFolderPath"
+    # $RootFolderPath = Join-Path -Path $PSScriptRoot -Childpath '../..' -Resolve
+    # Write-Warning "Root Folder Path: $RootFolderPath"
     # Copy to pester $TestDrive and put in repo folder
     # pass that repo folder to signature below
     New-ModuleSignature `
       -AzureKeyVaultUrl "https://www.cisa.gov" `
       -CertificateName "certificate name" `
       -ReleaseVersion "0.0.1" `
-      -RootFolderName $RootFolderPath
+      -RootFolderName "nonexistantfoldername" | Should -Throw
   }
 }
