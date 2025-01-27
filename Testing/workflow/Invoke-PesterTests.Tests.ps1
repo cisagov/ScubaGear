7ff3678c103dd7d61f4d79ae54c2d7392e20873b
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-01-27 10:03:21 -0500

    move test file under testing folder

--- Testing/workflow/Invoke-PesterTests.Tests.ps1
+++ Testing/workflow/Invoke-PesterTests.Tests.ps1
@@ -3,7 +3,8 @@
 
 Describe "Pester Check" {
   It "Passing test should pass." {
-    $TestFile = Join-Path -Path $PSScriptRoot -ChildPath '../../Testing/PesterTestFile/SimplePassFile.ps1' -Resolve
+    # $TestFile = Join-Path -Path $PSScriptRoot -ChildPath '../../Testing/PesterTestFile/SimplePassFile.ps1' -Resolve
+    $TestFile = Join-Path -Path $PSScriptRoot -ChildPath 'PesterTestFile/SimplePassFile.ps1' -Resolve
     # Source the function
     . utils/workflow/Invoke-PesterTests.ps1
     Invoke-PesterTests -Path $TestFile
