06903ee59f19f77f44e21ad5eeb349b70fd5b9ed
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-01-29 09:26:11 -0500

    Remove unused comments

--- utils/workflow/Initialize-ScubaGearForTesting.ps1
+++ utils/workflow/Initialize-ScubaGearForTesting.ps1
@@ -4,11 +4,7 @@ function Initialize-ScubaGearForTesting {
       Initializes ScubaGear, which installs the necessary modules and tools to run ScubaGear.
   #>
 
-  # Write-Debug 'Initializing ScubaGear for testing...'
   $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '..\..' -Resolve
-  # Write-Debug 'The repo root path is'
-  # Write-Debug $RepoRootPath
-  # Write-Debug 'Importing function Initialize-Scuba...'
   Import-Module (Join-Path -Path $RepoRootPath -ChildPath 'PowerShell/ScubaGear') -Function Initialize-Scuba
   Write-Debug 'Calling Initialize ScubaGear...'
   $Outputs = Initialize-SCuBA
