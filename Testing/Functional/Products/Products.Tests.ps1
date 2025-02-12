fc3d60d1d3cce09eb2ced3e490dbd0f103795f13
Devesh Agarwal <dagarwal@mitre.org>
2025-02-12 10:06:55 -0500

    added test for row headers

--- Testing/Functional/Products/Products.Tests.ps1
+++ Testing/Functional/Products/Products.Tests.ps1
@@ -332,8 +332,11 @@ Describe "Policy Checks for <ProductName>"{
                     elseif ($Table.GetProperty("id") -eq "license-info"){
                         #Currently empty to determine if necessary and what to test in section
                     }
+                    #Currently checks to make sure there are 4 row headers
                     elseif ($Table.GetProperty("id") -eq "privileged-service-principals"){
-                        #Currently empty to determine if necessary and what to test in section
+                        $RowHeaders = Get-SeElement -Element $Table -By TagName 'th'
+                        $RowHeaders | Should -BeExactly 4
+
                     }
                     else {
                         # Control report tables
