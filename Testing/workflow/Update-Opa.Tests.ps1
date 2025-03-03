1e54c43b39a90aeddd28cc790f620f7c5b8571e6
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-03-03 09:32:59 -0500

    move code to function
    fix path
    
    fix path
    
    use powershell
    
    fix filename
    
    fix path
    
    use output
    
    set warning again
    
    add pester test
    
    remove dead code for linter
    
    recomment publish
    
    lint
    
    update verb
    
    add removed code
    
    lint
    
    uncomment missing code
    
    readd temp var
    
    ignore var name error
    
    suppress error
    
    hid supportmodule
    
    comment out support module
    
    commented out version pattern
    
    comment out dupe var
    
    comment out opaversionpath
    
    remove $temp
    
    add $temp
    
    use call
    
    fix var call
    
    try invoke
    
    fix invoke
    
    add justification
    
    Clean dead code
    
    Add Pester test for current OPA version
    
    fix update opa path
    
    add writes for paths
    
    get version via getter
    
    add exit back
    
    remove class code
    
    remove getter
    
    Add getter
    
    Fix getter
    
    Use object and getter
    
    use singleton pattern
    
    fix scubaconfig syntax
    
    fix GetOpaVersion syntax
    
    call get opa version differently
    
    remove scubaconfig getinstance
    
    fixed variable name
    
    finish pester test for determine
    
    Remove dirty string manipulation
    
    add path to scuba config
    
    remove exit
    
    call function to update
    
    remove unused param
    
    change to older version
    
    Make reason explicit
    
    emit name of branch
    
    Improve warning
    
    only check
    
    try write info
    
    return to write warning
    
    catch exception for git ls-remote
    
    fix suppression location
    
    fix typo
    
    exit 0
    
    fix exit 0
    
    update opa
    
    check version
    
    use setter
    
    try again
    
    fix typo in method
    
    update w correct version
    
    set path
    
    fix typo
    
    add pester test
    
    add new pester test
    
    fix pattern typo
    
    fix error in path
    
    use correct var
    
    improve pester test
    
    stop using reserved word
    
    add line property
    
    use regex
    
    cleanup pester test, enable create PR
    
    run create PR
    
    lint
    
    suppress emoji
    
    cleanup
    
    lint
    
    lint
    
    lint
    
    Add code back to update ScubaConfig

---
+++ Testing/workflow/Update-Opa.Tests.ps1
@@ -0,0 +1,52 @@
+using module '..\..\PowerShell\ScubaGear\Modules\ScubaConfig\ScubaConfig.psm1'
+
+# The purpose of these tests is to verify that the functions used to update OPA are working.
+
+Describe "Update OPA" {
+    It "Determine if OPA needs to be updated" {
+        # Setup important paths
+        $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '../..' -Resolve
+        $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Update-Opa.ps1' -Resolve
+        # The current version of OPA used in SG is found in PowerShell/ScubaGear/ScubaGear.psm1
+        # in the variable DefaultOPAVersion
+        $ExpectedCurrentOPAVersion = [ScubaConfig]::GetOpaVersion()
+        # Call the function
+        . $ScriptPath
+        $ReturnValues = Confirm-OpaUpdateRequirements -RepoPath $RepoRootPath
+        # Check the results
+        $ActualCurrentOPAVersion = $ReturnValues["CurrentOPAVersion"]
+        $LatestOPAVersion = $ReturnValues["LatestOPAVersion"]
+        # The latest version of OPA is found here:
+        # https://github.com/open-policy-agent/opa/releases
+        $LatestOPAVersion | Should -Be "1.2.0"
+        $ActualCurrentOPAVersion | Should -Be $ExpectedCurrentOPAVersion
+    }
+    It "Update OPA version in config module" {
+        # Setup important paths
+        $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '../..' -Resolve
+        $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Update-Opa.ps1' -Resolve
+        # Call the function
+        . $ScriptPath
+        Update-OpaVersion -RepoPath $RepoRootPath -CurrentOpaVersion "1.0.1" -LatestOpaVersion "1.0.2"
+        # Check the results
+        $CurrentOPAVersion = [ScubaConfig]::GetOpaVersion()
+        $CurrentOPAVersion | Should -Be "1.0.2"
+    }
+    It "Update acceptable versions in support module" {
+        # Setup important paths
+        $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '../..' -Resolve
+        $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Update-Opa.ps1' -Resolve
+        $SupportPath = Join-Path -Path $PSScriptRoot -ChildPath '../../PowerShell/ScubaGear/Modules/Support/Support.psm1'
+        # Call the function
+        . $ScriptPath
+        Update-OpaVersion -RepoPath $RepoRootPath -CurrentOpaVersion "1.0.1" -LatestOpaVersion "1.0.2"
+        # Check the results
+        # Find all specific lines with this comment.
+        $MatchedLines = Select-String -Path $SupportPath -Pattern "# End Versions" -SimpleMatch
+        # There should be only 1 line in the support module that matches
+        $MatchedLines.Count | Should -Be 1
+        # Get that 1 line and test to see if it contains the new value.
+        $UpdatedLine = $MatchedLines[0].Line
+        $UpdatedLine | Should -Match ".1.0.2"  # This is a regex test.
+    }
+}
