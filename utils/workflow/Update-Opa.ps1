7cd2cb44407337047c07d01d8e095ebba1741f16
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-03-03 15:07:18 -0500

    comment out dupe var

--- utils/workflow/Update-Opa.ps1
+++ utils/workflow/Update-Opa.ps1
@@ -28,7 +28,7 @@ function Confirm-OpaUpdateRequirements {
 
     # Check if our current OPA version is outdated
     $OPAVersionPath = Join-Path -Path $RepoPath PowerShell/ScubaGear/Modules/Support/Support.psm1
-    $OPAVerRegex = "\'\d+\.\d+\.\d+\'"
+    # $OPAVerRegex = "\'\d+\.\d+\.\d+\'"
     # [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
     # $ExpectedVersionPattern = "ExpectedVersion = $OPAVerRegex"
     # [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
