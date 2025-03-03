e316d6aa11d983e5ade8a564c02f2740108d3445
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-03-03 12:20:44 -0500

    Test the output of ScubaGear's basic commands (#1538)
    * add dispatch
    
    * add push for testing
    
    temp replace secrets
    
    fix key vault
    
    write the info
    
    set specific info
    
    add error
    
    fix name
    
    fix errors
    
    Correct newline
    
    Fix newline
    
    continue to next line
    
    Another newline fix
    
    another output
    
    remove whitespace
    
    Clean up, should pass
    
    fix variable name
    
    check version
    
    Add intentional name error
    
    add exit 1
    
    fix exit code
    
    Fix one error.
    
    get the module version
    
    use actual version
    
    add comments to find version problem
    
    comments for version
    
    add comment
    
    Restore comments and version
    
    use write-debug
    
    use write-debug
    
    switch to warning
    
    try black magic
    
    try black magic
    
    Set correct version number
    
    check initiliaze output
    
    clarify output
    
    Fix typo
    
    Trivial change
    
    Remove comment from output
    
    Add outputs for initialize.
    
    removed write-host
    
    process the output
    
    replace Python w PowerShell.
    
    Put output up to Pester tests
    
    Update outputs
    
    fix global var name
    
    Fix output var in Pester
    
    test autotrust
    
    lint
    
    add key value tests
    
    adjust pester test
    
    remove dir output
    
    Comment dir
    
    remove dir
    
    Continue hunt for dir
    
    lint
    
    find hidden output
    
    improve writes
    
    use value
    
    more debugging comments
    
    add comments
    
    Lint
    
    comment
    
    comment
    
    comment
    
    clear function results
    
    undo comments
    
    fix typo
    
    differentiate string from other types
    
    list all output types
    
    test for desired strings
    
    lint
    
    separate it sections
    
    use global vars correctly
    
    use global vars everywhere
    
    move tests
    
    Fix loop error
    
    Tweak outputs
    
    trim the string
    
    remove testing code
    
    Clean up calling secrets
    
    Remove old comment
    
    removed debugging statement
    
    Remove debugging
    
    Remove unused comments
    
    fix opa version
    
    fix write-warning
    
    test write-verbose
    
    Removed verbose
    
    Removed return
    
    fix extra braces
    
    use env variable
    
    Fixed comments and parameters
    
    read description from manifest
    
    Cleaned up debugging statements
    
    * cleanup and remove debugging
    
    * Add review edits
    
    * add comments
    
    * create $global:SetupTimeThreshold

--- utils/workflow/Initialize-ScubaGearForTesting.ps1
+++ utils/workflow/Initialize-ScubaGearForTesting.ps1
@@ -4,12 +4,9 @@ function Initialize-ScubaGearForTesting {
       Initializes ScubaGear, which installs the necessary modules and tools to run ScubaGear.
   #>
 
-  Write-Output 'Initializing ScubaGear for testing...'
   $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '..\..' -Resolve
-  Write-Output 'The repo root path is'
-  Write-Output $RepoRootPath
-  Write-Output 'Importing function Initialize-Scuba...'
   Import-Module (Join-Path -Path $RepoRootPath -ChildPath 'PowerShell/ScubaGear') -Function Initialize-Scuba
-  Write-Output 'Calling Initialize ScubaGear...'
-  Initialize-SCuBA
+  Write-Debug 'Calling Initialize ScubaGear...'
+  $Outputs = Initialize-SCuBA
+  return $Outputs
 }
