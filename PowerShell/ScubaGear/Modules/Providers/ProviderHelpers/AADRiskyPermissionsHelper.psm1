e6a1c58762f6c5b17de9be9e56f4a1f4c690f318
mitchelbaker-cisa <mitchel.baker@cisa.dhs.gov>
2025-03-04 01:35:08 +0000

    add additional null check to Format-RiskyThirdPartyServicePrincipals

--- PowerShell/ScubaGear/Modules/Providers/ProviderHelpers/AADRiskyPermissionsHelper.psm1
+++ PowerShell/ScubaGear/Modules/Providers/ProviderHelpers/AADRiskyPermissionsHelper.psm1
@@ -438,6 +438,9 @@ function Format-RiskyThirdPartyServicePrincipals {
             #ConvertTo-Json -Depth 10 $RiskySPs > testsps.json 
             $ServicePrincipals = @()
             foreach ($ServicePrincipal in $RiskySPs) {
+                if ($null -eq $ServicePrincipal) {
+                    continue;
+                }
                 #$MatchedApplication = $RiskyApps | Where-Object { $_.AppId -eq $ServicePrincipal.AppId }
 
                 # If a service principal does not have an associated application registration,
