1ed54d71df27915f92a11a7b425c93989716bbe7
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-03-10 08:44:07 -0400

    finish pester test for determine

--- Testing/workflow/Update-Opa.Tests.ps1
+++ Testing/workflow/Update-Opa.Tests.ps1
@@ -6,39 +6,18 @@ Describe "Update OPA" {
     It "Determine if OPA needs to be updated" {
         # Setup important paths
         $RepoRootPath = Join-Path -Path $PSScriptRoot -ChildPath '../..' -Resolve
-        Write-Warning "The repo root path is $RepoRootPath"
         $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/workflow/Update-Opa.ps1' -Resolve
-        Write-Warning "The script path is $ScriptPath"
+        # The current version of OPA used in SG is found in PowerShell/ScubaGear/ScubaGear.psm1
+        # in the variable DefaultOPAVersion
+        $ExpectedCurrentOPAVersion = [ScubaConfig]::GetOpaVersion()
         # Call the function
         . $ScriptPath
         $ReturnValues = Confirm-OpaUpdateRequirements -RepoPath $RepoRootPath
+        $ActualCurrentOPAVersion = $ReturnValues["CurrentOPAVersion"]
+        $LatestOPAVersion = $ReturnValues["LatestOPAVersion"]
         # The latest version of OPA is found here:
         # https://github.com/open-policy-agent/opa/releases
-        $LatestOPAVersion = $ReturnValues["LatestOPAVersion"]
         $LatestOPAVersion | Should -Be "1.2.0"
-        # The current version of OPA used in SG is found in PowerShell/ScubaGear/ScubaGear.psm1
-        # in the variable DefaultOPAVersion
-        # $ScubaConfigPath = Join-Path -Path $RepoRootPath -ChildPath PowerShell/ScubaGear/Modules/ScubaConfig/ScubaConfig.psm1
-        # Write-Warning "The Scuba Config path is $ScubaConfigPath"
-        # $ScubaConfig = [ScubaConfig]::GetInstance()
-        # $Version = $ScubaConfig.GetOpaVersion()
-        # $Version = [ScubaConfig]::ScubaDefault('DefaultOPAVersion')
-        $Version = [ScubaConfig]::GetOpaVersion()
-        Write-Warning "The version from the getter is $Version"
-
-        # $OPAVerRegex = "\'\d+\.\d+\.\d+\'"
-        # $DefaultVersionPattern = "DefaultOPAVersion = $OPAVerRegex"
-        # $ScubaConfigModule = Get-Content $ScubaConfigPath -Raw
-        # $ExpectedCurrentOPAVersion = '0.0.0'
-        # if ($ScubaConfigModule -match $DefaultVersionPattern) {
-        #     $ExpectedCurrentOPAVersion = ($Matches[0] -split "=")[1] -replace " ", ""
-        #     $ExpectedCurrentOPAVersion = $CurrentOPAVersion -replace "'", ""
-        #     Write-Warning "The expected current OPA version is $CurrentOPAVersion."
-        # }
-        # else {
-        #     Write-Error "Failed to get the current OPA version from ScubaConfig.psm1."
-        # }
-        # $ActualCurrentOPAVersion = $ReturnValues["CurrentOPAVersion"]
-        # $ActualCurrentOPAVersion | Should -Be $ExpectedCurrentOPAVersion
+        $ActualCurrentOPAVersion | Should -Be $ExpectedCurrentOPAVersion
     }
 }
