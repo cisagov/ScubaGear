a08a8cbc4bf17c97d1ee1e9ae9869f420fe80829
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-03-11 12:00:25 -0400

    fix string syntax error

--- Testing/action/Get-Email.Tests.ps1
+++ Testing/action/Get-Email.Tests.ps1
@@ -5,7 +5,7 @@ Describe "Get Email Check" {
         $ScriptPath = Join-Path -Path $PSScriptRoot -ChildPath '../../utils/action/Get-Email.ps1' -Resolve
         . $ScriptPath
         # Setup dummy values
-        $FakeSetOfParams = Alias=TenantAlias,TenantDomain=example.onmicrosoft.com,TenantDisplayName=Example Display Name,AppId=12345678-abcd-9012-efgh-345678901234,ProductName=teams,M365Environment=commercial,Emails=someone@example.com
+        $FakeSetOfParams = "Alias=TenantAlias,TenantDomain=example.onmicrosoft.com,TenantDisplayName=Example Display Name,AppId=12345678-abcd-9012-efgh-345678901234,ProductName=teams,M365Environment=commercial,Emails=someone@example.com"
         # Attempt to extract email from the dummy values
         $Email = Get-Email -ProductAlias TenantAlias -Params $FakeSetOfParams
         $Email | Should -Be "someone@example.com"
