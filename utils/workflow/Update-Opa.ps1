b1463c0b25ec0b2f44ed7d51fe38d756e4873a10
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-03-18 15:46:00 -0400

    fix suppression location

--- utils/workflow/Update-Opa.ps1
+++ utils/workflow/Update-Opa.ps1
@@ -15,8 +15,8 @@ function Confirm-OpaUpdateRequirements {
 
     # Check if there is already an update branch
     $OPAVersionBumpBranch = "opa-version-bump-$($LatestOPAVersion)"
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification='$Temp required to avoid having PS eat the return code from git.')]
     try {
+        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification='$Temp required to avoid having PS eat the return code from git.')]
         $Temp = git ls-remote --exit-code --heads origin $OPAVersionBumpBranch
     }
     catch {
