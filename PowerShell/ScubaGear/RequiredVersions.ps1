ebc55055630ffcda63e29c0d48d6b77448dc2bfb
skirkpatrickMSFT <skirkpatrick@microsoft.com>
2025-04-25 14:03:17 -0400

    Update RequiredVersions.ps1

--- PowerShell/ScubaGear/RequiredVersions.ps1
+++ PowerShell/ScubaGear/RequiredVersions.ps1
@@ -3,72 +3,71 @@ $ModuleList = @(
     @{
         ModuleName = 'MicrosoftTeams'
         ModuleVersion = [version] '4.9.3'
-        MaximumVersion = [version] '7.0.0'
+        MaximumVersion = [version] '6.99.99999'
     },
     @{
         ModuleName = 'ExchangeOnlineManagement' # includes Defender
         ModuleVersion = [version] '3.2.0'
-        MaximumVersion = [version] '3.7.2'
+        MaximumVersion = [version] '3.4.99999'
     },
     @{
         ModuleName = 'Microsoft.Online.SharePoint.PowerShell' # includes OneDrive
         ModuleVersion = [version] '16.0.0'
-        MaximumVersion = [version] '16.0.25912.12000'
+        MaximumVersion = [version] '16.0.24810.12000'
     },
     @{
         ModuleName = 'PnP.PowerShell' # alternate for SharePoint PowerShell
         ModuleVersion = [version] '1.12.0'
-        MaximumVersion = [version] '3.1.0'
+        MaximumVersion = [version] '1.99.99999'
     },
     @{
         ModuleName = 'Microsoft.PowerApps.Administration.PowerShell'
         ModuleVersion = [version] '2.0.198'
-        MaximumVersion = [version] '2.0.210'
+        MaximumVersion = [version] '2.0.203'
     },
     @{
         ModuleName = 'Microsoft.PowerApps.PowerShell'
         ModuleVersion = [version] '1.0.0'
-        MaximumVersion = [version] '1.0.40'
+        MaximumVersion = [version] '1.99.99999'
     },
     @{
         ModuleName = 'Microsoft.Graph.Authentication'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.27.0'
+        MaximumVersion = [version] '2.99.99999'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Applications'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.27.0'
+        MaximumVersion = [version] '2.99.99999'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Users'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.27.0'
+        MaximumVersion = [version] '2.99.99999'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Groups'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.27.0'
+        MaximumVersion = [version] '2.99.99999'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Identity.DirectoryManagement'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.27.0'
+        MaximumVersion = [version] '2.99.99999'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.Identity.SignIns'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.27.0'
+        MaximumVersion = [version] '2.99.99999'
     },
     @{
         ModuleName = 'Microsoft.Graph.Beta.DirectoryObjects'
         ModuleVersion = [version] '2.0.0'
-        MaximumVersion = [version] '2.27.0'
+        MaximumVersion = [version] '2.99.99999'
     },
     @{
         ModuleName = 'powershell-yaml'
         ModuleVersion = [version] '0.4.2'
-        MaximumVersion = [version] '0.4.12'
+        MaximumVersion = [version] '0.99.99999'
     }
 )
-
