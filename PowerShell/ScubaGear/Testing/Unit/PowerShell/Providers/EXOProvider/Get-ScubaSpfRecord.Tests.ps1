666295cecce354800c05c69e38f9e150093615c5
Alden Hilton <adhilto@sandia.gov>
2025-04-29 10:27:27 -0700

    Update unit tests for spf, dkim, and dmarc functions

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/EXOProvider/Get-ScubaSpfRecord.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/EXOProvider/Get-ScubaSpfRecord.Tests.ps1
@@ -4,52 +4,116 @@ Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "$($ProviderPath)/Export
 
 InModuleScope 'ExportEXOProvider' {
     Describe -Tag 'ExportEXOProvider' -Name "Get-ScubaSpfRecord" {
-        Context "When high-confidence answers are available" {
-            BeforeAll {
-                Mock -CommandName Write-Warning {}
+        Context "When a txt record is found" {
+            It "Handles correct SPF records" {
                 Mock -CommandName Invoke-RobustDnsTxt {
                     @{
-                        "Answers" = @("v=spf1...");
-                        "HighConfidence" = $true;
-                        "LogEntries" = @("some text")
+                        "Answers" = @("v=spf1 include:spf.protection.outlook.com -all");
+                        "Errors" = @();
+                        "NXDomain" = $false
+                        "LogEntries" = @()
                     }
                 }
-            }
-            It "Resolves 1 domain name" {
-                # Test basic functionality
                 $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
                 Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
-                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
-                $Response.rdata -Contains "v=spf1..." | Should -Be $true
+                $Response.Compliant | Should -Be $true
+                $Response.Message | Should -Be "SPF record found."
             }
 
-            It "Resolves multiple domain names" {
-                # Test to ensure function will loop over the domain names provided in the -Domains argument.
-                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"},
-                    @{"DomainName" = "example.com"})
-                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 2
-                Should -Invoke -CommandName Write-Warning -Exactly -Times 0
-                $Response.rdata -Contains "v=spf1..." | Should -Be $true
-            }
-        }
-        Context "When high-confidence answers are not available" {
-            BeforeAll {
-                Mock -CommandName Write-Warning {}
+            It "Handles soft fail" {
                 Mock -CommandName Invoke-RobustDnsTxt {
                     @{
-                        "Answers" = @("v=spf1...");
-                        "HighConfidence" = $false;
-                        "LogEntries" = @("some text")
+                        "Answers" = @("v=spf1 include:spf.protection.outlook.com ~all");
+                        "Errors" = @();
+                        "NXDomain" = $false
+                        "LogEntries" = @()
                     }
                 }
-            }
-            It "Prints a warning" {
-                # If Invoke-RobustDnsTxt returns a low confidence answer, Get-ScubaSpfRecord should print a
-                # warning.
                 $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
                 Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
-                Should -Invoke -CommandName Write-Warning -Exactly -Times 1
-                $Response.rdata -Contains "v=spf1..." | Should -Be $true
+                $Response.Compliant | Should -Be $false
+                $Response.Message |
+                    Should -Be "SPF record found, but it does not hardfail (`"-all`") or redirect to one that does."
+            }
+
+            It "Handles no SPF record" {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @("something else, like a domain verification record");
+                        "Errors" = @();
+                        "NXDomain" = $false
+                        "LogEntries" = @()
+                    }
+                }
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                $Response.Compliant | Should -Be $false
+                $Response.Message |
+                    Should -Be "Domain name exists but no SPF records returned."
+            }
+
+            It "Handles some errors but answer still found" {
+                # There can be errors but still have a valid answer. For example, if the traditional DNS query failed
+                # but DoH worked
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @("v=spf1 include:spf.protection.outlook.com -all");
+                        "Errors" = @("something went wrong");
+                        "NXDomain" = $false
+                        "LogEntries" = @()
+                    }
+                }
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                $Response.Compliant | Should -Be $true
+                $Response.Message | Should -Be "SPF record found."
+            }
+        }
+
+        Context "When no txt record is found" {
+            It "Handles NXDomain" {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @();
+                        "Errors" = @();
+                        "NXDomain" = $true
+                        "LogEntries" = @()
+                    }
+                }
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                $Response.Compliant | Should -Be $false
+                $Response.Message | Should -Be "Domain does not exist."
+            }
+
+            It "Handles empty answer" {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @();
+                        "Errors" = @();
+                        "NXDomain" = $false
+                        "LogEntries" = @()
+                    }
+                }
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                $Response.Compliant | Should -Be $false
+                $Response.Message | Should -Be "Domain name exists but no answers returned."
+            }
+
+            It "Handles errors" {
+                Mock -CommandName Invoke-RobustDnsTxt {
+                    @{
+                        "Answers" = @();
+                        "Errors" = @("something went wrong");
+                        "NXDomain" = $false
+                        "LogEntries" = @()
+                    }
+                }
+                $Response = Get-ScubaSpfRecord -Domains @(@{"DomainName" = "example.com"})
+                Should -Invoke -CommandName Invoke-RobustDnsTxt -Exactly -Times 1
+                $Response.Compliant | Should -Be $false
+                $Response.Message | Should -Be "Exceptions other than NXDOMAIN returned."
             }
         }
     }
