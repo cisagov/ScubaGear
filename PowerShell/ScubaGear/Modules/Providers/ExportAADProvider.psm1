b61269732b9fb788847b5da0a2fef26cbed44a74
mitchelbaker-cisa <mitchel.baker@cisa.dhs.gov>
2025-05-15 19:06:56 +0000

    add comments to explain different scenarios for app reg/SP

--- PowerShell/ScubaGear/Modules/Providers/ExportAADProvider.psm1
+++ PowerShell/ScubaGear/Modules/Providers/ExportAADProvider.psm1
@@ -153,12 +153,23 @@ function Export-AADProvider {
     $RiskyApps = if ($null -eq $RiskyApps -or @($RiskyApps).Count -eq 0) { @() } else { $RiskyApps }
     $RiskySPs = if ($null -eq $RiskySPs -or @($RiskySPs).Count -eq 0) { @() } else { $RiskySPs }
 
+    # There are four cases that can occur
+    # 1. Both risky apps and risky 3rd party SPs exist
+    # 2. Neither risky apps or risky 3rd party SPs exist
+    # 3. No risky apps exist but risky 3rd party SPs exist
+    # 4. Risky apps exist but no risky 3rd party SPs exist
+
+    # "Format-RiskyApplications" will match app registrations with and without a corresponding service principal object.
+    # If an app registration does not have a service principal object, only app registration data will be displayed.
+    # If an app registration has a matching service principal object, app registration and service principal data will be aggregated together.
     $AggregateRiskyApps = ConvertTo-Json -Depth 3 @(
         if (@($RiskyApps).Count -gt 0 -and @($RiskySPs).Count -gt 0) {
             $Tracker.TryCommand("Format-RiskyApplications", @{"RiskyApps"=$RiskyApps; "RiskySPs"=$RiskySPs})
         }
     )
 
+    # "Format-RiskyThirdPartyServicePrincipals" does NOT return service principals created in its home tenant.
+    # It only returns risky service principals owned by external tenants. 
     $RiskyThirdPartySPs = ConvertTo-Json -Depth 3 @(
         if (@($RiskySPs).Count -gt 0) {
             $Tracker.TryCommand("Format-RiskyThirdPartyServicePrincipals", @{"RiskySPs"=$RiskySPs})
