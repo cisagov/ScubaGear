c333152f9bd0de9cc6ab21cd6c1ced1c9c63cc8e
GitHub Action <action@github.com>
2025-05-30 02:44:54 +0000

    Bump OPA version from v1.3.0 to  v1.5.0

--- PowerShell/ScubaGear/Modules/ScubaConfig/ScubaConfig.psm1
+++ PowerShell/ScubaGear/Modules/ScubaConfig/ScubaConfig.psm1
@@ -41,7 +41,7 @@ class ScubaConfig {
             "Hybrid Identity Administrator",
             "Application Administrator",
             "Cloud Application Administrator")
-        DefaultOPAVersion = '1.3.0'
+        DefaultOPAVersion = '1.5.0'
     }
 
     static [object]ScubaDefault ([string]$Name){
