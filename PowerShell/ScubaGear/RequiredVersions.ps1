3bcc9d782e3910b919d6354331ded4bfc56503a0
skirkpatrickMSFT <skirkpatrick@microsoft.com>
2025-09-09 16:12:12 -0400

    Update maximum version for PnP.PowerShell module

--- PowerShell/ScubaGear/RequiredVersions.ps1
+++ PowerShell/ScubaGear/RequiredVersions.ps1
@@ -19,7 +19,8 @@ $ModuleList = @(
     @{
         ModuleName = 'PnP.PowerShell' # alternate for SharePoint PowerShell
         ModuleVersion = [version] '1.12.0'
-        MaximumVersion = [version] '1.99.99999'
+        MaximumVersion = [version] '2.22.0'
+        IsPinned = "True"
     },
     @{
         ModuleName = 'Microsoft.PowerApps.Administration.PowerShell'
