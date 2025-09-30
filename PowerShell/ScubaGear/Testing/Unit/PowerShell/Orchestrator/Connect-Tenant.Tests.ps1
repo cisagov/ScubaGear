1c2d29ad75fcb5d7ac3088b2e80546283d11e59c
skirkpatrickMSFT <skirkpatrick@microsoft.com>
2025-09-30 14:34:49 -0400

    Refactor tests for Invoke-Connection in Connect-Tenant

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Connect-Tenant.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Connect-Tenant.Tests.ps1
@@ -1,91 +1,61 @@
-BeforeDiscovery {
-    $ModuleRootPath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\..\..\Modules'
-    Import-Module (Join-Path -Path $ModuleRootPath -ChildPath 'Orchestrator.psm1') -Force
-    Import-Module (Join-Path -Path $ModuleRootPath -ChildPath "Connection\Connection.psm1") -Force
-}
+$ModuleRootPath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\..\..\Modules'
+Import-Module (Join-Path -Path $ModuleRootPath -ChildPath 'Orchestrator.psm1') -Force
 
 InModuleScope Orchestrator {
     Describe -Tag 'Orchestrator' -Name 'Invoke-Connection' {
-        Context 'When interactively connecting to commercial Endpoints' {
-            BeforeAll {
-                [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ConnectParams')]
-                $ConnectParams = @{
+        Context 'When LogIn is true' {
+            BeforeEach {
+                # Fresh config & mock per test to avoid state leakage
+                $script:ScubaConfig = [PSCustomObject]@{
+                    ProductNames    = @('aad')  # default, overridden in tests
                     LogIn           = $true
-                    M365Environment = "commercial"
-                    BoundParameters = @{}
+                    M365Environment = 'commercial'
                 }
-
-                function Connect-Tenant {}
-                Mock Connect-Tenant{@()}
-
+                $script:BoundParameters = @{}
+                function Connect-Tenant { throw 'this will be mocked' }
+                Mock -ModuleName Orchestrator Connect-Tenant { @() }
             }
-            It 'With -ProductNames "aad", connects to Microsoft Graph' {
-                $ConnectParams += @{
-                    ProductNames = 'aad'
-                }
-                Invoke-Connection @ConnectParams |
+
+            It 'connects to aad (single product)' {
+                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters
                 Should -Invoke -CommandName Connect-Tenant -Times 1 -Exactly
-                $FailedAuthList.Length | Should -Be 0
+                $FailedAuthList | Should -BeNullOrEmpty
             }
-            It 'With -ProductNames "defender", connects to Microsoft Defender for Office 365' {
-                $ConnectParams += @{
-                    ProductNames = 'defender'
-                }
-                $FailedAuthList = Invoke-Connection @ConnectParams
-                $FailedAuthList.Length | Should -Be 0
+            It 'connects to defender' {
+                $ScubaConfig.ProductNames = @('defender')
+                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters
+                Should -Invoke -CommandName Connect-Tenant -Times 1 -Exactly
+                $FailedAuthList | Should -BeNullOrEmpty
             }
-            It 'With -ProductNames "exo", connects to Exchange Online' {
-                $ConnectParams += @{
-                    ProductNames = 'exo'
-                }
-                $FailedAuthList = Invoke-Connection @ConnectParams
-                $FailedAuthList.Length | Should -Be 0
+            It 'connects to all products in one call' {
+                $ScubaConfig.ProductNames = @('aad','defender','exo','powerplatform','sharepoint','teams')
+                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters
+                # Still only one Connect-Tenant invocation expected (array passed)
+                Should -Invoke -CommandName Connect-Tenant -Times 1 -Exactly
+                $FailedAuthList | Should -BeNullOrEmpty
             }
-            It 'With -ProductNames "powerplatform", connects to Power Platform' {
-                $ConnectParams += @{
-                    ProductNames = 'powerplatform'
-                }
-                $FailedAuthList = Invoke-Connection @ConnectParams
-                $FailedAuthList.Length | Should -Be 0
-            }
-            It 'With -ProductNames "sharepoint", connects to SharePoint Online' {
-                $ConnectParams += @{
-                    ProductNames = 'sharepoint'
-                }
-                $FailedAuthList = Invoke-Connection @ConnectParams
-                $FailedAuthList.Length | Should -Be 0
-            }
-            It 'With -ProductNames "teams", connects to Microsoft Teams' {
-                $ConnectParams += @{
-                    ProductNames = 'teams'
-                }
-                $FailedAuthList = Invoke-Connection @ConnectParams
-                $FailedAuthList.Length | Should -Be 0
-            }
-            It 'authenticates to all products' {
-                $ConnectParams += @{
-                    ProductNames = @("aad", "defender", "exo", "powerplatform", "sharepoint", "teams")
-                }
-                $FailedAuthList = Invoke-Connection @ConnectParams
-                $FailedAuthList.Length | Should -Be 0
+            It 'passes service principal parameters when provided' {
+                $ScubaConfig.ProductNames = @('aad')
+                $BoundParameters = @{ AppID = 'a'; CertificateThumbprint = 'b'; Organization = 'c' }
+                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters
+                Should -Invoke -CommandName Connect-Tenant -Times 1 -Exactly
+                $FailedAuthList | Should -BeNullOrEmpty
             }
         }
-        Context 'When -Login $false' {
-            BeforeAll {
-                [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'ConnectParams')]
-                $ConnectParams = @{
+
+        Context 'When LogIn is false' {
+            BeforeEach {
+                $script:ScubaConfig = [PSCustomObject]@{
+                    ProductNames    = @('aad')
                     LogIn           = $false
-                    M365Environment = "gcc"
-                    BoundParameters = @{}
+                    M365Environment = 'commercial'
                 }
-                function Connect-Tenant {}
-                Mock -ModuleName Orchestrator Connect-Tenant {@()}
+                $script:BoundParameters = @{}
+                function Connect-Tenant { throw 'this will be mocked' }
+                Mock -ModuleName Orchestrator Connect-Tenant { @() }
             }
-            It 'does not authenticate' {
-                $ConnectParams += @{
-                    ProductNames = 'aad'
-                }
-                Invoke-Connection @ConnectParams
+            It 'does not attempt authentication' {
+                Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters | Should -BeNullOrEmpty
                 Should -Invoke -CommandName Connect-Tenant -Times 0 -Exactly
             }
         }
