a35e635fa262f49a887150ea486a07bd22a3361c
skirkpatrickMSFT <skirkpatrick@microsoft.com>
2025-10-01 11:56:45 -0400

    Refactor Invoke-Connection calls in tests

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Connect-Tenant.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Connect-Tenant.Tests.ps1
@@ -11,33 +11,38 @@ InModuleScope Orchestrator {
                     LogIn           = $true
                     M365Environment = 'commercial'
                 }
-                $script:BoundParameters = @{}
                 function Connect-Tenant { throw 'this will be mocked' }
                 Mock -ModuleName Orchestrator Connect-Tenant { @() }
             }
 
             It 'connects to aad (single product)' {
-                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters
+                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig
                 Should -Invoke -CommandName Connect-Tenant -Times 1 -Exactly
                 $FailedAuthList | Should -BeNullOrEmpty
             }
             It 'connects to defender' {
                 $ScubaConfig.ProductNames = @('defender')
-                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters
+                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig
                 Should -Invoke -CommandName Connect-Tenant -Times 1 -Exactly
                 $FailedAuthList | Should -BeNullOrEmpty
             }
             It 'connects to all products in one call' {
                 $ScubaConfig.ProductNames = @('aad','defender','exo','powerplatform','sharepoint','teams')
-                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters
+                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig
                 # Still only one Connect-Tenant invocation expected (array passed)
                 Should -Invoke -CommandName Connect-Tenant -Times 1 -Exactly
                 $FailedAuthList | Should -BeNullOrEmpty
             }
             It 'passes service principal parameters when provided' {
-                $ScubaConfig.ProductNames = @('aad')
-                $BoundParameters = @{ AppID = 'a'; CertificateThumbprint = 'b'; Organization = 'c' }
-                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters
+                $ScubaConfig = [PSCustomObject]@{
+                    ProductNames = @('aad')
+                    LogIn = $true
+                    M365Environment = 'commercial'
+                    AppID = 'a'
+                    CertificateThumbprint = 'b'
+                    Organization = 'c'
+                }
+                $FailedAuthList = Invoke-Connection -ScubaConfig $ScubaConfig
                 Should -Invoke -CommandName Connect-Tenant -Times 1 -Exactly
                 $FailedAuthList | Should -BeNullOrEmpty
             }
@@ -50,12 +55,11 @@ InModuleScope Orchestrator {
                     LogIn           = $false
                     M365Environment = 'commercial'
                 }
-                $script:BoundParameters = @{}
                 function Connect-Tenant { throw 'this will be mocked' }
                 Mock -ModuleName Orchestrator Connect-Tenant { @() }
             }
             It 'does not attempt authentication' {
-                Invoke-Connection -ScubaConfig $ScubaConfig -BoundParameters $BoundParameters | Should -BeNullOrEmpty
+                Invoke-Connection -ScubaConfig $ScubaConfig | Should -BeNullOrEmpty
                 Should -Invoke -CommandName Connect-Tenant -Times 0 -Exactly
             }
         }
