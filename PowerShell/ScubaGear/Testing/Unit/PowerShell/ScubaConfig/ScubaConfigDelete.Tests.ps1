ec4123de81df2a4519c65bc36723baad17e8c0af
DickTracyII <ritracyi@microsoft.com>
2025-11-10 10:02:37 -0500

    revert original tests

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/ScubaConfig/ScubaConfigDelete.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/ScubaConfig/ScubaConfigDelete.Tests.ps1
@@ -1,99 +1,73 @@
 using module '..\..\..\..\Modules\ScubaConfig\ScubaConfig.psm1'
 
 InModuleScope ScubaConfig {
-    Describe -tag "Utils" -name 'ScubaConfigDelete - First Configuration' {
-        BeforeAll{
-            # Initialize the system
-            [ScubaConfig]::InitializeValidator()
-
-            # Create temporary YAML file for testing
-            $script:TempConfigFile1 = [System.IO.Path]::GetTempFileName()
-            $script:TempConfigFile1 = [System.IO.Path]::ChangeExtension($script:TempConfigFile1, '.yaml')
-
-            # First config with multiple products and an object
-            @"
-ProductNames:
-  - teams
-  - exo
-  - defender
-  - aad
-  - powerplatform
-  - sharepoint
-AnObject:
-  name: MyObjectName
-"@ | Set-Content -Path $script:TempConfigFile1
-
-            # Load the config once for all tests in this Describe
-            [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile1)
-        }
-
-        It 'Valid config file loads successfully'{
-            [ScubaConfig]::GetInstance().Configuration | Should -Not -BeNullOrEmpty
-        }
-
-        It 'Configuration has 6 product names'{
-            [ScubaConfig]::GetInstance().Configuration.ProductNames | Should -HaveCount 6
-        }
-
-        It 'Configuration has valid object parameter AnObject'{
-            [ScubaConfig]::GetInstance().Configuration.AnObject.name | Should -Be 'MyObjectName'
-        }
-
-        It 'Configuration does not have MissingObject'{
-            [ScubaConfig]::GetInstance().Configuration.MissingObject | Should -BeNullOrEmpty
-        }
-
-        AfterAll{
-            [ScubaConfig]::ResetInstance()
-            if (Test-Path $script:TempConfigFile1) {
-                Remove-Item $script:TempConfigFile1 -Force
+    Describe -tag "Utils" -name 'ScubaConfigDelete' {
+        context 'Delete configuration' {
+            BeforeAll{
+                [ScubaConfig]::ResetInstance()
+                function global:ConvertFrom-Yaml {
+                    @{
+                        ProductNames=@("teams", "exo", "defender", "aad", "powerplatform", "sharepoint")
+                        AnObject=@{name='MyObjectName'}
+                    }
+                }
+                # Create a temporary YAML file for testing
+                $script:TempConfigFile = [System.IO.Path]::GetTempFileName()
+                $script:TempConfigFile = [System.IO.Path]::ChangeExtension($script:TempConfigFile, '.yaml')
+                "ProductNames: ['aad']" | Set-Content -Path $script:TempConfigFile
             }
-        }
-    }
-
-    Describe -tag "Utils" -name 'ScubaConfigDelete - Second Configuration' {
-        BeforeAll{
-            # Initialize the system
-            [ScubaConfig]::InitializeValidator()
-
-            # Create temporary YAML file for testing
-            $script:TempConfigFile2 = [System.IO.Path]::GetTempFileName()
-            $script:TempConfigFile2 = [System.IO.Path]::ChangeExtension($script:TempConfigFile2, '.yaml')
-
-            # Second config with single product and different object
-            @"
-ProductNames:
-  - teams
-MissingObject:
-  name: MyMissingObjectName
-"@ | Set-Content -Path $script:TempConfigFile2
-
-            # Load the config once for all tests in this Describe
-            [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile2)
-        }
-
-        It 'Valid config file loads successfully'{
-            [ScubaConfig]::GetInstance().Configuration | Should -Not -BeNullOrEmpty
-        }
-
-        It 'Configuration has 1 product name'{
-            $ProductNames = [ScubaConfig]::GetInstance().Configuration.ProductNames
-            $ProductNames.Count | Should -Be 1
-            $ProductNames | Should -Contain 'teams'
-        }
-
-        It 'Configuration does not have AnObject'{
-            [ScubaConfig]::GetInstance().Configuration.AnObject | Should -BeNullOrEmpty
-        }
-
-        It 'Configuration has MissingObject'{
-            [ScubaConfig]::GetInstance().Configuration.MissingObject.name | Should -Be 'MyMissingObjectName'
-        }
-
-        AfterAll{
-            [ScubaConfig]::ResetInstance()
-            if (Test-Path $script:TempConfigFile2) {
-                Remove-Item $script:TempConfigFile2 -Force
+            It 'Valid config file'{
+                $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile)
+                $Result | Should -Be $true
+            }
+            It '6 Product names'{
+                $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile)
+                $Result | Should -Be $true
+                [ScubaConfig]::GetInstance().Configuration.ProductNames | Should -HaveCount 6 -Because "$([ScubaConfig]::GetInstance().Configuration.ParameterNames)"
+            }
+            It 'Valid object parameter'{
+                $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile)
+                $Result | Should -Be $true
+                [ScubaConfig]::GetInstance().Configuration.AnObject.name | Should -Be 'MyObjectName'
+            }
+            It 'Valid object parameter'{
+                $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile)
+                $Result | Should -Be $true
+                [ScubaConfig]::GetInstance().Configuration.MissingObject.name | Should -BeNullOrEmpty
+            }
+            It 'Reset Instance'{
+                [ScubaConfig]::ResetInstance()
+                function global:ConvertFrom-Yaml {
+                    @{
+                        ProductNames=@("teams")
+                        MissingObject=@{name='MyMissingObjectName'}
+                    }
+                }
+            }
+            It 'A different valid config file'{
+                $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile)
+                $Result | Should -Be $true
+            }
+            It '1 Product names'{
+                $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile)
+                $Result | Should -Be $true
+                [ScubaConfig]::GetInstance().Configuration.ProductNames | Should -HaveCount 1
+            }
+            It 'Valid object parameter'{
+                $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile)
+                $Result | Should -Be $true
+                [ScubaConfig]::GetInstance().Configuration.AnObject.name | Should -BeNullOrEmpty
+            }
+            It 'Valid object parameter'{
+                $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile)
+                $Result | Should -Be $true
+                [ScubaConfig]::GetInstance().Configuration.MissingObject.name | Should -Be 'MyMissingObjectName'
+            }
+            AfterAll{
+                [ScubaConfig]::ResetInstance()
+                if (Test-Path $script:TempConfigFile) {
+                    Remove-Item $script:TempConfigFile -Force
+                }
             }
         }
     }
