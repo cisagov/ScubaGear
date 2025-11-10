5d5f7d5b837a51f9acb70aacd2393b44e3e00f91
DickTracyII <ritracyi@microsoft.com>
2025-11-09 19:35:18 -0500

    spaces removed

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/ScubaConfig/ScubaConfigDelete.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/ScubaConfig/ScubaConfigDelete.Tests.ps1
@@ -5,14 +5,14 @@ InModuleScope ScubaConfig {
         context 'Delete configuration' {
             BeforeAll{
                 [ScubaConfig]::ResetInstance()
-                
+
                 # Create temporary YAML files for testing
                 $script:TempConfigFile1 = [System.IO.Path]::GetTempFileName()
                 $script:TempConfigFile1 = [System.IO.Path]::ChangeExtension($script:TempConfigFile1, '.yaml')
-                
+
                 $script:TempConfigFile2 = [System.IO.Path]::GetTempFileName()
                 $script:TempConfigFile2 = [System.IO.Path]::ChangeExtension($script:TempConfigFile2, '.yaml')
-                
+
                 # First config with multiple products and an object
                 @"
 ProductNames:
@@ -25,7 +25,7 @@ ProductNames:
 AnObject:
   name: MyObjectName
 "@ | Set-Content -Path $script:TempConfigFile1
-                
+
                 # Second config with single product and different object
                 @"
 ProductNames:
@@ -34,48 +34,48 @@ MissingObject:
   name: MyMissingObjectName
 "@ | Set-Content -Path $script:TempConfigFile2
             }
-            
+
             It 'Valid config file loads successfully'{
                 $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile1)
                 $Result | Should -Be $true
             }
-            
+
             It 'Configuration has 6 product names'{
                 [ScubaConfig]::GetInstance().Configuration.ProductNames | Should -HaveCount 6
             }
-            
+
             It 'Configuration has valid object parameter AnObject'{
                 [ScubaConfig]::GetInstance().Configuration.AnObject.name | Should -Be 'MyObjectName'
             }
-            
+
             It 'Configuration does not have MissingObject'{
                 [ScubaConfig]::GetInstance().Configuration.MissingObject | Should -BeNullOrEmpty
             }
-            
+
             It 'Reset instance clears configuration'{
                 [ScubaConfig]::ResetInstance()
                 [ScubaConfig]::GetInstance().Configuration | Should -BeNullOrEmpty
             }
-            
+
             It 'Load second config file successfully'{
                 $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile2)
                 $Result | Should -Be $true
             }
-            
+
             It 'Second configuration has 1 product name'{
                 $ProductNames = [ScubaConfig]::GetInstance().Configuration.ProductNames
                 $ProductNames.Count | Should -Be 1
                 $ProductNames | Should -Contain 'teams'
             }
-            
+
             It 'Second configuration does not have AnObject'{
                 [ScubaConfig]::GetInstance().Configuration.AnObject | Should -BeNullOrEmpty
             }
-            
+
             It 'Second configuration has MissingObject'{
                 [ScubaConfig]::GetInstance().Configuration.MissingObject.name | Should -Be 'MyMissingObjectName'
             }
-            
+
             AfterAll{
                 [ScubaConfig]::ResetInstance()
                 if (Test-Path $script:TempConfigFile1) {
--- PowerShell/ScubaGear/Testing/Unit/PowerShell/ScubaConfig/ScubaConfigLoadConfig.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/ScubaConfig/ScubaConfigLoadConfig.Tests.ps1
@@ -20,46 +20,46 @@ InModuleScope ScubaConfig {
                 [ScubaConfig]::ResetInstance()
             }
         }
-        
+
         context 'Handling repeated LoadConfig invocations' {
             BeforeAll {
                 # Create two temporary YAML files for testing
                 $script:TempConfigFile1 = [System.IO.Path]::GetTempFileName()
                 $script:TempConfigFile1 = [System.IO.Path]::ChangeExtension($script:TempConfigFile1, '.yaml')
-                
+
                 $script:TempConfigFile2 = [System.IO.Path]::GetTempFileName()
                 $script:TempConfigFile2 = [System.IO.Path]::ChangeExtension($script:TempConfigFile2, '.yaml')
-                
+
                 # First config with teams
                 @"
 ProductNames:
   - teams
 "@ | Set-Content -Path $script:TempConfigFile1
-                
+
                 # Second config with exo
                 @"
 ProductNames:
   - exo
 "@ | Set-Content -Path $script:TempConfigFile2
             }
-            
+
             It 'Load valid config file followed by another'{
                 $cfg = [ScubaConfig]::GetInstance()
-                
+
                 # Load the first file and check the ProductNames value
                 [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile1) | Should -BeTrue
                 $cfg.Configuration.ProductNames | Should -Contain 'teams'
                 $cfg.Configuration.ProductNames.Count | Should -Be 1
-                
+
                 # Load the second file and verify that ProductNames has changed
                 [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile2) | Should -BeTrue
                 $cfg.Configuration.ProductNames | Should -Contain 'exo'
                 $cfg.Configuration.ProductNames | Should -Not -Contain 'teams'
                 $cfg.Configuration.ProductNames.Count | Should -Be 1
-                
+
                 Should -Invoke -CommandName Write-Warning -Exactly -Times 0
             }
-            
+
             AfterAll {
                 [ScubaConfig]::ResetInstance()
                 if (Test-Path $script:TempConfigFile1) {
