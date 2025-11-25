813c434d34dcd404c104425191202eccce3ea6c4
DickTracyII <ritracyi@microsoft.com>
2025-11-25 15:28:35 -0500

    new branch for 1437.

---
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/ScubaConfig/ScubaConfigJsonMissingDefaults.Tests.ps1
@@ -0,0 +1,62 @@
+using module '..\..\..\..\Modules\ScubaConfig\ScubaConfig.psm1'
+
+InModuleScope ScubaConfig {
+    Describe -tag "Utils" -name 'ScubaConfigMissingDefaults' {
+        BeforeAll {
+            [ScubaConfig]::ResetInstance()
+        }
+
+        AfterAll {
+            [ScubaConfig]::ResetInstance()
+        }
+
+        Context 'General case'{
+            It 'Get Instance without loading'{
+               $Config1 = [ScubaConfig]::GetInstance()
+               $Config1 | Should -Not -BeNull
+               $Config2 =  [ScubaConfig]::GetInstance()
+
+               $Config1 -eq $Config2 | Should -Be $true
+            }
+            It 'Load invalid path'{
+                {[ScubaConfig]::GetInstance().LoadConfig('Bad path name')}| Should -Throw -ExceptionType([System.IO.FileNotFoundException])
+            }
+        }
+        context 'JSON Configuration' {
+            BeforeAll {
+                # Create a temporary YAML file for testing
+                $script:TempConfigFile = [System.IO.Path]::GetTempFileName()
+                $script:TempConfigFile = [System.IO.Path]::ChangeExtension($script:TempConfigFile, '.yaml')
+                "ProductNames: ['aad']" | Set-Content -Path $script:TempConfigFile
+            }
+            It 'Load valid config file'{
+                #uses global function instead of mocking
+                function global:ConvertFrom-Yaml {
+                    @{
+                        ProductNames=@('aad')
+                        M365Environment='commercial'
+                        AnObject=@{name='MyObjectName'}
+                        DisconnectOnExit = $false
+                    }
+                }
+                [ScubaConfig]::ResetInstance()
+                $Result = [ScubaConfig]::GetInstance().LoadConfig($script:TempConfigFile)
+                $Result | Should -Be $true
+            }
+            It 'Valid string parameter'{
+                [ScubaConfig]::GetInstance().Configuration.M365Environment | Should -Be 'commercial'
+            }
+            It 'Valid array parameter'{
+                [ScubaConfig]::GetInstance().Configuration.ProductNames | Should -Contain 'aad'
+            }
+            It 'Valid boolean parameter'{
+                [ScubaConfig]::GetInstance().Configuration.DisconnectOnExit | Should -Be $false
+            }
+            AfterAll {
+                if (Test-Path $script:TempConfigFile) {
+                    Remove-Item $script:TempConfigFile -Force
+                }
+            }
+        }
+    }
+}
