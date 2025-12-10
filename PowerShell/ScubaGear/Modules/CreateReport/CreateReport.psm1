54394ee795786f7df685c8c8879e8c86daac9bd1
dagarwal-ecs <devesh.agarwal@ecstech.com>
2025-12-10 18:17:51 -0500

    Update ResolutionDate from N/A to null (#1891)
    * update from N/A to null
    
    * update based on pr feedback

--- PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
+++ PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
@@ -290,7 +290,8 @@ function New-Report {
                         "OriginalResult"=$Result.DisplayString
                         "OriginalDetails"=$OriginalDetails
                         "Comments"=$Comments
-                        "ResolutionDate"= if ([string]::IsNullOrEmpty($OmitExpiration)) {"N/A"} else {$OmitExpiration}
+                        # Use null to clearly indicate absence of a date rather than the string "N/A"
+                        "ResolutionDate"= if ([string]::IsNullOrEmpty($OmitExpiration)) {$null} else {$OmitExpiration}
                     }
                     continue
                 }
@@ -320,7 +321,8 @@ function New-Report {
                         "OriginalResult"=$Result.DisplayString
                         "OriginalDetails"=$OriginalDetails
                         "Comments"=$Comments
-                        "ResolutionDate"= if ([string]::IsNullOrEmpty($RemediationDate)) {"N/A"} else {$RemediationDate}
+                        # Use null to clearly indicate absence of a date rather than the string "N/A"
+                        "ResolutionDate"= if ([string]::IsNullOrEmpty($RemediationDate)) {$null} else {$RemediationDate}
                     }
                     continue
                 }
@@ -340,7 +342,8 @@ function New-Report {
                     "OriginalResult"=$Result.DisplayString
                     "OriginalDetails"=$OriginalDetails
                     "Comments"=$Comments
-                    "ResolutionDate"= if ([string]::IsNullOrEmpty($RemediationDate)) {"N/A"} else {$RemediationDate}
+                    # Use null to clearly indicate absence of a date rather than the string "N/A"
+                    "ResolutionDate"= if ([string]::IsNullOrEmpty($RemediationDate)) {$null} else {$RemediationDate}
                 }
             }
             else {
@@ -361,8 +364,7 @@ function New-Report {
                     "OriginalResult"=$ControlResult
                     "OriginalDetails"=$ControlDetails
                     "Comments"=$Comments
-                    "AnnotationRemediationDate"="N/A"
-                    "OmissionExpirationDate"="N/A"
+                    "ResolutionDate"= if ([string]::IsNullOrEmpty($RemediationDate)) {$null} else {$RemediationDate}
                 }
                 Write-Warning -Message "WARNING: No test results found for Control Id $($Control.Id)"
             }
