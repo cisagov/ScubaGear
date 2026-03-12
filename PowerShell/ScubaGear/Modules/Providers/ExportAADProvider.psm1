a7481dfcb699046f7cd597481abc197f13f0e11f
mitchelbaker-cisa <mitchel.baker@cisa.dhs.gov>
2026-03-12 20:31:21 +0000

    add initial get-legacyexchangeserviceprincipal prototype

--- PowerShell/ScubaGear/Modules/Providers/ExportAADProvider.psm1
+++ PowerShell/ScubaGear/Modules/Providers/ExportAADProvider.psm1
@@ -199,6 +199,26 @@ function Export-AADProvider {
     )
     ##### End block
 
+    ##### This block gathers information on Exchange hybrid application configurations
+    Import-Module $PSScriptRoot/ProviderHelpers/AADHybridExchangeHelper.psm1
+
+    # Check if the first-party Office 365 Exchange Online service principal is configured with credentials.
+    # This is an indicator of compromise if keyCredentials are present. The organization has not completed
+    # remediation per Microsoft's guidance to remove remaining key credentials after migrating to the new
+    # dedicated hybrid application, or they are still in the legacy hybrid configuration.
+    $LegacyExchangeSP = ConvertTo-Json -Depth 3 @(
+        $Tracker.TryCommand("Get-LegacyExchangeServicePrincipal", @{
+            "M365Environment"=$M365Environment
+        })
+    )
+
+    #$DedicatedExchangeHybridApp = ConvertTo-Json -Depth 3 @(
+    #    $Tracker.TryCommand("Get-DedicatedExchangeHybridApplication", @{
+    #        "M365Environment"=$M365Environment
+    #    })
+    #)
+    ##### End block
+
     $SuccessfulCommands = ConvertTo-Json @($Tracker.GetSuccessfulCommands())
     $UnSuccessfulCommands = ConvertTo-Json @($Tracker.GetUnSuccessfulCommands())
 
@@ -219,6 +239,7 @@ function Export-AADProvider {
     "total_user_count": $UserCount,
     "risky_applications": $AggregateRiskyApps,
     "risky_third_party_service_principals": $RiskyThirdPartySPs,
+    "legacy_exchange_service_principal": $LegacyExchangeSP,
     "aad_successful_commands": $SuccessfulCommands,
     "aad_unsuccessful_commands": $UnSuccessfulCommands,
 "@
