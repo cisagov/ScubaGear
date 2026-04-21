4949e4390bb1d75814e6c6df0de5765a9fc15f9f
Hilton <Alden>
2026-04-21 15:15:28 -0700

    update copy baseline function

--- PowerShell/ScubaGear/Modules/Support/Support.psm1
+++ PowerShell/ScubaGear/Modules/Support/Support.psm1
@@ -30,7 +30,7 @@ function Copy-SCuBABaselineDocument {
         New-Item -ItemType Directory -Path $Destination | Out-Null
     }
 
-    @("teams", "exo", "defender", "aad", "powerbi", "powerplatform", "sharepoint") | ForEach-Object {
+    @("teams", "exo", "securitysuite", "aad", "powerbi", "powerplatform", "sharepoint") | ForEach-Object {
         $SourceFileName = Join-Path -Path $PSScriptRoot -ChildPath "..\..\baselines\$_.md"
         $TargetFileName = Join-Path -Path $Destination -ChildPath "$_.md"
         Copy-Item -Path $SourceFileName -Destination $Destination -Force:$Force -ErrorAction Stop  2> $null
