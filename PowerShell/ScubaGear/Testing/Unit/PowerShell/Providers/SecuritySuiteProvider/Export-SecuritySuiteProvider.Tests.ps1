a65b37a9458a442b0e843013b8f54f099801ce68
Ted Kolovos <107076927+tkol2022@users.noreply.github.com>
2026-05-06 14:05:03 -0400

    security suite unit test was missing mock for Get-HostedConnectionFilterPolicy and Write-ScubaLog

--- PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/SecuritySuiteProvider/Export-SecuritySuiteProvider.Tests.ps1
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Providers/SecuritySuiteProvider/Export-SecuritySuiteProvider.Tests.ps1
@@ -38,7 +38,7 @@ InModuleScope -ModuleName ExportSecuritySuiteProvider {
                                 $this.SuccessfulCommands += $Command
                                 return [pscustomobject]@{}
                             }
-                            "Get-HostedContentFilterPolicy" {
+                            "Get-HostedConnectionFilterPolicy" {
                                 $this.SuccessfulCommands += $Command
                                 return [pscustomobject]@{}
                             }
@@ -141,6 +141,9 @@ InModuleScope -ModuleName ExportSecuritySuiteProvider {
             Mock -ModuleName ExportSecuritySuiteProvider Get-AtpPolicyForO365 {}
             function Get-MgBetaUser {}
             Mock -ModuleName ExportSecuritySuiteProvider Get-MgBetaUser {}
+
+            function Write-ScubaLog {}
+            Mock -ModuleName ExportSecuritySuiteProvider Write-ScubaLog {}
 
             function Test-SCuBAValidProviderJson {
                 param (
