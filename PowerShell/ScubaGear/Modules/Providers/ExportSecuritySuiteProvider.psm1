5621372281562791146f392c00f8aa2674c26ffc
Hilton <Alden>
2026-05-13 10:29:58 -0700

    update provider

--- PowerShell/ScubaGear/Modules/Providers/ExportSecuritySuiteProvider.psm1
+++ PowerShell/ScubaGear/Modules/Providers/ExportSecuritySuiteProvider.psm1
@@ -56,6 +56,8 @@ function Export-SecuritySuiteProvider {
     $AntiPhishRule = ConvertTo-Json @($Tracker.TryCommand("Get-AntiPhishRule"))
     $AcceptedDomains = ConvertTo-Json @($Tracker.TryCommand("Get-AcceptedDomain"))
     $ConnectionFilter = ConvertTo-Json @($Tracker.TryCommand("Get-HostedConnectionFilterPolicy"))
+    $AntiMalwarePolicy = ConvertTo-Json @$Tracker.TryCommand("Get-MalwareFilterPolicy")
+    $AntiMalwareRule = ConvertTo-Json @$Tracker.TryCommand("Get-MalwareFilterRule")
 
     # Test if Defender specific commands are available. If the tenant does
     # not have a defender license (plan 1 or plan 2), the following
@@ -167,6 +169,8 @@ function Export-SecuritySuiteProvider {
     "conn_filter": $ConnectionFilter,
     "defender_license": $DefenderLicense,
     "defender_dlp_license": $DLPLicense,
+    "anti_malware_policies": $AntiMalwarePolicy,
+    "anti_malware_rules": $AntiMalwareRule,
     "securitysuite_successful_commands": $SuccessfulCommands,
     "securitysuite_unsuccessful_commands": $UnSuccessfulCommands,
 "@
