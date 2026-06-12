4b45bb1675d6e748adcf9ee8068500586a853287
skirkpatrickMSFT <skirkpatrick@microsoft.com>
2026-06-12 07:53:55 -0400

    Minimize merge conflicts: restore PP/SPO/PBI token blocks, align imports with 2090

--- PowerShell/ScubaGear/Modules/Providers/ProviderHelpers/CommandTracker.psm1
+++ PowerShell/ScubaGear/Modules/Providers/ProviderHelpers/CommandTracker.psm1
@@ -6,6 +6,7 @@ Import-Module -Name $PSScriptRoot/EXORestHelper.psm1 -Function Invoke-EXORestMet
 Import-Module -Name $PSScriptRoot/PowerPlatformRestHelper.psm1 -Function Get-PowerPlatformTenantSettingsRest, Get-PowerPlatformEnvironmentsRest, Get-PowerPlatformDlpPoliciesRest, Get-PowerPlatformTenantIsolationRest
 Import-Module -Name $PSScriptRoot/SPORestHelper.psm1 -Function Get-SPOTenantRest
 Import-Module -Name $PSScriptRoot/../../Utility/Utility.psm1 -Function Invoke-GraphDirectly, ConvertFrom-GraphHashtable
+Import-Module -Name $PSScriptRoot/AADAppManagementPolicyHelper.psm1 -Function Get-AppManagementPolicies
 Import-Module -Name $PSScriptRoot/../../Utility/ScubaLogging.psm1 -Function Write-ScubaLog
 Import-Module -Name $PSScriptRoot/AADAppManagementPolicyHelper.psm1 -Function Get-AppManagementPolicies
 
