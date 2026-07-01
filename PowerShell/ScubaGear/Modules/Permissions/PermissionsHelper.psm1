71afeae8c69667bf535b30355035d02d93b47145
Dick Tracy II <54296228+DickTracyII@users.noreply.github.com>
2026-07-01 09:09:18 -0400

    Update PowerShell/ScubaGear/Modules/Permissions/PermissionsHelper.psm1
    makes sense
    
    Co-authored-by: MichaelHicks-MSFT <181162230+MichaelHicks-MSFT@users.noreply.github.com>

--- PowerShell/ScubaGear/Modules/Permissions/PermissionsHelper.psm1
+++ PowerShell/ScubaGear/Modules/Permissions/PermissionsHelper.psm1
@@ -82,7 +82,7 @@ Function Get-ScubaGearPermissions {
 
     .NOTES
         NAME: Get-ScubaGearPermissions
-        VERSION: 1.9
+        VERSION: 2.0
 
         USE TO FIND PERMS:
             (Find-MgGraphCommand -Command Get-MgBetaPolicyRoleManagementPolicyAssignment).Permissions | Select Name, IsLeastPrivilege
