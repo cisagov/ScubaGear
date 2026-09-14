b2f92ba93b89ad559e46f15abf631fe9aae6c58f
skirkpatrickMSFT <skirkpatrick@microsoft.com>
2026-09-14 11:40:12 -0400

    Remove trailing whitespace in Connection.psm1 flagged by PSSA

--- PowerShell/ScubaGear/Modules/Connection/Connection.psm1
+++ PowerShell/ScubaGear/Modules/Connection/Connection.psm1
@@ -422,7 +422,7 @@ function Connect-Tenant {
                         $TokenData.TeamsUnifiedBaseUrl = ""
                         $TokenData.TeamsUnifiedAccessToken = ""
                     }
-                    
+
                     # MS Teams Powershell Cmdlets well-knodwn client ID used when authenticating interactively
                     $TeamsClientId = "12128f48-ec9e-42f0-b203-ea49fb6af367"
 
@@ -434,7 +434,7 @@ function Connect-Tenant {
                             -AppID $ServicePrincipalParams.CertThumbprintParams.AppID `
                             -Tenant $ServicePrincipalParams.CertThumbprintParams.Organization `
                             -M365Environment $M365Environment
-                        
+
                         # Set auth return values to empty strings since Teams unified setttings API is only available for interactive auth flows.
                         $TokenData.TeamsUnifiedBaseUrl = ""
                         $TokenData.TeamsUnifiedAccessToken = ""
