19cdf5e42af81648dbacdf4ece8e2ee7bf42bcaf
mitchelbaker-cisa <mitchel.baker@cisa.dhs.gov>
2026-09-16 22:50:22 +0000

    update smoke tests to check for anti-spam policies in security suite baseline

--- Testing/Functional/Products/Products.Tests.ps1
+++ Testing/Functional/Products/Products.Tests.ps1
@@ -526,12 +526,15 @@ Describe "Policy Checks for <ProductName>" {
                         }
                     }
                     # Security Suite configuration tables are appended to the Security Suite report.
-                    elseif ($null -ne $TableClass -and $TableClass -match "securitysuite-(sensitive-users|partner-domains|anti-phish-policies)-table") {
+                    elseif ($null -ne $TableClass -and $TableClass -match "securitysuite-(sensitive-users|partner-domains|anti-phish-policies|anti-spam-policies)-table") {
                         $ExpectedHeaders = @(if ($TableClass -match "securitysuite-sensitive-users-table") {
                             "Username", "Email"
                         }
                         elseif ($TableClass -match "securitysuite-partner-domains-table") {
                             "Partner Domain"
+                        }
+                        elseif ($TableClass -match "securitysuite-anti-spam-policies-table") {
+                            "", "Policy", "Enabled", "Priority", "Applicability", "Spam Actions", "Allowed Senders", "Allowed Sender Domains"
                         }
                         else {
                             "", "Policy", "Enabled", "Priority", "Applicability", "Impersonation Protection", "Partner Domains Protected", "Safety Indicators"
@@ -552,8 +555,8 @@ Describe "Policy Checks for <ProductName>" {
 
                             if ($RowData.Count -gt 0) {
                                 $RowData.Count | Should -BeExactly $ExpectedHeaders.Count
-                                if ($TableClass -match "securitysuite-anti-phish-policies-table") {
-                                    $RowData[2].Text | Should -BeIn @("true", "false") -Because "The anti-phish policy Enabled value must be a Boolean"
+                                if ($TableClass -match "securitysuite-(anti-phish|anti-spam)-policies-table") {
+                                    $RowData[2].Text | Should -BeIn @("true", "false") -Because "The protection policy Enabled value must be a Boolean"
                                 }
                             }
                         }
