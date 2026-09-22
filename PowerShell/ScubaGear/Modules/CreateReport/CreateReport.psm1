4b44db3979108c5db2cfc69c3a123effb0c275c3
Chad Poland <128160399+Chad-CISA@users.noreply.github.com>
2026-09-22 18:37:58 +0000

    Add tenant domain name to product specific reports (#2426)
    * Add tenant domain name to product specific reports
    
    * Verify tenant domain name in product report smoke test
    
    ---------
    
    Co-authored-by: Ted Kolovos <107076927+tkol2022@users.noreply.github.com>

--- PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
+++ PowerShell/ScubaGear/Modules/CreateReport/CreateReport.psm1
@@ -217,6 +217,7 @@ function New-Report {
 
     $MetaData += [pscustomobject]@{
         "Tenant Display Name" = $SettingsExport.tenant_details.DisplayName;
+        "Tenant Domain Name" = $SettingsExport.tenant_details.DomainName;
         "Report Date" = $SettingsExport.date;
         "Baseline Version" = $SettingsExport.baseline_version;
         "Module Version" = $SettingsExport.module_version
